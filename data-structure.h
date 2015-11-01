5757cb4f71abdc7b9f784be7f1bcd0d1a615fb11
yihaoye <470803539@qq.com>
2015-11-01 23:54:07 +0800

    add lecture 10,11

--- data-structure.h
+++ data-structure.h
@@ -151,3 +151,93 @@
 	
 	
 	
+//lecture 10
+//头指针是链表的必要元素，空链表也有头指针。头结点则不是必要的，但头结点能统一操作，比如在第一个有效结点前插入元素。头结点的数据域一般为空，但也可以用来存放链表长度。
+	
+//10.1 C语言中用结构指针来描述单链表
+	typedef struct Node
+	{
+		ElemType data;		// 数据域
+		struct Node *Next;	// 指针域
+	} Node;
+	typedef struct Node *LinkList;
+	//若p是指向线性表第i个结点的指针，则该结点的数据域可用p->data表示，该结点的指针域可用p->next表示
+	//若p->data = ai，则p->next->data = ai+1
+	
+	
+//10.2 getElem.c 实现GetElem的代码 (单链表)
+	
+	
+	
+	
+//lecture 11
+
+//11.1 ListInsert.c 实现ListInsert的代码 (单链表)
+	//初始条件：顺序线性表L已存在，1<=i<=ListLength(L)。
+	//操作结果：在L中第i个位置之前插入新的数据元素e，L的长度加1。
+	
+Status ListInsert(LinkList *L, int i, ElemType e)
+{
+	int j;
+	LinkList p, s;
+	
+	p = *L;
+	j = 1;
+	
+	while(p && j<i)  // 用于寻找第i个结点
+	{
+		p = p->next;
+		j++;
+	}
+	
+	if(!p || j>i)
+	{
+		return ERROR;
+	}
+	
+	s = (LinkList)malloc(sizeof(Node)); // malloc进行内存分配（返回所分配内存块的首地址，以指针返回），sizeof判断其参数（数据类型）所占字节数
+	s->data = e;
+	
+	s->next = p->next;
+	p->next = s;
+	
+	return OK;
+}
+	
+
+
+//11.2 ListDelete.c 实现ListDelete的代码 （单链表）
+	//初始条件：顺序线性表L已存在，1<=i<=ListLength(L)。
+	//操作结果：删除L的第i个数据元素，并用e返回其值，L的长度-1。
+	
+Status ListDelete(LinkList *L, int i, ElemType e)
+{
+	int j;
+	LinkList p, q;
+	
+	p = *L;
+	j = 1;
+	
+	while(p->next && j<i)  // 用于寻找第i个结点
+	{
+		p = p->next;
+		++j;
+	}
+	
+	if(!(p->next) || j>i)
+	{
+		return ERROR;
+	}
+	
+	q = p->next;
+	p->next = q->next;
+	
+	*e = q->data;
+	free(q);
+	
+	return OK;
+}
+	
+	
+	
+	
