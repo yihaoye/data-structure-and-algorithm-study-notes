57d332423c530de67cff0663361e0c3812a913c6
yihaoye <yy264@students.waikato.ac.nz>
2016-01-20 02:13:47 +1300

    leetcode practice added

---
+++ leetcode practice/medium/238 Product of Array Except Self.cpp
@@ -0,0 +1,12 @@
+//Question:
+/*
+ Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i] (除本身之外的数组之积). 
+
+ Solve it without division and in O(n).
+
+ For example, given [1,2,3,4], return [24,12,8,6]. 
+*/
+
+
+
+//Solution:
