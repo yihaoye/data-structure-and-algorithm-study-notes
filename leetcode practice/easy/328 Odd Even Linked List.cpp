ca61876ad2950a6911c78d5782e8d8a06e07e004
yihaoye <yy264@students.waikato.ac.nz>
2016-01-19 23:51:30 +1300

    leetcode practice added

--- leetcode practice/easy/328 Odd Even Linked List.cpp
+++ leetcode practice/easy/328 Odd Even Linked List.cpp
@@ -12,3 +12,36 @@ Note:
 The relative order inside both the even and odd groups should remain as it was in the input.
 The first node is considered odd, the second node even and so on ... 
 */
+
+
+
+
+//Solution:
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution { public: ListNode* oddEvenList(ListNode* head) {
+
+    if(head == NULL || head->next == NULL)
+        return head;
+    ListNode *odd = head;
+    ListNode *even_head = head->next;
+    ListNode *even = even_head;
+
+    while(even != NULL && even->next != NULL)
+    {
+        odd->next = even->next;
+        odd = odd->next;
+        even->next = odd->next;
+        even = even->next;
+    }
+    odd->next = even_head;
+    return head;
+	}
+
+};
