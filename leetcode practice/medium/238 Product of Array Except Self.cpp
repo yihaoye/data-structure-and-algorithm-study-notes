5244c739bfdc738cfab9dee233672e022e3123cc
yihaoye <yy264@students.waikato.ac.nz>
2016-04-14 21:38:42 +1200

    leetcode question 238

--- leetcode practice/medium/238 Product of Array Except Self.cpp
+++ leetcode practice/medium/238 Product of Array Except Self.cpp
@@ -9,7 +9,27 @@
 
 
 
-//Solution:
+//Others' Solution:
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> fromBegin(n);
+        fromBegin[0]=1;
+        vector<int> fromLast(n);
+        fromLast[0]=1;
 
+        for(int i=1;i<n;i++){
+            fromBegin[i]=fromBegin[i-1]*nums[i-1];
+            fromLast[i]=fromLast[i-1]*nums[n-i];
+        }
+
+        vector<int> res(n);
+        for(int i=0;i<n;i++){
+            res[i]=fromBegin[i]*fromLast[n-1-i];
+        }
+        return res;
+    }
+};
 
  
