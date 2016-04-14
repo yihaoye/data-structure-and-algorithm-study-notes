b21dc53547681727a6d91e0ef1a04f6ea7f119cf
yihaoye <yy264@students.waikato.ac.nz>
2016-04-14 22:37:41 +1200

    improve leetcode 238

--- leetcode practice/medium/238 Product of Array Except Self.cpp
+++ leetcode practice/medium/238 Product of Array Except Self.cpp
@@ -14,22 +14,18 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
-        vector<int> fromBegin(n);
-        fromBegin[0]=1;
-        vector<int> fromLast(n);
-        fromLast[0]=1;
-
-        for(int i=1;i<n;i++){
-            fromBegin[i]=fromBegin[i-1]*nums[i-1];
-            fromLast[i]=fromLast[i-1]*nums[n-i];
-        }
-
         vector<int> res(n);
-        for(int i=0;i<n;i++){
-            res[i]=fromBegin[i]*fromLast[n-1-i];
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i-1] * nums[i-1]; //res[i] means all left elements product except nums[i]
+        }
+        int right = 1;
+        for (int i = n-1; i >= 0; i--) {
+            res[i] *= right;	//"right" means all right elements product except nums[i] (for nums[i])
+            right *= nums[i];	//calculate nums[i-1]'s right handside elements product
         }
         return res;
-    }
+        }
 };
 
  
