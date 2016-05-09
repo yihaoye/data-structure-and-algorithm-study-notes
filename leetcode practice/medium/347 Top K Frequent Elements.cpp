ff0039272f1048deff0bf5e737c639ec9f583229
yihaoye <yy264@students.waikato.ac.nz>
2016-05-10 03:34:38 +1200

    add 347 c++ solution

---
+++ leetcode practice/medium/347 Top K Frequent Elements.cpp
@@ -0,0 +1,39 @@
+//Question:
+/*
+ Given a non-empty array of integers, return the k most frequent elements.
+
+For example,
+Given [1,1,1,2,2,3] and k = 2, return [1,2].
+
+Note:
+
+    You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+    Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+*/
+
+
+
+
+//Other's Solution: ?
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> map;
+        for(int num : nums){
+            map[num]++;
+        }
+
+        vector<int> res;
+        // pair<first, second>: first is frequency,  second is number
+        priority_queue<pair<int,int>> pq; 
+        for(auto it = map.begin(); it != map.end(); it++){
+            pq.push(make_pair(it->second, it->first));
+            if(pq.size() > (int)map.size() - k){
+                res.push_back(pq.top().second);
+                pq.pop();
+            }
+        }
+        return res;
+    }
+};
