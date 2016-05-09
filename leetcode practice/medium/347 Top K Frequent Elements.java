abfd8a5d9fa9fcda56481bb60180c8972005306c
yihaoye <yy264@students.waikato.ac.nz>
2016-05-10 03:54:18 +1200

    347 java add one more solution

--- leetcode practice/medium/347 Top K Frequent Elements.java
+++ leetcode practice/medium/347 Top K Frequent Elements.java
@@ -43,6 +43,41 @@ public List<Integer> topKFrequent(int[] nums, int k) {
     return res;
 }    
 
+
+//Other's Solution 2:
+public List<Integer> topKFrequent(int[] nums, int k) {
+    Map<Integer, Integer> frequencyMap = new HashMap<>();
+    int maxFrequency = 0;
+
+    for (int n : nums) {
+        int frequency = frequencyMap.getOrDefault(n, 0) + 1;
+        frequencyMap.put(n, frequency);
+        maxFrequency = Math.max(maxFrequency, frequency);
+    }
+
+    // here i is the frequency and bucket.get(i) is the numbers that having this frequency
+    List<List<Integer>> bucket = new ArrayList<>(maxFrequency);
+    while (maxFrequency-- >= 0) {
+        bucket.add(new ArrayList<>());
+    }
+
+    for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+        int frequency = entry.getValue();
+        bucket.get(frequency).add(entry.getKey());
+    }
+
+    List<Integer> res = new ArrayList<>();
+    for (int pos = bucket.size() - 1; pos >= 0 && res.size() < k; pos--) {
+        res.addAll(bucket.get(pos));
+    }
+    return res;
+}
+
+
+
+
+
+
 /* Someone's comment:
 
 Thanks for sharing, only one nitpick:
