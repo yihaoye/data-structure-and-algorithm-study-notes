ac346ee1e64421399daf938bda947f453acc927f
yihaoye <yy264@students.waikato.ac.nz>
2016-05-15 15:59:45 +1200

    add 94 solution

--- leetcode practice/medium/94 Binary Tree Inorder Traversal.java
+++ leetcode practice/medium/94 Binary Tree Inorder Traversal.java
@@ -19,4 +19,33 @@ Note: Recursive solution is trivial, could you do it iteratively?
 
 
 
-//Other's Solution:
+//Other's Solution: (Iterative solution)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution{
+	public List<Integer> inorderTraversal(TreeNode root) {
+	    List<Integer> list = new ArrayList<Integer>();
+
+	    Stack<TreeNode> stack = new Stack<TreeNode>();
+	    TreeNode cur = root;
+
+	    while(cur!=null || !stack.empty()){
+	        while(cur!=null){
+	            stack.add(cur);
+	            cur = cur.left;
+	        }
+	        cur = stack.pop();
+	        list.add(cur.val);
+	        cur = cur.right;
+	    }
+
+	    return list;
+	}
+}
