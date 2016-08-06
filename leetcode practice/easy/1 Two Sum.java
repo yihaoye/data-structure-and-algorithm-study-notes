76a0ee06dbd235afa9f61c9012f864e8e863ffb0
yihaoye <yy264@students.waikato.ac.nz>
2016-08-07 00:40:39 +1200

    add leetcode 1 two sum java

---
+++ leetcode practice/easy/1 Two Sum.java
@@ -0,0 +1,71 @@
+//Question:
+/*
+    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+    You may assume that each input would have exactly one solution.
+
+    Example:
+
+    Given nums = [2, 7, 11, 15], target = 9,
+
+    Because nums[0] + nums[1] = 2 + 7 = 9,
+    return [0, 1].
+
+    UPDATE (2016/2/13):
+    The return format had been changed to zero-based indices. Please read the above updated description carefully. 
+*/
+
+
+
+
+//Official Answer:
+//Approach #1 (Brute Force) [Accepted]
+public int[] twoSum(int[] nums, int target) {
+    for (int i = 0; i < nums.length; i++) {
+        for (int j = i + 1; j < nums.length; j++) {
+            if (nums[j] == target - nums[i]) {
+                return new int[] { i, j };
+            }
+        }
+    }
+    throw new IllegalArgumentException("No two sum solution");
+}
+
+
+
+//Approach #2 (Two-pass Hash Table) [Accepted]
+public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        map.put(nums[i], i);
+    }
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement) && map.get(complement) != i) {
+            return new int[] { i, map.get(complement) };
+        }
+    }
+    throw new IllegalArgumentException("No two sum solution");
+}
+
+
+
+//Approach #3 (One-pass Hash Table) [Accepted]
+public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) {
+            return new int[] { map.get(complement), i };
+        }
+        map.put(nums[i], i);
+    }
+    throw new IllegalArgumentException("No two sum solution");
+}
+/*
+Complexity Analysis:
+
+    Time complexity : O(n). We traverse the list containing nnn elements only once. Each look up in the table costs only O(1) time.
+
+    Space complexity : O(n). The extra space required depends on the number of items stored in the hash table, which stores at most nnn elements.
+*/
