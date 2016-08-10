9325c6d31177bffa9c76d09cb732ef386b9b8911
yihaoye <yy264@students.waikato.ac.nz>
2016-08-11 03:54:26 +1200

    leetcode 2 Add Two Numbers C++ answer

--- leetcode practice/medium/2 Add Two Numbers.cpp
+++ leetcode practice/medium/2 Add Two Numbers.cpp
@@ -9,3 +9,34 @@ Output: 7 -> 0 -> 8
 
 
 //Answer:
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {} //构造函数
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *p = new ListNode(0);
+        ListNode *point = p;
+        int sum = 0;
+        while(l1 != NULL || l2 != NULL)
+        {
+            
+            sum += (l1!=NULL ? l1->val:0) + (l2!=NULL ? l2->val:0);
+            p->val = sum % 10;
+            sum = sum / 10;
+            l1 = (l1==NULL ? NULL:l1->next); // if(l1)l1 = l1->next;
+            l2 = (l2==NULL ? NULL:l2->next); 
+            if(l1||l2||sum>0){
+                p->next = new ListNode(sum);
+                p = p->next;
+            }
+        }
+        return point;
+    }
+};
