5fd72b7a6103c4685e058cc0a66243b760f38405
yihaoye <yy264@students.waikato.ac.nz>
2016-08-12 21:09:42 +1200

    add leetcode question 3 C++ solution

--- leetcode practice/medium/3 Longest Substring Without Repeating Characters.cpp
+++ leetcode practice/medium/3 Longest Substring Without Repeating Characters.cpp
@@ -14,6 +14,19 @@ Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer 
 
 
 
-//Answer:
-
+//Other's Answer:
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256, -1);
+        int maxLen = 0, start = -1;
+        for (int i = 0; i != s.length(); i++) {
+            if (dict[s[i]] > start)
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
+        }
+        return maxLen;
+    }
+};
 
