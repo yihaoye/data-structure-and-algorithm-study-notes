7272d2e027c625712498c170c5fbe5c32e64f1c3
yihaoye <yy264@students.waikato.ac.nz>
2016-08-22 00:06:12 +1200

    other practice

---
+++ other practice/22-08-2016 sort in specified order.java
@@ -0,0 +1,34 @@
+//Question:
+/*
+given a integer "n" (for example: 100)
+Output list<Integer>: [1,10,11,12,13,14,15,16,17,18,19,100,2,20,21,22,23,24,25,26,27,28,29,3,30,31,32,33,34,35,36,37,38,39,4,40,41,42,43,44...]
+*/
+
+
+
+//Answer:
+public class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        int count=0;
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        ArrayList<ArrayList<Integer>> Lists = new ArrayList<ArrayList<Integer>>();
+        for(int i=0; i<9; i++){
+            Lists.add(new ArrayList<Integer>());
+        }
+        while(count<n){
+            count++;
+            Lists.get(get_top(count)-1).add(count);
+        }
+        for(ArrayList<Integer> temp : Lists){
+            result.addAll(temp);
+        }
+        return result;
+    }
+    
+    public int get_top(int number){
+        while(number>=10){
+            number = number / 10;
+        }
+        return number;
+    }
+}
