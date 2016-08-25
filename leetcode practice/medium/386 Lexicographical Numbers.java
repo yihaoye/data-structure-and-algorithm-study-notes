efe4cc03162cc39d59f5ec6ebb1da06a85c4561f
yihaoye <yy264@students.waikato.ac.nz>
2016-08-25 19:02:05 +1200

    leetcode q386 add recursive java solution

--- leetcode practice/medium/386 Lexicographical Numbers.java
+++ leetcode practice/medium/386 Lexicographical Numbers.java
@@ -11,7 +11,7 @@ Please optimize your algorithm to use less time and space. The input size may be
 
 
 
-//Other's Answer:
+//Other's Answer: (iterative solution)
 public class Solution {
     public List<Integer> lexicalOrder(int n) {
         List<Integer> list = new ArrayList<>(n);
@@ -30,5 +30,23 @@ public class Solution {
             }
         }
         return list;
+    }
+}
+
+
+//Other's Answer 2: (Recursive Java Solution using pre-order traversal)
+public class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> res = new ArrayList<>();
+        lexicalOrderHelper(res, 1, n);
+        return res;
+    }
+
+    private void lexicalOrderHelper(List<Integer> res, int i, int n) {
+        if(i > n) return;
+        res.add(i);
+        lexicalOrderHelper(res, i * 10, n);
+        if(i + 1 <= (i / 10) * 10 + 9) lexicalOrderHelper(res, i + 1, n);
+        else return;
     }
 }
