68e8041f501a2b7436e489c895fd9d9cfd6e00fb
yihaoye <yy264@students.waikato.ac.nz>
2016-08-29 23:59:26 +1200

    add explain

--- leetcode practice/medium/386 Lexicographical Numbers.java
+++ leetcode practice/medium/386 Lexicographical Numbers.java
@@ -32,6 +32,12 @@ public class Solution {
         return list;
     }
 }
+/*
+The basic idea is to find the next number to add.
+Take 45 for example: if the current number is 45, the next one will be 450 (450 == 45 * 10)(if 450 <= n), or 46 (46 == 45 + 1) (if 46 <= n) or 5 (5 == 45 / 10 + 1)(5 is less than 45 so it is for sure less than n).
+We should also consider n = 600, and the current number = 499, the next number is 5 because there are all "9"s after "4" in "499" so we should divide 499 by 10 until the last digit is not "9".
+It is like a tree, and we are easy to get a sibling, a left most child and the parent of any node.
+*/
 
 
 //Other's Answer 2: (Recursive Java Solution using pre-order traversal)
