59a1d20b112b8670281a0feee7774a2b7847d1f7
yihaoye <yy264@students.waikato.ac.nz>
2016-10-09 00:29:43 +1300

    leetcode q176

---
+++ leetcode practice/database/easy/176 Second Highest Salary.sql
@@ -0,0 +1,36 @@
+# Question:
+/*
+ Write a SQL query to get the second highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+
+For example, given the above Employee table, the second highest salary is 200. If there is no second highest salary, then the query should return null.
+*/
+
+
+
+
+
+# Others' Solution A:
+SELECT max(Salary)
+FROM Employee
+WHERE Salary < (SELECT max(Salary) FROM Employee)
+/* Explain:
+Using max() will return a NULL if the value doesn't exist. So there is no need to UNION a NULL. Of course, if the second highest value is guaranteed to exist, using LIMIT 1,1 will be the best answer.
+*/
+
+
+
+# Others' Solution B:
+select (
+  select distinct Salary from Employee order by Salary Desc limit 1 offset 1
+)as second
+/* Explain: 
+Change the number after 'offset' gives u n-th highest salary
+*/
