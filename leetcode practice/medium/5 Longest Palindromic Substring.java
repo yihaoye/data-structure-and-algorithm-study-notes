6486da7bf76398078cd102c7b00ee8d34b63aca7
yihaoye <yihaoyemail@gmail.com>
2019-02-20 23:34:43 +1300

    leetcode question 5 other solution

--- leetcode practice/medium/5 Longest Palindromic Substring.java
+++ leetcode practice/medium/5 Longest Palindromic Substring.java
@@ -13,6 +13,35 @@ Input: "cbbd"
 Output: "bb"
 */
 
+
+// Other's Solution:
+public class Solution {
+    private int lo, maxLen;
+    
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2)
+            return s;
+        
+        for (int i = 0; i < len-1; i++) {
+             extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
+             extendPalindrome(s, i, i+1); //assume even length.
+        }
+        return s.substring(lo, lo + maxLen);
+    }
+    
+    private void extendPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        if (maxLen < k - j - 1) {
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
+    }}
+
+
 // My Solution:
 class Solution {
     public String longestPalindrome(String s) {
