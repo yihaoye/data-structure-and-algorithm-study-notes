2878fc9af00486f481e5ec5a193fd9d4cfa46412
yihaoye <yihaoyemail@gmail.com>
2019-02-20 22:38:39 +1300

    question 14 my solution removed, future work

--- leetcode practice/easy/14 Longest Common Prefix.java
+++ leetcode practice/easy/14 Longest Common Prefix.java
@@ -38,31 +38,7 @@ class Solution {
 // My Answer:
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String result = "";
-        int i = 0;
-        if (strs.length <= 1) {
-            return result;
-        }
-        char[][] charArrays = new char[strs.length][];
-        for (String str : strs) {
-            if (str == "") {
-                return result;
-            }
-            charArrays[i] = str.toCharArray();
-            i++;
-        }
-        int j = 0;
-        while (true) {
-            for (int k = 0; k < i-1; k++) {
-                // if (charArrays[k].length < j || charArrays[k+1].length < j) {
-                //     return result;
-                // }
-                if (charArrays[k][j] != charArrays[k+1][j]) {
-                    return result;
-                }
-            }
-            result += charArrays[0][j];
-            j++;
-        }
+        String prefix;
+        return prefix;
     }
 }
