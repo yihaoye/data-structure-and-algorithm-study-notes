7dbea6abf3db8337507e65e6ef9aae21824066cf
yihaoye <yihao.ye@vendhq.com>
2019-04-22 17:58:09 +1200

    leetcode question 401 roun 2

--- Leetcode Practices/algorithms/easy/401 Binary Search.java
+++ Leetcode Practices/algorithms/easy/401 Binary Search.java
@@ -20,6 +20,45 @@ The minute must be consist of two digits and may contain a leading zero, for exa
 
 
 
+// Orther's Solution: (Backtracking)
+public class Solution {
+    public List<String> readBinaryWatch(int num) {
+        List<String> res = new ArrayList<>();
+        int[] nums1 = new int[]{8, 4, 2, 1}, nums2 = new int[]{32, 16, 8, 4, 2, 1};
+        for(int i = 0; i <= num; i++) {
+            List<Integer> list1 = generateDigit(nums1, i);
+            List<Integer> list2 = generateDigit(nums2, num - i);
+            for(int num1: list1) {
+                if(num1 >= 12) continue;
+                for(int num2: list2) {
+                    if(num2 >= 60) continue;
+                    res.add(num1 + ":" + (num2 < 10 ? "0" + num2 : num2));
+                }
+            }
+        }
+        return res;
+    }
+
+    private List<Integer> generateDigit(int[] nums, int count) {
+        List<Integer> res = new ArrayList<>();
+        generateDigitHelper(nums, count, 0, 0, res);
+        return res;
+    }
+
+    private void generateDigitHelper(int[] nums, int count, int pos, int sum, List<Integer> res) {
+        if(count == 0) {
+            res.add(sum);
+            return;
+        }
+        
+        for(int i = pos; i < nums.length; i++) {
+            generateDigitHelper(nums, count - 1, i + 1, sum + nums[i], res);    
+        }
+    }
+}
+
+
+
 // My Solution 1: (Really bad performance for scale in both time and space, so it is not practical)
 class Solution {
     public List<String> readBinaryWatch(int num) {
