147ee9daf9f3468ca4cbf857228f672250bdff60
yihaoye <yihao.ye@vendhq.com>
2019-04-28 19:42:18 +1200

    leetcode question 110 round 2

--- Leetcode Practices/algorithms/easy/110 Balanced Binary Tree.java
+++ Leetcode Practices/algorithms/easy/110 Balanced Binary Tree.java
@@ -32,6 +32,30 @@ Return false.
 
 
 
+// Other's Solution:
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        return height(root) != -1;
+        
+    }
+
+    public int height(TreeNode node){
+        if (node == null) return 0;
+
+        int lH = height(node.left);
+        if (lH == -1) return -1;
+
+        int rH = height(node.right);
+        if (rH == -1) return -1;
+        
+        if (lH - rH < -1 || lH - rH > 1) return -1;
+        return Math.max(lH, rH) + 1;
+    }
+}
+
+
+
 // My Solution:
 /**
  * Definition for a binary tree node.
