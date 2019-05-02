e3f369c028e5ef09e52fc4c7db2f6391aaa4df87
yihaoye <yihao.ye@vendhq.com>
2019-05-02 18:29:13 +1200

    leetcode question 973 round 2

--- Leetcode Practices/algorithms/medium/973 K Closest Points to Origin.java
+++ Leetcode Practices/algorithms/medium/973 K Closest Points to Origin.java
@@ -32,6 +32,16 @@ Note:
 
 
 
+// Other's Solution:
+class Solution {
+    public int[][] kClosest(int[][] points, int K) {
+        Arrays.sort(points, (p1, p2) -> p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1]);
+        return Arrays.copyOfRange(points, 0, K);
+    }
+}
+
+
+
 // My Solution:
 class Solution {
     public int[][] kClosest(int[][] points, int K) {
