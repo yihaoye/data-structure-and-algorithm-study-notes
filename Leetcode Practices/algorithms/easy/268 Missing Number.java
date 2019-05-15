e7c234c1ed17acf50c554479c994c19bbb6d951a
yihaoye <yihao.ye@vendhq.com>
2019-05-15 23:58:56 +1200

    leetcode question 268 round 0 more other solutions

--- Leetcode Practices/algorithms/easy/268 Missing Number.java
+++ Leetcode Practices/algorithms/easy/268 Missing Number.java
@@ -15,7 +15,7 @@ Your algorithm should run in linear runtime complexity. Could you implement it u
 
 
 
-// Other's Solution:
+// Other's Solution 1:
 class Solution {
     public int missingNumber(int[] nums) {
         int xor = 0, i = 0;
@@ -25,3 +25,49 @@ class Solution {
         return xor ^ i;
     }
 }
+/*
+The basic idea is to use XOR operation. Since a^b^b =a, which means two xor operations with the same number will eliminate the number and reveal the original number.
+In this solution, apply XOR operation to both the index and value of the array. In a complete array with no missing numbers, the index and value should be perfectly corresponding( nums[index] = index), so in a missing array, what left finally is the missing number.
+*/
+
+
+
+// Other's Solution 2:
+// 1.XOR
+class Solution {
+    public int missingNumber(int[] nums) { //xor
+        int res = nums.length;
+        for(int i=0; i<nums.length; i++){
+            res ^= i;
+            res ^= nums[i];
+        }
+        return res;
+    }
+}
+// 2.SUM
+class Solution {
+    public int missingNumber(int[] nums) { //sum
+        int len = nums.length;
+        int sum = (0+len)*(len+1)/2;
+        for(int i=0; i<len; i++)
+            sum-=nums[i];
+        return sum;
+    }
+}
+// 3.Binary Search
+class Solution {
+    public int missingNumber(int[] nums) { //binary search
+        Arrays.sort(nums);
+        int left = 0, right = nums.length, mid= (left + right)/2;
+        while(left<right){
+            mid = (left + right)/2;
+            if(nums[mid]>mid) right = mid;
+            else left = mid+1;
+        }
+        return left;
+    }
+}
+/*
+Summary:
+If the array is in order, I prefer Binary Search method. Otherwise, the XOR method is better.
+*/
