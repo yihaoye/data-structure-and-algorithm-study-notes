ca6d0263eca0df9e67b18116c07f9fd81daffbcd
yihaoye <yihao.ye@vendhq.com>
2019-05-23 23:46:30 +1200

    leetcode question 763 others solution

--- Leetcode Practices/algorithms/medium/763 Partition Labels.java
+++ Leetcode Practices/algorithms/medium/763 Partition Labels.java
@@ -16,6 +16,36 @@ S will consist of lowercase letters ('a' to 'z') only.
 
 
 
+// Other's Solution:
+// 1. traverse the string record the last index of each char.
+// 2. using pointer to record end of the current sub string.
+class Solution {
+    public List<Integer> partitionLabels(String S) {
+        if(S == null || S.length() == 0){
+            return null;
+        }
+        List<Integer> list = new ArrayList<>();
+        int[] map = new int[26];  // record the last index of the each char
+
+        for(int i = 0; i < S.length(); i++){
+            map[S.charAt(i)-'a'] = i;
+        }
+        // record the end index of the current sub string
+        int last = 0;
+        int start = 0;
+        for(int i = 0; i < S.length(); i++){
+            last = Math.max(last, map[S.charAt(i)-'a']);
+            if(last == i){
+                list.add(last - start + 1);
+                start = last + 1;
+            }
+        }
+        return list;
+    }
+}
+
+
+
 // My Solution:
 class Solution {
     public List<Integer> partitionLabels(String S) {
