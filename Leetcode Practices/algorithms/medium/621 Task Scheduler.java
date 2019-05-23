fc65d78863a010e038087a642666251a2811e801
yihaoye <yihao.ye@vendhq.com>
2019-05-23 15:55:32 +1200

    leetcode question 621 round 0 still

--- Leetcode Practices/algorithms/medium/621 Task Scheduler.java
+++ Leetcode Practices/algorithms/medium/621 Task Scheduler.java
@@ -22,7 +22,7 @@ The integer n is in the range [0, 100].
 
 
 
-// Other's Solution:
+// Other's Solution 1:
 public class Solution {
     public int leastInterval(char[] tasks, int n) {
         int[] storage = new int[26];
@@ -44,6 +44,36 @@ public class Solution {
         }
         int space = (n + 1) * (max - 1) + count;
         return (space < tasks.length) ? tasks.length : space;
+    }
+}
+
+
+
+// Other's Solution 2:
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        // count char freq, task name doesn't matter, only freq matters
+        int[] freq = new int[26];
+        for (char c: tasks) freq[c - 'A']++;
+        // sort first, so we have max freq at freq[25]
+        Arrays.sort(freq);
+        int time = 0;
+        while (freq[25] > 0) { // while we still have task to do, start from most freq task
+            // GREEDY
+            // each round/row, try to finish n tasks
+            for (int i = 0, p = 25; i <= n; i++) { // n is the cooling down factor, p points to the next task to consume
+                if (p >= 0 && freq[p] > 0) { // if there is still task to do
+                    freq[p]--; // do task
+                    p--;       // move p to next freq task
+                    time++;    // take one cycle
+                } else if (freq[25] != 0) { // if this is NOT last row, need to fill in idle cycle
+                    time++;    // take one cycle
+                } // else freq[25] == 0 .   no more task to do and last row. we WON'T fill in idle cycle
+            }
+            // sort again so next round we're going to start from most freq task and consume n task if possible
+            Arrays.sort(freq);
+        }
+        return time;
     }
 }
 
