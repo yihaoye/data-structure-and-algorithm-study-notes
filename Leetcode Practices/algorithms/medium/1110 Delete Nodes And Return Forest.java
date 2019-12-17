4f220628d5d766ae95b43cc9814d491d62850fa6
yihaoye <yihao.ye@vendhq.com>
2019-12-17 18:28:59 +1300

    leetcode q1110 round 2

--- Leetcode Practices/algorithms/medium/1110 Delete Nodes And Return Forest.java
+++ Leetcode Practices/algorithms/medium/1110 Delete Nodes And Return Forest.java
@@ -22,6 +22,31 @@ to_delete contains distinct values between 1 and 1000.
 
 
 
+// Other's Solution:
+class Solution {
+    Set<Integer> to_delete_set;
+    List<TreeNode> res;
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        to_delete_set = new HashSet<>();
+        res = new ArrayList<>();
+        for (int i : to_delete)
+            to_delete_set.add(i);
+        helper(root, true);
+        return res;
+    }
+
+    private TreeNode helper(TreeNode node, boolean is_root) {
+        if (node == null) return null;
+        boolean deleted = to_delete_set.contains(node.val);
+        if (is_root && !deleted) res.add(node);
+        node.left = helper(node.left, deleted);
+        node.right =  helper(node.right, deleted);
+        return deleted ? null : node;
+    }
+}
+
+
+
 // My Solution 1:
 /**
  * Definition for a binary tree node.
