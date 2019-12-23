a1fed78dc51d61cca0bb033e578a55348460847d
yihaoye <yihao.ye@vendhq.com>
2019-12-23 18:41:16 +1300

    leetcode q1170 round 2

--- Leetcode Practices/algorithms/easy/1170 Compare Strings by Frequency of the Smallest Character.java
+++ Leetcode Practices/algorithms/easy/1170 Compare Strings by Frequency of the Smallest Character.java
@@ -27,6 +27,47 @@ queries[i][j], words[i][j] are English lowercase letters.
 
 
 
+// Other's Solution (binary search):
+class Solution {
+    public int[] numSmallerByFrequency(String[] queries, String[] words) {
+        int[] q = new int[queries.length], w = new int[words.length];
+        for (int i = 0; i < q.length; i++) {
+            q[i] = findMinCharCount(queries[i]);
+        }
+        for (int i = 0; i < w.length; i++) {
+            w[i] = findMinCharCount(words[i]);
+        }
+        Arrays.sort(w);
+        int[] ans = new int[q.length];
+        for (int i = 0; i < q.length; i++) {
+            int l = 0, r = w.length - 1;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (q[i] >= w[mid]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+            ans[i] = w.length - l;
+        }
+        return ans;
+    }
+
+    private int findMinCharCount(String str) {
+        int[] arr = new int[26];
+        for (char ch : str.toCharArray())
+            arr[ch - 'a']++;
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] != 0)
+                return arr[i];
+        }
+        return 0;
+    }
+}
+
+
+
 // My Solution 1:
 class Solution {
     public int[] numSmallerByFrequency(String[] queries, String[] words) {
