368e3b314eb0e913444136281829e6591193ec83
yihaoye <yihao.ye@vendhq.com>
2020-02-27 01:54:17 +1300

    leetcode q189 round 1

---
+++ Leetcode Practices/algorithms/easy/189 Rotate Array.java
@@ -0,0 +1,62 @@
+/*
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+
+Input: [1,2,3,4,5,6,7] and k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: [-1,-100,3,99] and k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+Note:
+
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+*/
+
+
+
+// My Solution 1:
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        
+        // edge case process
+        if (k > len) k = k%len;
+        if (len == 1 || k == 0) return;
+        
+        // migrate process
+        migrate(nums, len, k);
+    }
+    
+    public void migrate(int[] nums, int len, int k) {
+        int tmp = 0;
+        for (int i=0; i<len; i+=k) {
+            for (int j=0; j<k && i+k+j<len; j++) {
+                tmp = nums[i+k+j];
+                nums[i+k+j] = nums[j];
+                nums[j] = tmp;
+            }
+        }
+        if (len%k != 0) migrate(nums, k, k-len%k);
+    }
+    
+    /* example */
+    // [1,2,3,4,5,6,7,8] and k=3
+    // after migrate:
+    // tmp=8
+    // [7,8,6,1,2,3,4,5]
+    // after nested migrate: (for [7,8,6], len=3, k=1)
+    // tmp=6
+    // [6,7,8,1,2,3,4,5]
+    
+    // Although nested method, performance is ok hedged since either migrate() or nested migrate() take time or both similar average.
+}
