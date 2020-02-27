c2e365884ce732d9590d94027f04424e2d9c2a2d
yihaoye <yihao.ye@vendhq.com>
2020-02-27 14:59:36 +1300

    leetcode q189 round 0

--- Leetcode Practices/algorithms/easy/189 Rotate Array.java
+++ Leetcode Practices/algorithms/easy/189 Rotate Array.java
@@ -81,4 +81,20 @@ class Solution2 {
             end--;
         }
     }
+
+    /* Explain:
+        let a= [1,2,3,4,5,6,7]
+        k = 3.
+
+        we have to first reverse the whole array by swapping first element with the last one and so on..
+        you will get[7,6,5,4,3,2,1]
+
+        reverse the elements from 0 to k-1
+        reverse the elements 7,6,5
+        you will get [5,6,7,4,3,2,1]
+
+        reverse the elements from k to n-1
+        reverse the elements 4,3,2,1
+        you will get[5,6,7,1,2,3,4]
+    */
 }
