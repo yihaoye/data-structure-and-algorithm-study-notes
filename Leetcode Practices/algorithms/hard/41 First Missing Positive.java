7f621f32a1e2a1d18be8ed3c9234aef507b49dda
yihaoye <yihao.ye@vendhq.com>
2020-02-29 00:16:59 +1300

    leetcode q41 round 0

--- Leetcode Practices/algorithms/hard/41 First Missing Positive.java
+++ Leetcode Practices/algorithms/hard/41 First Missing Positive.java
@@ -38,3 +38,23 @@ class Solution {
 
 
 
+// Other's Solution:
+public class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while(i < nums.length){
+            if(nums[i] == i+1 || nums[i] <= 0 || nums[i] > nums.length) i++;
+            else if(nums[nums[i]-1] != nums[i]) swap(nums, i, nums[i]-1);
+            else i++;
+        }
+        i = 0;
+        while(i < nums.length && nums[i] == i+1) i++;
+        return i+1;
+    }
+    
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
