9cba8f0ea5043c4be9a60f2f42e96942896ffe56
yihaoye <yihao.ye@vendhq.com>
2020-03-01 12:15:24 +1300

    leetcode q299 round 2

--- Leetcode Practices/algorithms/easy/299 Bulls and Cows.java
+++ Leetcode Practices/algorithms/easy/299 Bulls and Cows.java
@@ -50,3 +50,29 @@ class Solution {
         return bulls+"A"+cows+"B";
     }
 }
+
+
+
+// Other's Solution:
+class Solution2 {
+    public String getHint(String secret, String guess) {
+        // The idea is to iterate over the numbers in secret and in guess and count all bulls right away. 
+        // For cows maintain an array that stores count of the number appearances in secret and in guess. 
+        // Increment cows when either number from secret was already seen in guest or vice versa.
+        int bulls = 0;
+        int cows = 0;
+        int[] numbers = new int[10];
+        for (int i = 0; i<secret.length(); i++) {
+            int s = Character.getNumericValue(secret.charAt(i));
+            int g = Character.getNumericValue(guess.charAt(i));
+            if (s == g) bulls++;
+            else {
+                if (numbers[s] < 0) cows++;
+                if (numbers[g] > 0) cows++;
+                numbers[s] ++;
+                numbers[g] --;
+            }
+        }
+        return bulls + "A" + cows + "B";
+    }
+}
