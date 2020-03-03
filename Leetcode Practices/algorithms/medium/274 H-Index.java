086c5ed7b802d55f0a34efbb8677ce1368048ba0
yihaoye <yihao.ye@vendhq.com>
2020-03-04 00:41:51 +1300

    leetcode q274 round 1

---
+++ Leetcode Practices/algorithms/medium/274 H-Index.java
@@ -0,0 +1,33 @@
+/*
+Given an array of citations (each citation is a non-negative integer) of a researcher, write a function to compute the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: "A scientist has index h if h of his/her N papers have at least h citations each, and the other N − h papers have no more than h citations each."
+
+Example:
+
+Input: citations = [3,0,6,1,5]
+Output: 3 
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had 
+             received 3, 0, 6, 1, 5 citations respectively. 
+             Since the researcher has 3 papers with at least 3 citations each and the remaining 
+             two with no more than 3 citations each, her h-index is 3.
+Note: If there are several possible values for h, the maximum one is taken as the h-index.
+*/
+
+
+
+// My Solution:
+class Solution {
+    public int hIndex(int[] citations) {
+        int len = citations.length;
+        if (len == 0) return 0;
+        Arrays.sort(citations);
+        if (citations[0] >= len) return len;
+        for (int i=1; i<len; i++) {
+            // len is N, len-i is h, i is N-h
+            if (citations[i] >= len-i && citations[i-1] <= len-i) return len-i;
+        }
+        
+        return 0;
+    }
+}
