a3a90336fba0e4339703ff550f7c861a206bc5eb
yihaoye <yihao.ye@vendhq.com>
2020-03-08 23:40:37 +1300

    leetcode q275 round 2

--- Leetcode Practices/algorithms/medium/275 H-Index II.java
+++ Leetcode Practices/algorithms/medium/275 H-Index II.java
@@ -46,3 +46,26 @@ class Solution {
         return len-start;
     }
 }
+
+
+
+// Other's Solution:
+class Solution {
+    public int hIndex(int[] citations) {
+        int len = citations.length;
+        int lo = 0, hi = len - 1;
+        while (lo <= hi) {
+            int med = (hi + lo) / 2;
+            if (citations[med] == len - med) {
+                return len - med;
+            } else if (citations[med] < len - med) {
+                lo = med + 1;
+            } else { 
+                // (citations[med] > len-med), med qualified as a hIndex,
+                // but we have to continue to search for a higher one.
+                hi = med - 1;
+            }
+        }
+        return len - lo;
+    }
+}
