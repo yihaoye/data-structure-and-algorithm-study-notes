e71de07af74ce5b52e22950d26e3c3700baaf2a1
yihaoye <yihao.ye@vendhq.com>
2020-04-25 01:35:17 +1200

    lc q547 round 2

--- Leetcode Practices/algorithms/medium/547 Friend Circles.java
+++ Leetcode Practices/algorithms/medium/547 Friend Circles.java
@@ -84,3 +84,64 @@ class UnionFindSet {
         return u;
     }
 }
+
+
+
+// Other's Solution:
+class Solution {
+    public int findCircleNum(int[][] M) {
+        UnionFindSet ufs = new UnionFindSet(M.length);
+        for (int i=0; i<M.length; i++) {
+            for (int j=i+1; j<M.length; j++) {
+                if (M[i][j] == 1) ufs.Union(i, j);
+            }
+        }
+        
+        Set<Integer> circles = new HashSet<>();
+        for (int i = 0; i < M.length; ++i) {
+            circles.add(ufs.Find(i));
+        }
+        
+        return circles.size();
+    }
+}
+
+class UnionFindSet {
+    private int[] parents_;
+    private int[] ranks_;
+ 
+    public UnionFindSet(int n) {
+        parents_ = new int[n + 1];
+        ranks_ = new int[n + 1];
+        for (int i = 0; i < parents_.length; ++i) {
+            parents_[i] = i;
+            ranks_[i] = 1;
+        }
+    }
+ 
+    public boolean Union(int u, int v) {
+        int pu = Find(u);
+        int pv = Find(v);
+        if (pu == pv) return false;
+
+        if (ranks_[pv] > ranks_[pu])
+            parents_[pu] = pv;           
+        else if (ranks_[pu] > ranks_[pv])
+            parents_[pv] = pu;
+        else {
+            parents_[pv] = pu;
+            ranks_[pu] += 1;
+        }
+
+        return true;
+    }
+ 
+    public int Find(int u) {
+        while (parents_[u] != u) {
+            parents_[u] = parents_[parents_[u]];
+            u = parents_[u];
+        }
+        return u;
+    }
+}
+// https://zxi.mytechroad.com/blog/graph/leetcode-547-friend-circles/
