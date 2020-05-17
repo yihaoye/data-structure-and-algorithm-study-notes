a428b1471e6e48955508121b2886ed1ae0894646
yihaoye <yihao.ye@vendhq.com>
2020-05-17 18:30:10 +1200

    other practices

--- Other Practices/2019 amz oa.java
+++ Other Practices/2019 amz oa.java
@@ -78,3 +78,40 @@ public class Solution {
 		return res;
 	}
 }
+
+
+
+// https://leetcode.com/discuss/interview-question/373202
+// Other's Solution:
+public class Solution {
+	public List<int[]> getPairs(List<int[]> a, List<int[]> b, int target) {
+		Collections.sort(a, (i,j) -> i[1] - j[1]);
+		Collections.sort(b, (i,j) -> i[1] - j[1]);
+		List<int[]> result = new ArrayList<>();
+		int max = Integer.MIN_VALUE;
+		int m = a.size();
+		int n = b.size();
+		int i =0;
+		int j =n-1;
+		while(i < m && j >= 0) {
+			int sum = a.get(i)[1] + b.get(j)[1];
+			if(sum > target) {
+				--j;
+			} else {
+				if(max <= sum) {
+					if(max < sum) {
+						max = sum;
+						result.clear();
+					}
+					result.add(new int[]{a.get(i)[0], b.get(j)[0]});
+					int index = j-1;
+					while(index >=0 && b.get(index)[1] == b.get(index+1)[1]) {
+						result.add(new int[]{a.get(i)[0], b.get(index--)[0]});
+					}
+				}
+				++i;
+			}
+		}
+		return result;
+	}
+}
