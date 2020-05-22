993e1f4b4639e5fb772a9be758e533d7d73bea7c
yihaoye <yihao.ye@vendhq.com>
2020-05-22 23:14:21 +1200

    other practices

--- Other Practices/08-05-2020 pre oas.java
+++ Other Practices/08-05-2020 pre oas.java
@@ -142,32 +142,58 @@ public class Solution {
 			map.get(link[1]).add(link[0]);
 		}
 		Set<Integer> set = new HashSet<>();
-		int[] low = new int[numRouters];
-		int[] ids = new int[numRouters]; // ids for IDs?
-		int[] parent = new int[numRouters]; 
-		Arrays.fill(ids, -1);
+		int[] low = new int[numRouters]; // low time of vertex: minimum of all the visit time of all the adjacent vertices which are reachable from the given vertex in the DFS traversal, i.e. back edge vertex (可绕过现有路径任何节点所到达的最原始的祖先/根) (https://www.youtube.com/watch?v=jFZsDDB0-vo)
+		int[] visit = new int[numRouters]; // visit for visited order in the program (数据将基于首先哪个节点被起始访问而不同，但是无论是起始于哪个节点，最后整个算法的结果是一样的)
+		int[] parent = new int[numRouters];  // parent of given vertex
+		Arrays.fill(visit, -1);
 		Arrays.fill(parent, -1);
 		for(int i=0; i<numRouters; i++) {
-			if(ids[i] == -1)
-				dfs(map, low, ids, parent, i, set);
+			if(visit[i] == -1)
+				dfs(map, low, visit, parent, i, set);
 		}
 		return new ArrayList<>(set);
 	}
 	
-	private static void dfs(Map<Integer, Set<Integer>> map, int[] low, int[] ids, int[] parent, int cur, Set<Integer> res) {
+	private static void dfs(Map<Integer, Set<Integer>> map, int[] low, int[] visit, int[] parent, int cur, Set<Integer> res) {
 		int children = 0;
-		ids[cur] = low[cur] = ++time;
-		for(int nei : map.get(cur)) { // nei for neighbour
-			if(ids[nei] == -1) {
+		visit[cur] = low[cur] = ++time;
+		for(int nei : map.get(cur)) { // nei stand for neighbour
+			if(visit[nei] == -1) {
 				children++;
 				parent[nei] = cur;
-				dfs(map, low, ids, parent, nei, res);
+				dfs(map, low, visit, parent, nei, res);
 				low[cur] = Math.min(low[cur], low[nei]);
-				if((parent[cur] == -1 && children > 1) || (parent[cur] != -1 && low[nei] >= ids[cur]))
+				if((parent[cur] == -1 && children > 1) || (parent[cur] != -1 && low[nei] >= visit[cur]))
 					res.add(cur);
 			}
-			else if(nei != parent[cur])
-				low[cur] = Math.min(low[cur], ids[nei]);
+			else if(nei != parent[cur]) // For avoiding a circle in between 2 points. Also the visited node may have a lower discovery count then the current low.
+				low[cur] = Math.min(low[cur], visit[nei]);
 		}
 	}
 }
+// 简易教程：https://www.youtube.com/watch?v=jFZsDDB0-vo
+// Related: https://www.geeksforgeeks.org/articulation-points-or-cut-vertices-in-a-graph/
+/*
+How to find all articulation points in a given graph?
+A simple approach is to one by one remove all vertices and see if removal of a vertex causes disconnected graph. Following are steps of simple approach for connected graph.
+
+1) For every vertex v, do following
+	a) Remove v from graph
+	b) See if the graph remains connected (We can either use BFS or DFS)
+	c) Add v back to the graph
+Time complexity of above method is O(V*(V+E)) for a graph represented using adjacency list. Can we do better?
+
+A O(V+E) algorithm to find all Articulation Points (APs)
+The idea is to use DFS (Depth First Search). In DFS, we follow vertices in tree form called DFS tree. In DFS tree, a vertex u is parent of another vertex v, if v is discovered by u (obviously v is an adjacent of u in graph). In DFS tree, a vertex u is articulation point if one of the following two conditions is true.
+1) u is root of DFS tree and it has at least two children.
+2) u is not root of DFS tree and it has a child v such that no vertex in subtree rooted with v has a back edge to one of the ancestors (in DFS tree) of u.
+Following figure shows same points as above with one additional point that a leaf in DFS Tree can never be an articulation point.
+![](https://media.geeksforgeeks.org/wp-content/uploads/ArticulationPoints.png)
+将无向连通图从某个顶点开始进行DFS遍历，遍历会得到一个DFS树(深度优先生成树)
+判断顶点u是否是关节点的原则:
+1、如果u是整颗DFS树的根，如果u至少有两个子女,那么u必然是关节点。
+2、DFS树中叶节点不是关节点
+3、非根顶点u(不整颗DFS树的根)不是关节点的充要条件是：它的任一子孙都可以沿某条路径绕过顶点u达到u的任一祖先w (w!=u),u不属于该路径(这条路径包括回边)
+ps:（回边是原本图中有的,在DFS数里没有的)
+针对判定原则里的1我们再定义一个变量children用于记录当前顶点的子女数，如果当前顶点是根结点(整棵树的根)，再定一个数组parent,parent[u]表示u的双亲是谁。
+*/
