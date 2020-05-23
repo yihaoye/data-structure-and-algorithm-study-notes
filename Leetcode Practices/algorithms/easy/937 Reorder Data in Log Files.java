1fb466f94dc9224c43460fa610034b1763cab872
yihaoye <yihao.ye@vendhq.com>
2020-05-23 23:04:15 +1200

    lc q937 round 2

--- Leetcode Practices/algorithms/easy/937 Reorder Data in Log Files.java
+++ Leetcode Practices/algorithms/easy/937 Reorder Data in Log Files.java
@@ -28,6 +28,36 @@ logs[i] is guaranteed to have an identifier, and a word after the identifier.
 
 
 
+// Other's Solution:
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        Comparator<String> myComp = new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                int s1SpaceIndex = s1.indexOf(' ');
+                int s2SpaceIndex = s2.indexOf(' ');
+                char s1FirstCharacter = s1.charAt(s1SpaceIndex+1);
+                char s2FirstCharacter = s2.charAt(s2SpaceIndex+1);
+                
+                if (s1FirstCharacter <= '9') {
+                    if (s2FirstCharacter <= '9') return 0;
+                    else return 1;
+                }
+                if (s2FirstCharacter <= '9') return -1;
+                
+                int preCompute = s1.substring(s1SpaceIndex+1).compareTo(s2.substring(s2SpaceIndex+1));
+                if (preCompute == 0) return s1.substring(0,s1SpaceIndex).compareTo(s2.substring(0,s2SpaceIndex));
+                return preCompute;
+            }
+        };
+        
+        Arrays.sort(logs, myComp);
+        return logs;
+    }
+}
+
+
+
 // My Solution:
 class Solution {
     public String[] reorderLogFiles(String[] logs) {
