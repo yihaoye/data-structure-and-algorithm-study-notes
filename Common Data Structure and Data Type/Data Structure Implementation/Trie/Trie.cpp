03ec1aa6ba70ff2842a984f592a82839e781ab1b
yihaoye <yihao.ye@vendhq.com>
2020-06-03 01:25:26 +1200

    trie data structure study

--- Common Data Structure and Data Type/Data Structure Implementation/Trie/Trie.cpp
+++ Common Data Structure and Data Type/Data Structure Implementation/Trie/Trie.cpp
@@ -1,13 +1,13 @@
 struct trie {
-  int nex[100000][26], cnt;
+  int next[100000][26], cnt;
   bool exist[100000];  // 该结点结尾的字符串是否存在
 
   void insert(char *s, int l) {  // 插入字符串
     int p = 0;
     for (int i = 0; i < l; i++) {
       int c = s[i] - 'a';
-      if (!nex[p][c]) nex[p][c] = ++cnt;  // 如果没有，就添加结点
-      p = nex[p][c];
+      if (!next[p][c]) next[p][c] = ++cnt;  // 如果没有，就添加结点
+      p = next[p][c];
     }
     exist[p] = 1;
   }
@@ -15,8 +15,8 @@ struct trie {
     int p = 0;
     for (int i = 0; i < l; i++) {
       int c = s[i] - 'a';
-      if (!nex[p][c]) return 0;
-      p = nex[p][c];
+      if (!next[p][c]) return 0;
+      p = next[p][c];
     }
     return exist[p];
   }
