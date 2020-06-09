14e36cce470bff52bd8eca95015942732707b1de
yihaoye <yihao.ye@vendhq.com>
2020-06-09 15:38:08 +1200

    lc q46 round 0

--- Leetcode Practices/algorithms/medium/46 Permutations.java
+++ Leetcode Practices/algorithms/medium/46 Permutations.java
@@ -45,3 +45,27 @@ For example, if the input num[] is {1,2,3}: First, add 1 into the initial List<L
 Then, 2 can be added in front or after 1. So we have to copy the List in answer (it's just {1}), add 2 in position 0 of {1}, then copy the original {1} again, and add 2 in position 1. Now we have an answer of {{2,1},{1,2}}. There are 2 lists in the current answer.
 Then we have to add 3. first copy {2,1} and {1,2}, add 3 in position 0; then copy {2,1} and {1,2}, and add 3 into position 1, then do the same thing for position 3. Finally we have 2*3=6 lists in answer, which is what we want.
 */
+
+
+
+// Other's Solution (Backtrack):
+class Solution {
+  public List<List<Integer>> permute(int[] nums) {
+    List<List<Integer>> res = new ArrayList<>();
+    backtrack(res, new ArrayList<>(), nums);
+    return res;
+  }
+
+  private void backtrack(List<List<Integer>> res, List<Integer> tempList, int[] nums){
+    if (tempList.size() == nums.length) {
+      res.add(new ArrayList<>(tempList));
+    } else {
+      for (int i = 0; i < nums.length; i++) { 
+          if (tempList.contains(nums[i])) continue; // element already exists, skip
+          tempList.add(nums[i]);
+          backtrack(res, tempList, nums);
+          tempList.remove(tempList.size() - 1);
+      }
+    }
+  } 
+}
