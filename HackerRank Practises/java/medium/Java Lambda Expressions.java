cde9dd47cc7a83a802717837c04b336a70d8c2b4
yihaoye <yihao.ye@vendhq.com>
2020-10-04 16:25:05 +1300

    hackerrank java medium

--- HackerRank Practises/java/medium/Java Lambda Expressions.java
+++ HackerRank Practises/java/medium/Java Lambda Expressions.java
@@ -35,3 +35,58 @@ PALINDROME
 ODD
 COMPOSITE
 */
+
+
+
+// other's solution:
+import java.io.*;
+import java.util.*;
+interface PerformOperation {
+ boolean check(int a);
+}
+class MyMath {
+ public static boolean checker(PerformOperation p, int num) {
+  return p.check(num);
+ }
+ public static PerformOperation isOdd() {
+  return num -> num%2 != 0;
+ }
+ public static PerformOperation isPrime() {
+  return num -> java.math.BigInteger.valueOf(num).isProbablePrime(1);
+ }
+ public static PerformOperation isPalindrome() {
+  return num -> Integer.toString(num).equals(new StringBuilder(Integer.toString(num)).reverse().toString());
+ }
+}
+
+public class Solution {
+
+ public static void main(String[] args) throws IOException {
+  MyMath ob = new MyMath();
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  int T = Integer.parseInt(br.readLine());
+  PerformOperation op;
+  boolean ret = false;
+  String ans = null;
+  while (T--> 0) {
+   String s = br.readLine().trim();
+   StringTokenizer st = new StringTokenizer(s);
+   int ch = Integer.parseInt(st.nextToken());
+   int num = Integer.parseInt(st.nextToken());
+   if (ch == 1) {
+    op = ob.isOdd();
+    ret = ob.checker(op, num);
+    ans = (ret) ? "ODD" : "EVEN";
+   } else if (ch == 2) {
+    op = ob.isPrime();
+    ret = ob.checker(op, num);
+    ans = (ret) ? "PRIME" : "COMPOSITE";
+   } else if (ch == 3) {
+    op = ob.isPalindrome();
+    ret = ob.checker(op, num);
+    ans = (ret) ? "PALINDROME" : "NOT PALINDROME";
+   }
+   System.out.println(ans);
+  }
+ }
+}
