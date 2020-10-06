f7ad855feff5333d4707a505203450d13675e475
yihaoye <yihao.ye@vendhq.com>
2020-10-07 01:36:01 +1300

    lc q953 round 1

---
+++ Leetcode Practices/algorithms/easy/953 Verifying an Alien Dictionary.java
@@ -0,0 +1,67 @@
+/*
+In an alien language, surprisingly they also use english lowercase letters, but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
+
+Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given words are sorted lexicographicaly in this alien language.
+
+ 
+
+Example 1:
+
+Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+Output: true
+Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
+Example 2:
+
+Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+Output: false
+Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
+Example 3:
+
+Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+Output: false
+Explanation: The first three characters "app" match, and the second string is shorter (in size.) According to lexicographical rules "apple" > "app", because 'l' > '∅', where '∅' is defined as the blank character which is less than any other character (More info).
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 20
+order.length == 26
+All characters in words[i] and order are English lowercase letters.
+*/
+
+
+
+// My Solution:
+class Solution {
+    public boolean isAlienSorted(String[] words, String order) {
+        int i = 0;
+        int[][] indexsArr = new int[words.length][20];
+        for (String word : words) {
+            toIndexs(word, order, indexsArr, i);
+            i++;
+        }
+        for (int j=0; j<i-1; j++) {
+            if (!compareIndexs(indexsArr[j], indexsArr[j+1])) return false;
+        }
+        return true;
+    }
+    
+    public void toIndexs(String word, String order, int[][] indexsArr, int i) {
+        char[] wordArr = word.toCharArray();
+        for (int j=0; j<wordArr.length; j++) {
+            indexsArr[i][j] = order.indexOf(wordArr[j]) + 1;
+        }
+    }
+    
+    public boolean compareIndexs(int[] indexsA, int[] indexsB) {
+        int i = 0;
+        while (indexsA[i] != 0 && indexsB[i] != 0 && i<20) {
+            if (indexsA[i] < indexsB[i]) return true;
+            if (indexsA[i] > indexsB[i]) return false;
+            i++;
+        }
+        if (i == 20 || indexsA[i] == indexsB[i] || indexsB[i] != 0) return true;
+        else return false;
+    }
+}
