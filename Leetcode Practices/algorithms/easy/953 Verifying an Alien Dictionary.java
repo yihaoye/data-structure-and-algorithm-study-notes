75cde2d528aa89ceda028a20fc49d39999b04af8
yihaoye <yihao.ye@vendhq.com>
2020-10-07 14:35:24 +1300

    lc q953 round 2

--- Leetcode Practices/algorithms/easy/953 Verifying an Alien Dictionary.java
+++ Leetcode Practices/algorithms/easy/953 Verifying an Alien Dictionary.java
@@ -65,3 +65,35 @@ class Solution {
         else return false;
     }
 }
+
+
+
+// Other's Solution: (Complexity - Time O(NS) Space O(1))
+class Solution {
+    int[] mapping = new int[26];
+    public boolean isAlienSorted(String[] words, String order) {
+        for (int i = 0; i < order.length(); i++)
+            mapping[order.charAt(i) - 'a'] = i;
+        for (int i = 1; i < words.length; i++)
+            if (bigger(words[i - 1], words[i]))
+                return false;
+        return true;
+    }
+
+    boolean bigger(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        for (int i = 0; i < n && i < m; ++i)
+            if (s1.charAt(i) != s2.charAt(i))
+                return mapping[s1.charAt(i) - 'a'] > mapping[s2.charAt(i) - 'a'];
+        return n > m;
+    }
+}
+/*
+Build a transform mapping from order,
+Find all alien words with letters in normal order.
+
+For example, if we have order = "xyz..."
+We can map the word "xyz" to "abc" or "123"
+
+Then we check if all words are in sorted order.
+*/
