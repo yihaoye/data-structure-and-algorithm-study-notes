9ad84f51c4e4db803423805ca81ee7ee9ba0675d
yihaoye <yihao.ye@vendhq.com>
2020-10-10 21:22:44 +1300

    lc q706

--- Leetcode Practices/algorithms/easy/706 Design HashMap.java
+++ Leetcode Practices/algorithms/easy/706 Design HashMap.java
@@ -61,6 +61,30 @@ class MyHashMap {
  * int param_2 = obj.get(key);
  * obj.remove(key);
  */
+// Above can be optimized in following:
+class MyHashMap {
+    int[] map;
+    
+    /** Initialize your data structure here. */
+    public MyHashMap() {
+        map = new int[1000000];
+    }
+    
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        map[key] = value + 1;
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+        return map[key] - 1;
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+        map[key] = 0;
+    }
+}
 
 
 
