76f41978f0ed001e6575e0fee526382e2a6c8055
yihaoye <yihao.ye@vendhq.com>
2020-10-18 10:48:47 +1300

    tool sets java array equals

--- Tool Sets/Arrays.java
+++ Tool Sets/Arrays.java
@@ -1,3 +1,9 @@
 // 数组全填充
 int[] arr = new int[10000];
 Arrays.fill(arr, -1);
+
+// 多维数组比较是否相等
+String[][] arr1 = new String[][]{{"a", "a"}};
+String[][] arr2 = new String[][]{{"a", "a"}};
+Arrays.equals(arr1, arr2); // Return false - comparing two dimensional arrays, which means the elements of these arrays are themselves arrays. Therefore, when the elements are compared (using Object's equals), false is returned, since Object's equals compares Object references.
+Arrays.deepEquals(arr1, arr2); // Returns true if the two specified arrays are deeply equal to one another. Unlike the equals(Object [], Object []) method, this method is appropriate for use with nested arrays of arbitrary depth.
