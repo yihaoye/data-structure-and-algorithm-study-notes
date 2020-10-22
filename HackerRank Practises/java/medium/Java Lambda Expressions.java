58b6fe7af71fcd4387c702550247388a509c457b
yihaoye <yihao.ye@vendhq.com>
2020-10-23 11:59:17 +1300

    java 8 lambda

--- HackerRank Practises/java/medium/Java Lambda Expressions.java
+++ HackerRank Practises/java/medium/Java Lambda Expressions.java
@@ -200,3 +200,74 @@ public class RecursiveFibonacci {
   }
 }
 // 将 Fibonacci 序列中的最后两个元素求和来产生下一个元素。
+
+// 方法引用
+// 方法引用组成：类名或对象名，后面跟 :: ，然后跟方法名称。
+// functional/MethodReferences.java
+import java.util.*;
+
+interface Callable { // [1]
+  void call(String s);
+}
+
+class Describe {
+  void show(String msg) { // [2]
+    System.out.println(msg);
+  }
+}
+
+public class MethodReferences {
+  static void hello(String name) { // [3]
+    System.out.println("Hello, " + name);
+  }
+  static class Description {
+    String about;
+    Description(String desc) { about = desc; }
+    void help(String msg) { // [4]
+      System.out.println(about + " " + msg);
+    }
+  }
+  static class Helper {
+    static void assist(String msg) { // [5]
+      System.out.println(msg);
+    }
+  }
+  public static void main(String[] args) {
+    Describe d = new Describe();
+    Callable c = d::show; // [6]
+    c.call("call()"); // [7]
+
+    c = MethodReferences::hello; // [8]
+    c.call("Bob");
+
+    c = new Description("valuable")::help; // [9]
+    c.call("information");
+
+    c = Helper::assist; // [10]
+    c.call("Help!");
+  }
+}
+/*
+[1] 我们从单一方法接口开始（同样，你很快就会了解到这一点的重要性）。
+
+[2] show() 的签名（参数类型和返回类型）符合 Callable 的 call() 的签名。
+
+[3] hello() 也符合 call() 的签名。
+
+[4] help() 也符合，它是静态内部类中的非静态方法。
+
+[5] assist() 是静态内部类中的静态方法。
+
+[6] 我们将 Describe 对象的方法引用赋值给 Callable ，它没有 show() 方法，而是 call() 方法。 但是，Java 似乎接受用这个看似奇怪的赋值，因为方法引用符合 Callable 的 call() 方法的签名。
+
+[7] 我们现在可以通过调用 call() 来调用 show()，因为 Java 将 call() 映射到 show()。
+
+[8] 这是一个静态方法引用。
+
+[9] 这是 [6] 的另一个版本：对已实例化对象的方法的引用，有时称为绑定方法引用。
+
+[10] 最后，获取静态内部类中静态方法的引用与 [8] 中通过外部类引用相似。
+*/
+
+// 在 Lambda 表达式当中不允许声明一个与局部变量同名的参数或者局部变量。
+
