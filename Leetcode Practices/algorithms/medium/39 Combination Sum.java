663e7fc1cc899abbd301581bc72be6ae0241e8b4
yihaoye <yihao.ye@vendhq.com>
2020-12-12 01:06:51 +1300

    lc q39 round 2

--- Leetcode Practices/algorithms/medium/39 Combination Sum.java
+++ Leetcode Practices/algorithms/medium/39 Combination Sum.java
@@ -78,3 +78,27 @@ class Solution {
         return;
     }
 }
+
+
+
+// Other's Solution:
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, target, 0);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start){
+        if (remain < 0) return;
+        else if (remain == 0) list.add(new ArrayList<>(tempList));
+        else { 
+            for (int i = start; i < nums.length; i++) {
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
