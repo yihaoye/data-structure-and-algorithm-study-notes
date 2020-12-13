31624d34547005431662fe5116b212a1caf0bd31
yihaoye <yihao.ye@vendhq.com>
2020-12-13 22:48:14 +1300

    lc q131 round 2

--- Leetcode Practices/algorithms/medium/131 Palindrome Partitioning.java
+++ Leetcode Practices/algorithms/medium/131 Palindrome Partitioning.java
@@ -56,3 +56,34 @@ class Solution {
         return str.equals(sbr.reverse().toString());
     }
 }
+
+
+
+// Other's Solution:
+class Solution {
+    public List<List<String>> partition(String s) {
+       List<List<String>> list = new ArrayList<>();
+       backtrack(list, new ArrayList<>(), s, 0);
+       return list;
+    }
+
+    public void backtrack(List<List<String>> list, List<String> tempList, String s, int start){
+       if(start == s.length())
+          list.add(new ArrayList<>(tempList));
+       else{
+          for(int i = start; i < s.length(); i++){
+             if(isPalindrome(s, start, i)){
+                tempList.add(s.substring(start, i + 1));
+                backtrack(list, tempList, s, i + 1);
+                tempList.remove(tempList.size() - 1);
+             }
+          }
+       }
+    }
+
+    public boolean isPalindrome(String s, int low, int high){
+       while(low < high)
+          if(s.charAt(low++) != s.charAt(high--)) return false;
+       return true;
+    } 
+}
