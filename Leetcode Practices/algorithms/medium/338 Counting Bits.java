fd9770585effcaf7a368e163e433c40c00975ace
yihaoye <yihao.ye@vendhq.com>
2020-12-14 01:14:48 +1300

    lc q338 round 2

--- Leetcode Practices/algorithms/medium/338 Counting Bits.java
+++ Leetcode Practices/algorithms/medium/338 Counting Bits.java
@@ -47,3 +47,23 @@ class Solution {
         return count[0];
     }
 }
+
+
+
+// Other's Solution:
+// An easy recurrence for this problem is f[i] = f[i / 2] + i % 2.
+class Solution {
+    public int[] countBits(int num) {
+        int[] f = new int[num + 1];
+        for (int i=1; i<=num; i++) f[i] = f[i >> 1] + (i & 1); // 也可以写 for (int i=1; i<=num; i++) f[i] = f[i/2] + (i%2);
+        return f;
+    }
+}
+/*
+https://leetcode.com/problems/counting-bits/discuss/79539/Three-Line-Java-Solution/84518
+Explaination.
+Take number X for example, 10011001.
+Divide it in 2 parts:
+<1>the last digit ( 1 or 0, which is " i&1 ", equivalent to " i%2 " )
+<2>the other digits ( the number of 1, which is " f[i >> 1] ", equivalent to " f[i/2] " )
+*/
