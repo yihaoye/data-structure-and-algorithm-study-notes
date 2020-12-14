5447a4609b6e26ac9d6f6f8d211eecafbc938105
yihaoye <yihao.ye@vendhq.com>
2020-12-14 20:36:11 +1300

    composite pattern

--- Leetcode Practices/object oriented design/design pattern/Adapter.java
+++ Leetcode Practices/object oriented design/design pattern/Adapter.java
@@ -1,39 +1,39 @@
+public class Task implements Callable<Long> {
+    private long num;
+    public Task(long num) {
+        this.num = num;
+    }
+
+    public Long call() throws Exception {
+        long r = 0;
+        for (long n = 1; n <= this.num; n++) {
+            r = r + n;
+        }
+        System.out.println("Result: " + r);
+        return r;
+    }
+}
+
+public class RunnableAdapter implements Runnable {
+    // 引用待转换接口:
+    private Callable<?> callable;
+
+    public RunnableAdapter(Callable<?> callable) {
+        this.callable = callable;
+    }
+
+    // 实现指定接口:
+    public void run() {
+        // 将指定接口调用委托给转换接口调用:
+        try {
+            callable.call();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
 public class Main {
-    public class Task implements Callable<Long> {
-        private long num;
-        public Task(long num) {
-            this.num = num;
-        }
-
-        public Long call() throws Exception {
-            long r = 0;
-            for (long n = 1; n <= this.num; n++) {
-                r = r + n;
-            }
-            System.out.println("Result: " + r);
-            return r;
-        }
-    }
-
-    public class RunnableAdapter implements Runnable {
-        // 引用待转换接口:
-        private Callable<?> callable;
-
-        public RunnableAdapter(Callable<?> callable) {
-            this.callable = callable;
-        }
-
-        // 实现指定接口:
-        public void run() {
-            // 将指定接口调用委托给转换接口调用:
-            try {
-                callable.call();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
     public void main(String[] args) {
         Callable<Long> callable = new Task(123450000L);
         Thread thread = new Thread(new RunnableAdapter(callable));
---
+++ Leetcode Practices/object oriented design/design pattern/Composite.java
@@ -0,0 +1,113 @@
+/*
+将对象组合成树形结构以表示“部分-整体”的层次结构，使得用户对单个对象和组合对象的使用具有一致性。
+组合模式（Composite）经常用于树形结构，为了简化代码，使用 Composite 可以把一个叶子节点与一个父节点统一起来处理。
+来看一个具体的例子。在 XML 或 HTML 中，从根节点开始，每个节点都可能包含任意个其他节点，这些层层嵌套的节点就构成了一颗树。
+要以树的结构表示 XML，我们可以先抽象出节点类型 Node
+*/
+
+public interface Node {
+    // 添加一个节点为子节点:
+    Node add(Node node);
+    // 获取子节点:
+    List<Node> children();
+    // 输出为XML:
+    String toXml();
+}
+
+public class ElementNode implements Node {
+    private String name;
+    private List<Node> list = new ArrayList<>();
+
+    public ElementNode(String name) {
+        this.name = name;
+    }
+
+    public Node add(Node node) {
+        list.add(node);
+        return this;
+    }
+
+    public List<Node> children() {
+        return list;
+    }
+
+    public String toXml() {
+        String start = "<" + name + ">\n";
+        String end = "</" + name + ">\n";
+        StringJoiner sj = new StringJoiner("", start, end);
+        list.forEach(node -> {
+            sj.add(node.toXml() + "\n");
+        });
+        return sj.toString();
+    }
+}
+
+public class TextNode implements Node {
+	private String text;
+
+	public TextNode(String text) {
+		this.text = text;
+	}
+
+	public Node add(Node node) {
+		throw new UnsupportedOperationException();
+	}
+
+	public List<Node> children() {
+		return List.of();
+	}
+
+	public String toXml() {
+		return text;
+	}
+}
+
+public class CommentNode implements Node {
+	private String text;
+
+	public CommentNode(String text) {
+		this.text = text;
+	}
+
+	public Node add(Node node) {
+		throw new UnsupportedOperationException();
+	}
+
+	public List<Node> children() {
+		return List.of();
+	}
+
+	public String toXml() {
+		return "<!-- " + text + " -->";
+	}
+}
+
+public class Main {
+    public void main() {
+        Node root = new ElementNode("school");
+        root.add(new ElementNode("classA")
+                .add(new TextNode("Tom"))
+                .add(new TextNode("Alice")));
+        root.add(new ElementNode("classB")
+                .add(new TextNode("Bob"))
+                .add(new TextNode("Grace"))
+                .add(new CommentNode("comment...")));
+        System.out.println(root.toXml());
+    }
+}
+
+/*
+最后通过 root 节点输出的 XML 如下：
+
+<school>
+<classA>
+Tom
+Alice
+</classA>
+<classB>
+Bob
+Grace
+<!-- comment... -->
+</classB>
+</school>
+*/
