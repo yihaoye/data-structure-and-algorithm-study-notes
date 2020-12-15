03e0ccd0bc08c946a8e69aef16770ddb24677640
yihaoye <yihao.ye@vendhq.com>
2020-12-16 02:37:10 +1300

    java Asynchronous

---
+++ Computer System Layer/并发与并行(Java)/Defog/Asynchronous.java
@@ -0,0 +1,24 @@
+// https://www.youtube.com/watch?v=rUbZcRgaSY4
+
+// wrong example:
+for (Integer id : employeeIds) {
+    // Step 1: fetch employee details from db
+    Future<Employee> future = service.submit(new EmployeeFetcher(id));
+    Employee emp = future.get(); // blocking
+    // Step 2: fetch employee tax rate from rest service
+    Future<TaxRate> rateFuture = service.submit(new TaxRateFetcher(emp));
+    TaxRate taxRate = rateFuture.get(); // blocking
+    // Step 3: calculate current year tax
+    BigDecimal tax = calculateTax(emp, taxRate);
+    // Step 4: send email to employee using rest service
+    service.submit(new SendEmail(emp, tax));
+}
+
+// correct example:
+// callback chaining, similar to JavaScript
+for (Integer id : employeeIds) {
+    CompletableFuture.supplyAsync(() -> fetchEmployee(id))
+        .thenApplyAsync(employee -> fetchTaxRate(employee))
+        .thenApplyAsync(taxRate -> calculateTax(taxRate))
+        .thenAcceptAsync(taxValue -> sendEmail(taxValue));
+}
