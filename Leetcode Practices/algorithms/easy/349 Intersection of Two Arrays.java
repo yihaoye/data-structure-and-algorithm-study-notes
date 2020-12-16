6737ff0cecddd756eb0fa4ba51e6710afc239749
yihaoye <yihao.ye@vendhq.com>
2020-12-16 19:18:46 +1300

    lc q349 round 2

--- Leetcode Practices/algorithms/easy/349 Intersection of Two Arrays.java
+++ Leetcode Practices/algorithms/easy/349 Intersection of Two Arrays.java
@@ -18,6 +18,7 @@ The result can be in any order.
 
 
 // My Solution:
+// Time complexity: O(n)
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         // double sets
@@ -34,5 +35,77 @@ class Solution {
             i++;
         }
         return res;
+    }
+}
+
+
+
+// Other's Solution:
+// Sort both arrays, use two pointers
+// Time complexity: O(nlogn)
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] result = new int[set.size()];
+        int k = 0;
+        for (Integer num : set) {
+            result[k++] = num;
+        }
+        return result;
+    }
+}
+
+
+
+// Other's Solution:
+// Binary search
+// Time complexity: O(nlogn)
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums2);
+        for (Integer num : nums1) {
+            if (binarySearch(nums2, num)) {
+                set.add(num);
+            }
+        }
+        int i = 0;
+        int[] result = new int[set.size()];
+        for (Integer num : set) {
+            result[i++] = num;
+        }
+        return result;
+    }
+    
+    public boolean binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
     }
 }
