862ff0e21eb4bed6b3bcc9ab442136f02dd37d42
yihaoye <yihao.ye@vendhq.com>
2020-12-19 00:31:39 +1300

    lc q628 round 2

--- Leetcode Practices/algorithms/easy/628 Maximum Product of Three Numbers.java
+++ Leetcode Practices/algorithms/easy/628 Maximum Product of Three Numbers.java
@@ -34,3 +34,47 @@ class Solution {
         return nums[len-1]*nums[0]*nums[1];
     }
 }
+
+
+
+// Other's Solution (Actually same with My Solution, worth mention here to easy understand Other's Solution 2):
+class Solution {
+    public int maximumProduct(int[] nums) {
+            
+        Arrays.sort(nums);
+        //One of the Three Numbers is the maximum value in the array.
+
+        int a = nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
+        int b = nums[0] * nums[1] * nums[nums.length - 1];
+        return a > b ? a : b;
+    }
+}
+
+
+
+// Other's Solution 2 (O(N)，原理与上面一样，只是一次 for 循环就求出那几个最值，不必排序):
+class Solution {
+    public int maximumProduct(int[] nums) {
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE, min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        for (int n : nums) {
+            if (n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (n > max3) {
+                max3 = n;
+            }
+
+            if (n < min1) {
+                min2 = min1;
+                min1 = n;
+            } else if (n < min2) {
+                min2 = n;
+            }
+        }
+        return Math.max(max1*max2*max3, max1*min1*min2);
+    }
+}
