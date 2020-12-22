cd451f18e898f908689bc8574cae314b199060ed
yihaoye <yihao.ye@vendhq.com>
2020-12-22 21:38:21 +1300

    java annotations

--- HackerRank Practises/java/medium/Java Annotations.java
+++ HackerRank Practises/java/medium/Java Annotations.java
@@ -103,4 +103,42 @@ Java 的注解可以分为三类：
 如果只写注解，相当于全部使用默认值。
 */
 
+/*
+Java 使用 @interface 定义注解：
 
+可定义多个参数和默认值，核心参数使用 value 名称；
+
+元注解
+有一些注解可以修饰其他注解，这些注解就称为元注解（meta annotation）。Java 标准库已经定义了一些元注解，开发者只需要使用元注解，通常不需要自己去编写元注解。
+	@Target - 最常用的元注解是 @Target。使用 @Target 可以定义 Annotation 能够被应用于源码的哪些位置：
+		类或接口：ElementType.TYPE；
+		字段：ElementType.FIELD；
+		方法：ElementType.METHOD；
+		构造方法：ElementType.CONSTRUCTOR；
+		方法参数：ElementType.PARAMETER。
+	@Retention - 另一个重要的元注解 @Retention 定义了 Annotation 的生命周期（如果 @Retention 不存在，则该 Annotation 默认为 CLASS）：
+		仅编译期：RetentionPolicy.SOURCE；
+		仅 class 文件：RetentionPolicy.CLASS；
+		运行期：RetentionPolicy.RUNTIME。
+	@Repeatable - 使用 @Repeatable 这个元注解可以定义 Annotation 是否可重复（这个注解应用不是特别广泛）。
+	@Inherited - 使用 @Inherited 定义子类是否可继承父类定义的 Annotation。@Inherited 仅针对 @Target(ElementType.TYPE) 类型的 annotation 有效，并且仅针对 class 的继承，对 interface 的继承无效。
+
+
+定义 Annotation 的步骤：第一步，用 @interface 定义注解；第二步，添加参数、默认值；第三步，用元注解配置注解；
+必须设置 @Target 来指定 Annotation 可以应用的范围；
+应当设置 @Retention(RetentionPolicy.RUNTIME) 便于运行期读取该 Annotation。
+*/
+public @interface Report {
+    int type() default 0;
+    String level() default "info";
+    String value() default "";
+}
+// 注解的参数类似无参数方法，可以用 default 设定一个默认值（强烈推荐）。最常用的参数应当命名为 value。
+
+/*
+可以在运行期通过反射读取 RUNTIME 类型的注解，注意千万不要漏写 @Retention(RetentionPolicy.RUNTIME)，否则运行期无法读取到该注解。
+
+可以通过程序处理注解来实现相应的功能：
+	对 JavaBean 的属性值按规则进行检查；
+	JUnit 会自动运行 @Test 标记的测试方法。
+*/
