ac43c6b065439ea456d40781e10660d7615680c7
yihaoye <yihao.ye@vendhq.com>
2020-12-26 11:46:29 +1300

    lc q219 round 2

--- Leetcode Practices/algorithms/easy/219 Contains Duplicate II.java
+++ Leetcode Practices/algorithms/easy/219 Contains Duplicate II.java
@@ -31,3 +31,21 @@ class Solution {
         return false;
     }
 }
+
+
+
+// Other's Solution:
+public boolean containsNearbyDuplicate(int[] nums, int k) {
+    Set<Integer> set = new HashSet<Integer>();
+    for (int i = 0; i < nums.length; i++) {
+        if (i > k) set.remove(nums[i-k-1]); // remove element if its distance to nums[i] is not lesser than k
+        if (!set.add(nums[i])) return true; // because all still existed elements is closer than k distance to the num[i], therefore if the add() return false, it means there's a same value element already existed within the distance k, therefore return true.
+    }
+    return false;
+}
+/*
+It iterates over the array using a sliding window. The front of the window is at i, the rear of the window is k steps back. 
+The elements within that window are maintained using a Set. 
+While adding new element to the set, if add() returns false, it means the element already exists in the set. 
+At that point, we return true. If the control reaches out of for loop, it means that inner return true never executed, meaning no such duplicate element was found.
+*/
