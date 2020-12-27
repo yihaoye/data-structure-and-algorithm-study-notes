afb28539f085497a4e14de5d1a92f92da9476988
yihaoye <yihao.ye@vendhq.com>
2020-12-27 15:45:34 +1300

    lc q1188 round 0

--- Leetcode Practices/algorithms/medium/1188 Design Bounded Blocking Queue.java
+++ Leetcode Practices/algorithms/medium/1188 Design Bounded Blocking Queue.java
@@ -116,3 +116,59 @@ class BoundedBlockingQueue {
 		}
     }
 }
+
+
+
+// Other's Solution 2:
+class BoundedBlockingQueue {
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition full = lock.newCondition();
+    private Condition empty = lock.newCondition();
+    private int[] queue;
+    private int tail = 0;
+    private int head = 0;
+    private int size = 0;
+    public BoundedBlockingQueue(int capacity) {
+        queue = new int[capacity];
+    }
+    
+    public void enqueue(int element) throws InterruptedException {
+		lock.lock();
+        try {
+            while(size == queue.length) {
+                full.await();
+            }
+            queue[tail++] = element;
+            tail %= queue.length;
+            size++;
+            empty.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+    
+    public int dequeue() throws InterruptedException {
+		lock.lock();
+        try {
+            while(size == 0) {
+                empty.await();
+            }
+            int res = queue[head++];
+            head %= queue.length;
+            size--;
+            full.signal();
+            return res;
+        } finally {
+            lock.unlock();
+        }
+    }
+    
+    public int size() throws InterruptedException {
+        lock.lock();
+		try {
+			return this.size;
+		} finally {
+			lock.unlock();
+		}
+    }
+}
