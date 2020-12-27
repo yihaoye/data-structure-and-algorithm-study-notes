e5b1973c4c0db905459bdbe61abcc25f48d887c5
yihaoye <yihao.ye@vendhq.com>
2020-12-27 20:51:32 +1300

    lc q311 round 2

--- Leetcode Practices/algorithms/medium/311 Sparse Matrix Multiplication.java
+++ Leetcode Practices/algorithms/medium/311 Sparse Matrix Multiplication.java
@@ -34,7 +34,7 @@ Constraints:
 
 
 
-// My Solution (pure for loop):
+// My Solution (单纯的 for loop 计算模拟矩阵相乘逻辑):
 class Solution {
     public int[][] multiply(int[][] A, int[][] B) {
         int aH = A.length, aW = A[0].length, bW = B[0].length;
@@ -51,3 +51,26 @@ class Solution {
         return res;
     }
 }
+
+
+
+// Other's Solution (基本与上面解法一样，只是优化了不必要的与 0 元素相乘):
+class Solution {
+    public int[][] multiply(int[][] A, int[][] B) {
+        int aH = A.length, aW = A[0].length, bW = B[0].length;
+        int[][] res = new int[aH][bW];
+        
+        for (int ai=0; ai<aH; ai++) {
+            for (int aj=0; aj<aW; aj++) {
+                if (A[ai][aj] != 0) { // 优化
+                    for (int bj=0; bj<bW; bj++) {
+                        if (B[aj][bj] != 0) res[ai][bj] += A[ai][aj]*B[aj][bj]; // 优化
+                    }
+                }
+            }
+        }
+        
+        return res;
+    }
+}
+
