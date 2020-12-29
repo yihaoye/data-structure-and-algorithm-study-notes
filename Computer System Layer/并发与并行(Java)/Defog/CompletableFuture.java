e846ae04d8ef2230004095412063b9dc655a542b
yihaoye <yihao.ye@vendhq.com>
2020-12-30 00:08:29 +1300

    CompletableFuture

--- Computer System Layer/并发与并行(Java)/Defog/CompletableFuture.java
+++ Computer System Layer/并发与并行(Java)/Defog/CompletableFuture.java
@@ -1,6 +1,13 @@
 // https://www.youtube.com/watch?v=ImtZgX1nmr8
 
 
+/*
+CompletableFuture used for:
+
+perform possible asynchronous (non-blocking) computation and trigger dependant computations which could also be asynchronous.
+*/
+
+
 public static void main(String[] args) {
     
     ExecutorService service = new Executors.newFixedThreadPool(10);
@@ -28,17 +35,19 @@ public static void main(String[] args) {
 public static void main(String[] args) {
 
     for (int i=0; i<100; i++) {
-        CompletableFuture.supplyAsync(() -> getOrder())
+        CompletableFuture.supplyAsync(() -> getOrder()) // first operation has to be async always, in this case, all the subsequent operations are handled by same thread
             .thenApply(order -> enrich(order))
             .thenApply(order -> performPayment(order))
             .thenApply(order -> dispatch(order))
             .thenAccept(order -> sendEmail(order));
+        // 上面的代码没有指定自定义的线程池，则会使用默认内置的 ForkJoinPool.commonPool()
 
+        /* 下面这段代码是指定自定义线程池的情况 */
         // ExecutorService cpuBound = new Executors.newFixedThreadPool(4);
         // ExecutorService ioBound = new Executors.newCachedThreadPool();
-        // CompletableFuture.supplyAsync(() -> getOrder(), ioBound)
-        //     .thenApplyAsync(order -> enrich(order), cpuBound)
-        //     .thenApplyAsync(order -> performPayment(order), ioBound)
+        // CompletableFuture.supplyAsync(() -> getOrder(), ioBound) // specify own/customized thread pool when with sec paramter ExecutorService
+        //     .thenApplyAsync(order -> enrich(order), cpuBound) // subsequent operation not handled by same thread when using thenApplyAsync()
+        //     .thenApplyAsync(order -> performPayment(order), ioBound) // specify own/customized thread pool when with sec paramter ExecutorService
         //     .exceptionally(e -> new FailedOrder())
         //     .thenApplyAsync(order -> dispatch(order))
         //     .thenAccept(order -> sendEmail(order))
