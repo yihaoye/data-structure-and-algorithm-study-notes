82a99be318d98397f77e022a60dc27419fbc3817
yihaoye <yihao.ye@vendhq.com>
2021-06-08 00:23:20 +1200

    leetcode q10 round 0 comments

--- Leetcode Practices/algorithms/hard/10 Regular Expression Matching.java
+++ Leetcode Practices/algorithms/hard/10 Regular Expression Matching.java
@@ -57,17 +57,16 @@ class Solution {
         // initialization: 
 		// dp[0][0] = true, since empty string matches empty pattern
 		dp[0][0] = true;
-
         // https://youtu.be/ym1zpF7sInc?t=101
-        for (int j = 2; j < p.length(); j++) {
-            if (p.charAt(j) == '*') {
+        for (int j = 2; j <= p.length(); j++) {
+            if (p.charAt(j-1) == '*') { // 注意是 p.charAt(j-1)，而不是 p.charAt(j)，因为 dp 数组的 j 的 0 为 null 其实为字符串第 -1 位
                 dp[0][j] = dp[0][j-2];
             }
         }
 
         for (int i = 0; i < s.length(); i++) {
             for (int j = 0; j < p.length(); j++) {
-                if (p.charAt(j) == s.charAt(i) || p.charAt(j) == '.') {
+                if (p.charAt(j) == s.charAt(i) || p.charAt(j) == '.') { // 这里的 s.charAt(i) 和 p.charAt(j) 正好对应 dp[i+1][x] 和 dp[x][j+1]，原因如上 - dp 数组的 i 和 j 的 0 为 null 其实为字符串第 -1 位
                     dp[i+1][j+1] = dp[i][j];
                 } else if (p.charAt(j) == '*') {
                     if (p.charAt(j-1) == s.charAt(i) || p.charAt(j-1) == '.') {
@@ -92,5 +91,5 @@ dp[i][j] means whether [0...j] of p regex match [0...i] of s.
                             dp[i][j] = dp[i][j-2]  // in this case, a* only counts as empty (e.g. Example4's "c*" : Input: s = "aab", p = "c*a*b")
                     Or (逻辑或) If p.charAt(j-1) == s.charAt(i) or p.charAt(j-1) == '.' :
                             dp[i][j] = dp[i-1][j]   // in this case, a* counts as single - multiple a. (https://youtu.be/ym1zpF7sInc?t=302)
-                            // Leetcode 最高票题解还检查并 or dp[i][j-1]，实际上不需要，因为只要 dp[i-1][j] == true 则 dp[i][j-1] 必为 true 反之未必？
+                            // Leetcode 最高票题解还检查并 or dp[i][j-1]，实际上不需要，因为只要 dp[i][j-1] == true 则 dp[i-1][j] 必为 true，但是反之未必 (参考 https://youtu.be/ym1zpF7sInc?t=419 的 dp[s.length()-1][p.length()-2] 与 dp[s.length()-2][p.length()-1])
 */
