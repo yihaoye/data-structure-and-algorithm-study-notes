f11119f1d0c03f365082177c9ba0726eab49b653
yihaoye <yihao.ye@vendhq.com>
2021-06-07 23:46:58 +1200

    leetcode q10 round 0 comments

--- Leetcode Practices/algorithms/hard/10 Regular Expression Matching.java
+++ Leetcode Practices/algorithms/hard/10 Regular Expression Matching.java
@@ -46,7 +46,7 @@ It is guaranteed for each appearance of the character '*', there will be a previ
 
 
 
-// Other's Solution (DP):
+// Other's Solution (DP - 时间复杂度 O(N*M)):
 class Solution {
     public boolean isMatch(String s, String p) {
         // corner case
@@ -55,16 +55,10 @@ class Solution {
         boolean[][] dp = new boolean[s.length()+1][p.length()+1];
 
         // initialization: 
-		// 1. dp[0][0] = true, since empty string matches empty pattern
+		// dp[0][0] = true, since empty string matches empty pattern
 		dp[0][0] = true;
 
-		// 2. dp[i][0] = false (which is default value of the boolean array) since empty pattern cannot match non-empty string
-		// 3. dp[0][j]: what pattern matches empty string ""? It should be #*#*#*#*..., or (#*)* if allow me to represent regex using regex :P, 
-		// and for this case we need to check manually: 
-        // as we can see, the length of pattern should be even && the character at the even position should be *, 
-		// thus for odd length, dp[0][j] = false which is default. So we can just skip the odd position, i.e. j starts from 2, the interval of j is also 2. 
-		// and notice that the length of repeat sub-pattern #* is only 2, we can just make use of dp[0][j-2] rather than scanning j length each time 
-		// for checking if it matches #*#*#*#*.
+        // https://youtu.be/ym1zpF7sInc?t=101
         for (int j = 2; j <= p.length(); j += 2) {
             if (p.charAt(j-1) == '*' && dp[0][j-2]) {
                 dp[0][j] = true;
@@ -73,17 +67,13 @@ class Solution {
 
         for (int i = 0 ; i < s.length(); i++) {
             for (int j = 0; j < p.length(); j++) {
-                if (p.charAt(j) == '.') {
+                if (p.charAt(j) == s.charAt(i) || p.charAt(j) == '.') {
                     dp[i+1][j+1] = dp[i][j];
-                }
-                if (p.charAt(j) == s.charAt(i)) {
-                    dp[i+1][j+1] = dp[i][j];
-                }
-                if (p.charAt(j) == '*') {
-                    if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {
-                        dp[i+1][j+1] = dp[i+1][j-1];
+                } else if (p.charAt(j) == '*') {
+                    if (p.charAt(j-1) == s.charAt(i) || p.charAt(j-1) == '.') {
+                        dp[i+1][j+1] = dp[i+1][j-1] || dp[i][j+1];
                     } else {
-                        dp[i+1][j+1] = (dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]);
+                        dp[i+1][j+1] = dp[i+1][j-1];
                     }
                 }
             }
@@ -94,13 +84,13 @@ class Solution {
 /*
 dp[i][j] means whether [0...j] of p regex match [0...i] of s.
 
-1, If p.charAt(j) == s.charAt(i) :  dp[i][j] = dp[i-1][j-1];
+1, If p.charAt(j) == s.charAt(i) : dp[i][j] = dp[i-1][j-1];
 2, If p.charAt(j) == '.' : dp[i][j] = dp[i-1][j-1];
 3, If p.charAt(j) == '*': 
    here are two sub conditions:
-               3.1   if p.charAt(j-1) != s.charAt(i) : dp[i][j] = dp[i][j-2]  // in this case, a* only counts as empty (e.g. Example4's "c*" : Input: s = "aab", p = "c*a*b")
-               3.2   if p.charAt(j-1) == s.charAt(i) or p.charAt(j-1) == '.':
-                                dp[i][j] = dp[i-1][j]   // in this case, a* counts as multiple a (if p[j] == * && p[j-1] ~= s[i] && dp[i-1][j] == true, it means p[j-1..j]:s[i-1..i] ~= a*:aa i.e. s[i-1] == s[i]?. https://youtu.be/l3hda49XcDE?t=764)
-                                        or dp[i][j-1]   // in this case, a* counts as single a
-                                        or dp[i][j-2]   // in this case, a* counts as empty (丢弃 p[j-1] 和 p[j]，按 3.1 来)
+                    If dp[i][j-2] == true :
+                            dp[i][j] = dp[i][j-2]  // in this case, a* only counts as empty (e.g. Example4's "c*" : Input: s = "aab", p = "c*a*b")
+                    Or (逻辑或) If p.charAt(j-1) == s.charAt(i) or p.charAt(j-1) == '.' :
+                            dp[i][j] = dp[i-1][j]   // in this case, a* counts as single - multiple a. (https://youtu.be/ym1zpF7sInc?t=302)
+                            // Leetcode 最高票题解还检查并 or dp[i][j-1]，实际上不需要，因为只要 dp[i-1][j] == true 则 dp[i][j-1] 必为 true 反之未必？
 */
