829da6b9cd15b9aae99474a89b76970a585997db
yihaoye <yihaoyemail@gmail.com>
2021-11-09 00:01:16 +1300

    lc q300 round 1

--- Leetcode Practices/algorithms/medium/300 Longest Increasing Subsequence.java
+++ Leetcode Practices/algorithms/medium/300 Longest Increasing Subsequence.java
@@ -1,9 +1,42 @@
+/*
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+
+ 
+
+Example 1:
+
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+Example 2:
+
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+Example 3:
+
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+ 
+
+Constraints:
+
+1 <= nums.length <= 2500
+-104 <= nums[i] <= 104
+ 
+
+Follow up: Can you come up with an algorithm that runs in O(n log(n)) time complexity?
+*/
+
+
+
+// My Solution:
 class Solution {
     public int lengthOfLIS(int[] nums) {
         /*
             a: 思路 - DP，维持一个哈希表，以 nums2 为开始，nums2 即为 nums 的首 2 位子数组的哈希表，哈希表包含了 nums2 每个元素作为结尾的 LIS 值（Key 为元素，Val 为其 LIS），然后计算 nums3 时遍历 nums2，若 nums 3 的新元素大于 nums2 的被遍历元素时则对该元素的 LIS 值加一为 nums3 可得的 max 临时值，结尾时 put(nums3新元素, max)。
             b: 空间复杂度为 O(N)
-            c: Start Time 9:13pm, 9:38pm
         */
         HashMap<Integer, Integer> map = new HashMap<>();
         int res = 1; // final max
