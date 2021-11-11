adefd2418d65c863965c6a5a4484191f7b86a771
yihaoye <yihaoyemail@gmail.com>
2021-11-11 13:56:48 +1300

    lc q53 round 0

--- Leetcode Practices/algorithms/easy/53 Maximum Subarray.java
+++ Leetcode Practices/algorithms/easy/53 Maximum Subarray.java
@@ -26,3 +26,25 @@ class Solution {
         return max;
     }
 }
+
+
+
+// Other's Solution:
+class Solution {
+    public int maxSubArray(int[] nums) {
+        /*
+            a: 思路 - DP，dp[i] 表示以 nums[i] 结尾的连续子数组的最大和
+            b: 时间复杂度 O(N)，空间复杂度 O(N)
+            c: 看答案
+        */
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int res = dp[0];
+        for (int i=1; i<nums.length; i++) {
+            dp[i] = Math.max(dp[i-1] + nums[i], nums[i]);
+            res = Math.max(res, dp[i]);
+        }
+        
+        return res;
+    }
+}
