cd0233d3ce2c321072b897680c1d324266f8673e
yihaoye <yihaoyemail@gmail.com>
2021-11-14 11:38:12 +1300

    lc q63 add explain

--- Leetcode Practices/algorithms/medium/63 Unique Paths II.java
+++ Leetcode Practices/algorithms/medium/63 Unique Paths II.java
@@ -30,7 +30,7 @@ There are two ways to reach the bottom-right corner:
 
 
 // My Solution 1:
-// 思路 - DP，本题与题目 62 类似，遍历横纵坐标，dp[i, j] 代表着到达坐标 [i, j] 的路径可能性数量，因为机器人只能向右或向下移动，所以 dp[i, j] = dp[i-1, j] + dp[i][j-1]，在内循环时遇到障碍（即值为 1 的情况时），直接跳过本次内循环即可，因为后面 dp 保持为原始值 0 即正解
+// 思路 - DP，本题与题目 62 类似，遍历横纵坐标，dp[i, j] 代表着到达坐标 [i, j] 的路径可能性数量，因为机器人只能向右或向下移动，所以 dp[i, j] = dp[i-1, j] + dp[i][j-1]，在内循环时遇到障碍（即值为 1 的情况时），直接跳过本次 j 即可，而 dp[i][j] 维持原始值 0 即正解（注意，这里不会使后面的 dp[i+k][j] 为 0）
 // 时间复杂度 O(N*M)，空间复杂度 O(N*M)
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
