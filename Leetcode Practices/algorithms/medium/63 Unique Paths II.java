5236dce602d4d49234d33c732d8631ecf40573d0
yihaoye <yihaoyemail@gmail.com>
2021-11-14 01:13:25 +1300

    lc q63 add explain

--- Leetcode Practices/algorithms/medium/63 Unique Paths II.java
+++ Leetcode Practices/algorithms/medium/63 Unique Paths II.java
@@ -30,19 +30,21 @@ There are two ways to reach the bottom-right corner:
 
 
 // My Solution 1:
+// 思路 - DP，本题与题目 62 类似，遍历横纵坐标，dp[i, j] 代表着到达坐标 [i, j] 的路径可能性数量，因为机器人只能向右或向下移动，所以 dp[i, j] = dp[i-1, j] + dp[i][j-1]，在内循环时遇到障碍（即值为 1 的情况时），直接跳过本次内循环即可，因为后面 dp 保持为原始值 0 即正解
+// 时间复杂度 O(N*M)，空间复杂度 O(N*M)
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length, n = obstacleGrid[0].length; // only difference with question 62
-        int[][] mem = new int[m][n];
+        int[][] dp = new int[m][n];
         for (int i=0; i<m; i++) {
             for (int j=0; j<n; j++) {
                 if (obstacleGrid[i][j] == 1) continue; // only difference with question 62
-                if (i == 0 && j == 0) mem[i][j] = 1;
-                else if (i == 0) mem[i][j] = mem[i][j-1];
-                else if (j == 0) mem[i][j] = mem[i-1][j];
-                else mem[i][j] = mem[i-1][j] + mem[i][j-1];
+                if (i == 0 && j == 0) dp[i][j] = 1;
+                else if (i == 0) dp[i][j] = dp[i][j-1];
+                else if (j == 0) dp[i][j] = dp[i-1][j];
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
-        return mem[m-1][n-1];
+        return dp[m-1][n-1];
     }
 }
