9b985be5c0b8f5b88d382b0fd7147637cf11fbd5
yihaoye <yihaoyemail@gmail.com>
2021-11-21 13:41:36 +1300

    lc q105 round 0

---
+++ Leetcode Practices/algorithms/medium/105 Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -0,0 +1,55 @@
+/*
+Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+ 
+
+Example 1:
+https://assets.leetcode.com/uploads/2021/02/19/tree.jpg
+
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+Example 2:
+
+Input: preorder = [-1], inorder = [-1]
+Output: [-1]
+ 
+
+Constraints:
+
+1 <= preorder.length <= 3000
+inorder.length == preorder.length
+-3000 <= preorder[i], inorder[i] <= 3000
+preorder and inorder consist of unique values.
+Each value of inorder also appears in preorder.
+preorder is guaranteed to be the preorder traversal of the tree.
+inorder is guaranteed to be the inorder traversal of the tree.
+*/
+
+
+
+// Other's Solution:
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        /*
+            没思路看题解 - 前序遍历中最左为根，以此定位其在中序遍历中的索引，在该索引的左边皆是根左子树的节点，右边皆是根右子树的节点，后面以此类推递归求解（https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/discuss/34538/My-Accepted-Java-Solution）
+            时间复杂度 O(N)，空间复杂度 O(N) - 其中栈部分最理想是 O(logN)
+        */
+        return helper(0, 0, inorder.length - 1, preorder, inorder);
+    }
+
+    public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
+        if (preStart > preorder.length - 1 || inStart > inEnd) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int inIndex = 0; // Index of current root in inorder
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == root.val) {
+                inIndex = i;
+            }
+        }
+        root.left = helper(preStart + 1, inStart, inIndex - 1, preorder, inorder);
+        root.right = helper(preStart + inIndex - inStart + 1, inIndex + 1, inEnd, preorder, inorder); // 关于右子树的 preStart 解释：https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/discuss/34538/My-Accepted-Java-Solution/32854
+        return root;
+    }
+}
