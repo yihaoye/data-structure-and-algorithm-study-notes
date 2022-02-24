2d2442972285607d2ece678fc12f70166322dd29
yihaoye <yihaoyemail@gmail.com>
2022-02-25 12:22:07 +1300

    lc q664 round 0

---
+++ Leetcode Practices/algorithms/hard/664 Strange Printer.java
@@ -0,0 +1,58 @@
+/*
+There is a strange printer with the following two special properties:
+
+The printer can only print a sequence of the same character each time.
+At each turn, the printer can print new characters starting from and ending at any place and will cover the original existing characters.
+Given a string s, return the minimum number of turns the printer needed to print it.
+
+ 
+
+Example 1:
+
+Input: s = "aaabbb"
+Output: 2
+Explanation: Print "aaa" first and then print "bbb".
+Example 2:
+
+Input: s = "aba"
+Output: 2
+Explanation: Print "aaa" first and then print "b" from the second place of the string, which will cover the existing character 'a'.
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s consists of lowercase English letters.
+
+*/
+
+
+
+
+
+// Other's Solution:
+class Solution {
+    public int strangePrinter(String s) {
+        /*
+            动态规划 - https://leetcode-cn.com/problems/strange-printer/solution/qi-guai-de-da-yin-ji-by-leetcode-solutio-ogbu/
+            时间复杂度 O(N^3)，空间复杂度 O(N^2)
+        */
+        int n = s.length();
+        int[][] f = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            f[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    f[i][j] = f[i][j - 1];
+                } else {
+                    int minn = Integer.MAX_VALUE;
+                    for (int k = i; k < j; k++) {
+                        minn = Math.min(minn, f[i][k] + f[k + 1][j]);
+                    }
+                    f[i][j] = minn;
+                }
+            }
+        }
+        return f[0][n - 1];
+    }
+}
