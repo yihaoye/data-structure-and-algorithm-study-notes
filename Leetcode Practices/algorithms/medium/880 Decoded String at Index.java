b08afed44e0efda2ac1eddcd0b59d622a174a03a
yihaoye <yihaoyemail@gmail.com>
2022-02-25 12:06:09 +1300

    lc 880 round 0

---
+++ Leetcode Practices/algorithms/medium/880 Decoded String at Index.java
@@ -0,0 +1,71 @@
+/*
+You are given an encoded string s. To decode the string to a tape, the encoded string is read one character at a time and the following steps are taken:
+
+If the character read is a letter, that letter is written onto the tape.
+If the character read is a digit d, the entire current tape is repeatedly written d - 1 more times in total.
+Given an integer k, return the kth letter (1-indexed) in the decoded string.
+
+ 
+
+Example 1:
+
+Input: s = "leet2code3", k = 10
+Output: "o"
+Explanation: The decoded string is "leetleetcodeleetleetcodeleetleetcode".
+The 10th letter in the string is "o".
+Example 2:
+
+Input: s = "ha22", k = 5
+Output: "h"
+Explanation: The decoded string is "hahahaha".
+The 5th letter is "h".
+Example 3:
+
+Input: s = "a2345678999999999999999", k = 1
+Output: "a"
+Explanation: The decoded string is "a" repeated 8301530446056247680 times.
+The 1st letter is "a".
+ 
+
+Constraints:
+
+2 <= s.length <= 100
+s consists of lowercase English letters and digits 2 through 9.
+s starts with a letter.
+1 <= k <= 109
+It is guaranteed that k is less than or equal to the length of the decoded string.
+The decoded string is guaranteed to have less than 263 letters.
+*/
+
+
+
+
+
+class Solution {
+    public String decodeAtIndex(String S, int K) {
+        /*
+            逆向模拟 - https://leetcode-cn.com/problems/decoded-string-at-index/solution/suo-yin-chu-de-jie-ma-zi-fu-chuan-by-leetcode/
+            时间复杂度 O(N)，空间复杂度 O(N)
+        */
+        long size = 0;
+        int N = S.length();
+
+        // Find size = length of decoded string
+        for (int i = 0; i < N; ++i) {
+            char c = S.charAt(i);
+            if (Character.isDigit(c)) size *= c - '0';
+            else size++;
+        }
+
+        for (int i = N-1; i >= 0; --i) {
+            char c = S.charAt(i);
+            K %= size;
+            if (K == 0 && Character.isLetter(c)) return Character.toString(c);
+
+            if (Character.isDigit(c)) size /= c-'0';
+            else size--;
+        }
+
+        return null;
+    }
+}
