7145a4de576883db6efac85e31906a3b5229173a
yihaoye <yihaoyemail@gmail.com>
2022-02-25 12:13:49 +1300

    lc q229 round 0

---
+++ Leetcode Practices/algorithms/medium/229 Majority Element II.java
@@ -0,0 +1,90 @@
+/*
+Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: [3]
+Example 2:
+
+Input: nums = [1]
+Output: [1]
+Example 3:
+
+Input: nums = [1,2]
+Output: [1,2]
+ 
+
+Constraints:
+
+1 <= nums.length <= 5 * 104
+-109 <= nums[i] <= 109
+ 
+
+Follow up: Could you solve the problem in linear time and in O(1) space?
+*/
+
+
+
+
+
+// Other's Solution:
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        // 摩尔投票法 - https://leetcode-cn.com/problems/majority-element-ii/solution/liang-fu-dong-hua-yan-shi-mo-er-tou-piao-fa-zui-zh/
+        // 时间复杂度 O(N)，空间复杂度 O(1)
+        // 创建返回值
+        List<Integer> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
+        // 初始化两个候选人candidate，和他们的计票
+        int cand1 = nums[0], count1 = 0;
+        int cand2 = nums[0], count2 = 0;
+
+        // 摩尔投票法，分为两个阶段：配对阶段和计数阶段
+        // 配对阶段
+        for (int num : nums) {
+            // 投票
+            if (cand1 == num) {
+                count1++;
+                continue;
+            }
+            if (cand2 == num) {
+                count2++;
+                continue;
+            }
+
+            // 第1个候选人配对
+            if (count1 == 0) {
+                cand1 = num;
+                count1++;
+                continue;
+            }
+            // 第2个候选人配对
+            if (count2 == 0) {
+                cand2 = num;
+                count2++;
+                continue;
+            }
+
+            count1--;
+            count2--;
+        }
+
+        // 计数阶段
+        // 找到了两个候选人之后，需要确定票数是否满足大于 N/3
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (cand1 == num) count1++;
+            else if (cand2 == num) count2++;
+        }
+
+        if (count1 > nums.length / 3) res.add(cand1);
+        if (count2 > nums.length / 3) res.add(cand2);
+
+        return res;
+    }
+}
+
