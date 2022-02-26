77a6c6040f87b36c903bfa37715f2dcd1755a3e8
yihaoye <yihaoyemail@gmail.com>
2022-02-26 20:39:12 +1300

    lc q720 round 0

---
+++ Leetcode Practices/algorithms/medium/720 Longest Word in Dictionary.java
@@ -0,0 +1,91 @@
+/*
+Given an array of strings words representing an English Dictionary, return the longest word in words that can be built one character at a time by other words in words.
+
+If there is more than one possible answer, return the longest word with the smallest lexicographical order. If there is no answer, return the empty string.
+
+ 
+
+Example 1:
+
+Input: words = ["w","wo","wor","worl","world"]
+Output: "world"
+Explanation: The word "world" can be built one character at a time by "w", "wo", "wor", and "worl".
+Example 2:
+
+Input: words = ["a","banana","app","appl","ap","apply","apple"]
+Output: "apple"
+Explanation: Both "apply" and "apple" can be built from other words in the dictionary. However, "apple" is lexicographically smaller than "apply".
+ 
+
+Constraints:
+
+1 <= words.length <= 1000
+1 <= words[i].length <= 30
+words[i] consists of lowercase English letters.
+*/
+
+
+
+// Other's Solution:
+class Solution {
+    public String longestWord(String[] words) {
+        /*
+            字典树+DFS - https://leetcode-cn.com/problems/longest-word-in-dictionary/solution/ci-dian-zhong-zui-chang-de-dan-ci-by-leetcode/
+            时间复杂度 O(∑wi) - wi 是 words[i] 的长度，空间复杂度 O(∑wi)
+        */
+        Trie trie = new Trie();
+        int index = 0;
+        for (String word: words) {
+            trie.insert(word, ++index); // index 从 1 开始
+        }
+        trie.words = words;
+        return trie.dfs();
+    }
+}
+
+class Node {
+    char c;
+    HashMap<Character, Node> children = new HashMap();
+    int end;
+    public Node(char c){
+        this.c = c;
+    }
+}
+
+class Trie {
+    Node root;
+    String[] words;
+    public Trie() {
+        root = new Node('0');
+    }
+
+    public void insert(String word, int index) {
+        Node cur = root;
+        for (char c: word.toCharArray()) {
+            cur.children.putIfAbsent(c, new Node(c));
+            cur = cur.children.get(c);
+        }
+        cur.end = index;
+    }
+
+    public String dfs() {
+        String res = "";
+        Deque<Node> stack = new LinkedList();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+            if (node.end > 0 || node == root) {
+                if (node != root) {
+                    String word = words[node.end - 1];
+                    if (word.length() > res.length() || word.length() == res.length() && word.compareTo(res) < 0) {
+                        res = word;
+                    }
+                }
+                for (Node nei: node.children.values()) {
+                    stack.push(nei);
+                }
+            }
+        }
+        return res;
+    }
+}
