fc72d0d22b082838067bc439096efa067c0df59f
yihaoye <yihaoyemail@gmail.com>
2022-02-26 20:25:40 +1300

    lc q726 round 0

---
+++ Leetcode Practices/algorithms/hard/726 Number of Atoms.java
@@ -0,0 +1,120 @@
+/*
+Given a string formula representing a chemical formula, return the count of each atom.
+
+The atomic element always starts with an uppercase character, then zero or more lowercase letters, representing the name.
+
+One or more digits representing that element's count may follow if the count is greater than 1. If the count is 1, no digits will follow.
+
+For example, "H2O" and "H2O2" are possible, but "H1O2" is impossible.
+Two formulas are concatenated together to produce another formula.
+
+For example, "H2O2He3Mg4" is also a formula.
+A formula placed in parentheses, and a count (optionally added) is also a formula.
+
+For example, "(H2O2)" and "(H2O2)3" are formulas.
+Return the count of all elements as a string in the following form: the first name (in sorted order), followed by its count (if that count is more than 1), followed by the second name (in sorted order), followed by its count (if that count is more than 1), and so on.
+
+The test cases are generated so that all the values in the output fit in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: formula = "H2O"
+Output: "H2O"
+Explanation: The count of elements are {'H': 2, 'O': 1}.
+Example 2:
+
+Input: formula = "Mg(OH)2"
+Output: "H2MgO2"
+Explanation: The count of elements are {'H': 2, 'Mg': 1, 'O': 2}.
+Example 3:
+
+Input: formula = "K4(ON(SO3)2)2"
+Output: "K4N2O14S4"
+Explanation: The count of elements are {'K': 4, 'N': 2, 'O': 14, 'S': 4}.
+ 
+
+Constraints:
+
+1 <= formula.length <= 1000
+formula consists of English letters, digits, '(', and ')'.
+formula is always valid.
+*/
+
+
+
+// Other's Solution:
+class Solution {
+    /*
+        看题解：栈+哈希表 - https://leetcode-cn.com/problems/number-of-atoms/solution/yuan-zi-de-shu-liang-by-leetcode-solutio-54lv/
+        时间复杂度 O(N^2)，空间复杂度 O(N)
+    */
+    int i, n;
+    String formula;
+
+    public String countOfAtoms(String formula) {
+        this.i = 0;
+        this.n = formula.length();
+        this.formula = formula;
+
+        Deque<Map<String, Integer>> stack = new LinkedList<Map<String, Integer>>();
+        stack.push(new HashMap<String, Integer>());
+        while (i < n) {
+            char ch = formula.charAt(i);
+            if (ch == '(') {
+                i++;
+                stack.push(new HashMap<String, Integer>()); // 将一个空的哈希表压入栈中，准备统计括号内的原子数量
+            } else if (ch == ')') {
+                i++;
+                int num = parseNum(); // 括号右侧数字
+                Map<String, Integer> popMap = stack.pop(); // 弹出括号内的原子数量
+                Map<String, Integer> topMap = stack.peek();
+                for (Map.Entry<String, Integer> entry : popMap.entrySet()) {
+                    String atom = entry.getKey();
+                    int v = entry.getValue();
+                    topMap.put(atom, topMap.getOrDefault(atom, 0) + v * num); // 将括号内的原子数量乘上 num，加到上一层的原子数量中
+                }
+            } else {
+                String atom = parseAtom();
+                int num = parseNum();
+                Map<String, Integer> topMap = stack.peek();
+                topMap.put(atom, topMap.getOrDefault(atom, 0) + num); // 统计原子数量
+            }
+        }
+
+        Map<String, Integer> map = stack.pop();
+        TreeMap<String, Integer> treeMap = new TreeMap<String, Integer>(map);
+
+        StringBuffer sb = new StringBuffer();
+        for (Map.Entry<String, Integer> entry : treeMap.entrySet()) {
+            String atom = entry.getKey();
+            int count = entry.getValue();
+            sb.append(atom);
+            if (count > 1) {
+                sb.append(count);
+            }
+        }
+        return sb.toString();
+    }
+
+    public String parseAtom() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(formula.charAt(i++)); // 扫描首字母
+        while (i < n && Character.isLowerCase(formula.charAt(i))) {
+            sb.append(formula.charAt(i++)); // 扫描首字母后的小写字母
+        }
+        return sb.toString();
+    }
+
+    public int parseNum() {
+        if (i == n || !Character.isDigit(formula.charAt(i))) {
+            return 1; // 不是数字，视作 1
+        }
+        int num = 0;
+        while (i < n && Character.isDigit(formula.charAt(i))) {
+            num = num * 10 + formula.charAt(i++) - '0'; // 扫描数字
+        }
+        return num;
+    }
+}
