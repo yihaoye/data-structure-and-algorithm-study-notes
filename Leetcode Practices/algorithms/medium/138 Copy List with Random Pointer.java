d3c237d313e46a2ee17c2c637b3213103841baa0
yihaoye <yihaoyemail@gmail.com>
2022-03-30 17:43:37 +1300

    lc q138 round 2

--- Leetcode Practices/algorithms/medium/138 Copy List with Random Pointer.java
+++ Leetcode Practices/algorithms/medium/138 Copy List with Random Pointer.java
@@ -89,3 +89,48 @@ class Solution {
         return map.get(head.toString());
     }
 }
+
+
+
+// Other's Solution:
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+class Solution {
+    public Node copyRandomList(Node head) {
+        /*
+            https://leetcode-cn.com/problems/copy-list-with-random-pointer/solution/fu-zhi-dai-sui-ji-zhi-zhen-de-lian-biao-rblsf/
+             时间复杂度 O(N)，空间复杂度 O(1)
+        */
+        if (head == null) {
+            return null;
+        }
+        for (Node node = head; node != null; node = node.next.next) {
+            Node nodeNew = new Node(node.val);
+            nodeNew.next = node.next;
+            node.next = nodeNew;
+        }
+        for (Node node = head; node != null; node = node.next.next) {
+            Node nodeNew = node.next;
+            nodeNew.random = (node.random != null) ? node.random.next : null;
+        }
+        Node headNew = head.next;
+        for (Node node = head; node != null; node = node.next) {
+            Node nodeNew = node.next;
+            node.next = node.next.next;
+            nodeNew.next = (nodeNew.next != null) ? nodeNew.next.next : null;
+        }
+        return headNew;
+    }
+}
