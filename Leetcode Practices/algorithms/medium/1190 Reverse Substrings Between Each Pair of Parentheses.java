962bacf9cfd5934f34ebecd6895e33d1883bc70b
yihaoye <yihaoyemail@gmail.com>
2022-03-30 17:13:08 +1300

    lc q1190 round 0

---
+++ Leetcode Practices/algorithms/medium/1190 Reverse Substrings Between Each Pair of Parentheses.java
@@ -0,0 +1,71 @@
+/*
+You are given a string s that consists of lower case English letters and brackets.
+
+Reverse the strings in each pair of matching parentheses, starting from the innermost one.
+
+Your result should not contain any brackets.
+
+ 
+
+Example 1:
+
+Input: s = "(abcd)"
+Output: "dcba"
+Example 2:
+
+Input: s = "(u(love)i)"
+Output: "iloveu"
+Explanation: The substring "love" is reversed first, then the whole string is reversed.
+Example 3:
+
+Input: s = "(ed(et(oc))el)"
+Output: "leetcode"
+Explanation: First, we reverse the substring "oc", then "etco", and finally, the whole string.
+ 
+
+Constraints:
+
+1 <= s.length <= 2000
+s only contains lower case English characters and parentheses.
+It is guaranteed that all parentheses are balanced.
+
+*/
+
+
+
+
+
+// Other's Solution:
+class Solution {
+    public String reverseParentheses(String s) {
+        /*
+            每次碰到括号反转当前方向遍历 - https://leetcode-cn.com/problems/reverse-substrings-between-each-pair-of-parentheses/solution/fan-zhuan-mei-dui-gua-hao-jian-de-zi-chu-gwpv/
+            时间复杂度 O(N)，空间复杂度 O(N)
+        */
+        int n = s.length();
+        int[] pair = new int[n];
+        Deque<Integer> stack = new LinkedList<Integer>();
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else if (s.charAt(i) == ')') {
+                int j = stack.pop();
+                pair[i] = j;
+                pair[j] = i;
+            }
+        }
+
+        StringBuffer sb = new StringBuffer();
+        int index = 0, step = 1;
+        while (index < n) {
+            if (s.charAt(index) == '(' || s.charAt(index) == ')') {
+                index = pair[index];
+                step = -step;
+            } else {
+                sb.append(s.charAt(index));
+            }
+            index += step;
+        }
+        return sb.toString();
+    }
+}
