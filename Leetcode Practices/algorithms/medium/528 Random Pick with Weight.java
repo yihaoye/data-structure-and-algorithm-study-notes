fc18d5bdd27423c0338de8945ed031f451fdc68d
yihaoye <yihaoyemail@gmail.com>
2022-04-07 15:56:56 +1200

    lc q528 round 0

---
+++ Leetcode Practices/algorithms/medium/528 Random Pick with Weight.java
@@ -0,0 +1,89 @@
+/**
+You are given a 0-indexed array of positive integers w where w[i] describes the weight of the ith index.
+
+You need to implement the function pickIndex(), which randomly picks an index in the range [0, w.length - 1] (inclusive) and returns it. The probability of picking an index i is w[i] / sum(w).
+
+For example, if w = [1, 3], the probability of picking index 0 is 1 / (1 + 3) = 0.25 (i.e., 25%), and the probability of picking index 1 is 3 / (1 + 3) = 0.75 (i.e., 75%).
+ 
+
+Example 1:
+
+Input
+["Solution","pickIndex"]
+[[[1]],[]]
+Output
+[null,0]
+
+Explanation
+Solution solution = new Solution([1]);
+solution.pickIndex(); // return 0. The only option is to return 0 since there is only one element in w.
+Example 2:
+
+Input
+["Solution","pickIndex","pickIndex","pickIndex","pickIndex","pickIndex"]
+[[[1,3]],[],[],[],[],[]]
+Output
+[null,1,1,1,1,0]
+
+Explanation
+Solution solution = new Solution([1, 3]);
+solution.pickIndex(); // return 1. It is returning the second element (index = 1) that has a probability of 3/4.
+solution.pickIndex(); // return 1
+solution.pickIndex(); // return 1
+solution.pickIndex(); // return 1
+solution.pickIndex(); // return 0. It is returning the first element (index = 0) that has a probability of 1/4.
+
+Since this is a randomization problem, multiple answers are allowed.
+All of the following outputs can be considered correct:
+[null,1,1,1,1,0]
+[null,1,1,1,1,1]
+[null,1,1,1,0,0]
+[null,1,1,1,0,1]
+[null,1,0,1,0,0]
+......
+and so on.
+ 
+
+Constraints:
+
+1 <= w.length <= 104
+1 <= w[i] <= 105
+pickIndex will be called at most 104 times.
+ */
+
+
+
+// Other's Solution:
+class Solution {
+    // 前缀和+二分搜索 - https://leetcode-cn.com/problems/random-pick-with-weight/solution/an-quan-zhong-sui-ji-xuan-ze-by-leetcode-h13t/
+    // Time: init O(N) pick O(logN), Space: O(N)
+    int[] pre;
+    int total;
+
+    public Solution(int[] w) {
+        pre = new int[w.length];
+        pre[0] = w[0];
+        for (int i=1; i<w.length; i++) pre[i] = pre[i-1] + w[i];
+        total = pre[w.length-1];
+    }
+
+    public int pickIndex() {
+        int x = (int) (Math.random() * total) + 1;
+        return binarySearch(x);
+    }
+
+    private int binarySearch(int x) {
+        int low = 0, high = pre.length - 1;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (pre[mid] < x) low = mid + 1;
+            else high = mid;
+        }
+        return low;
+    }
+}
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
