22efa41833b609ca833409c68dd177a52b78ece1
yihaoye <yihaoyemail@gmail.com>
2022-04-16 22:39:53 +1200

    lc q105 improve

--- Leetcode Practices/algorithms/medium/105 Construct Binary Tree from Preorder and Inorder Traversal.java
+++ Leetcode Practices/algorithms/medium/105 Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -53,3 +53,25 @@ class Solution {
         return root;
     }
 }
+
+// 上面的解法可以用哈希表对 helper 里的 for 循环优化
+class Solution {
+    Map<Integer, Integer> inMap = new HashMap<>();
+    
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i=0; i<inorder.length; i++) inMap.put(inorder[i], i);
+        return helper(0, 0, inorder.length - 1, preorder, inorder);
+    }
+
+    public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
+        if (preStart > preorder.length - 1 || inStart > inEnd) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int inIndex = inMap.get(root.val); // Index of current root in inorder
+
+        root.left = helper(preStart + 1, inStart, inIndex - 1, preorder, inorder);
+        root.right = helper(preStart + inIndex - inStart + 1, inIndex + 1, inEnd, preorder, inorder);
+        return root;
+    }
+}
