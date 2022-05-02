52ec04bf544258be47aea17dcd7211ca42e8157a
yihaoye <yihaoyemail@gmail.com>
2022-05-02 22:33:49 +1200

    lc q16 format

--- Leetcode Practices/algorithms/medium/16 3Sum Closest.java
+++ Leetcode Practices/algorithms/medium/16 3Sum Closest.java
@@ -36,10 +36,10 @@ class Solution {
         */
         Arrays.sort(nums);
         int res = nums[0] + nums[1] + nums[2];
-        for(int i=0;i<nums.length;i++) {
-            int start = i+1, end = nums.length - 1;
+        for(int i=0; i<nums.length; i++) {
+            int start = i + 1, end = nums.length - 1;
             while(start < end) {
-                int sum = nums[start] + nums[end] + nums[i];
+                int sum = nums[i] + nums[start] + nums[end];
                 if (Math.abs(target - sum) < Math.abs(target - res)) res = sum;
                 
                 if (sum > target) end--;
