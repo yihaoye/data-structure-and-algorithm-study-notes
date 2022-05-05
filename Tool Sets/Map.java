e09c9c648b0068eadb5b810af7cd52a317afc980
yihaoye <yihaoyemail@gmail.com>
2022-05-05 13:09:01 +1200

    tool set map

--- Tool Sets/Map.java
+++ Tool Sets/Map.java
@@ -3,4 +3,5 @@ TreeMap<Integer, ArrayList<Integer>> tmap = new TreeMap<>();
 // subMap(K fromKey, boolean fromInclusive, K toKey, boolean toInclusive) Returns a view of the portion of this map whose keys range from fromKey to toKey.
 List<List<Integer>> lists = tmap.subMap(0, true, Integer.MAX_VALUE, true).values();
 
-
+// TreeMap.firstKey
+int firstKey = tmap.firstKey();
