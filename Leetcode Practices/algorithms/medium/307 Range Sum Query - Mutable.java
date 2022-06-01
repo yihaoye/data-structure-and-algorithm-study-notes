1dd030bd0ee2d2271069fd84722c08c44c11a7fd
yihaoye <yihaoyemail@gmail.com>
2022-06-01 22:45:19 +1200

    lc q307 round 2

--- Leetcode Practices/algorithms/medium/307 Range Sum Query - Mutable.java
+++ Leetcode Practices/algorithms/medium/307 Range Sum Query - Mutable.java
@@ -83,3 +83,80 @@ class NumArray {
  * obj.update(index,val);
  * int param_2 = obj.sumRange(left,right);
  */
+
+
+
+// Other's Solution 2:
+class NumArray {
+    /*
+        经典线段树场景，实现一个线段树
+        时间复杂度 O(logN)，空间复杂度 O(N)
+    */
+    private int[] nums;
+    private SegmentTreeNode root;
+    
+    public NumArray(int[] nums) {
+        this.nums = nums;
+        if (this.nums != null) this.root = buildTree(0, nums.length-1);
+    }
+    
+    public void update(int index, int val) {
+        updateTree(this.root, index, val);
+    }
+    
+    public int sumRange(int left, int right) {
+        return sumRange(this.root, left, right);
+    }
+    
+    private SegmentTreeNode buildTree(int start, int end) {
+        if (start == end) return new SegmentTreeNode(start, end, this.nums[start], null, null);
+        int mid = start + (end - start) / 2;
+        SegmentTreeNode left = buildTree(start, mid);
+        SegmentTreeNode right = buildTree(mid + 1, end);
+        return new SegmentTreeNode(start, end, left.val + right.val, left, right);
+    }
+    
+    private void updateTree(SegmentTreeNode root, int index, int val) {
+        if (root.start == index && root.end == index) {
+            root.val = val;
+            return;
+        }
+
+        if (index <= root.mid) updateTree(root.left, index, val);
+        else updateTree(root.right, index, val);
+
+        root.val = root.left.val + root.right.val;
+    }
+  
+    private int sumRange(SegmentTreeNode root, int left, int right) {
+        if (root.start == left && root.end == right) return root.val;
+          
+        if (right <= root.mid) return sumRange(root.left, left, right);
+        else if (left > root.mid) return sumRange(root.right, left, right);
+        else return sumRange(root.left, left, root.mid) + sumRange(root.right, root.mid + 1, right);
+    }
+    
+    class SegmentTreeNode {
+        int start;
+        int end;
+        int mid;
+        int val;
+        SegmentTreeNode left;
+        SegmentTreeNode right;
+
+        public SegmentTreeNode(int start, int end, int val, SegmentTreeNode left, SegmentTreeNode right) {
+            this.start = start;
+            this.end = end;
+            this.mid = start + (end - start) / 2;
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(index,val);
+ * int param_2 = obj.sumRange(left,right);
+ */
