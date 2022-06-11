872392fad71556d7dc0f05845474b593fa7d3a2c
yihaoye <yihaoyemail@gmail.com>
2022-06-11 22:47:37 +1200

    lc q309 add comments

--- Leetcode Practices/algorithms/medium/309 Best Time to Buy and Sell Stock with Cooldown.java
+++ Leetcode Practices/algorithms/medium/309 Best Time to Buy and Sell Stock with Cooldown.java
@@ -40,8 +40,8 @@ class Solution {
 		int[] s0 = new int[prices.length];
 		int[] s1 = new int[prices.length];
 		int[] s2 = new int[prices.length];
-		s1[0] = -prices[0];
 		s0[0] = 0;
+        s1[0] = -prices[0];
 		s2[0] = Integer.MIN_VALUE;
 		for (int i = 1; i < prices.length; i++) {
 			s0[i] = Math.max(s0[i-1], s2[i-1]);
