e7a392a644565faa0ccf69330dfc135b808d5470
yihaoye <yihaoyemail@gmail.com>
2022-07-20 21:13:14 +1200

    tool linked list node

--- Tool Sets/LinkedListNode.java
+++ Tool Sets/LinkedListNode.java
@@ -25,16 +25,37 @@ public class DLinkedList {
         node.next = head.next;
         head.next.prev = node;
         head.next = node;
+        size++;
+    }
+
+    public void addToTail(DLinkedNode node) {
+        node.next = tail;
+        node.prev = tail.prev;
+        tail.prev.next = node;
+        tail.prev = node;
+        size++;
     }
 
     public void removeNode(DLinkedNode node) {
         node.prev.next = node.next;
         node.next.prev = node.prev;
+        size--;
     }
 
     public void moveToHead(DLinkedNode node) {
         removeNode(node);
         addToHead(node);
+    }
+
+    public void moveToTail(DLinkedNode node) {
+        removeNode(node);
+        addToTail(node);
+    }
+
+    public DLinkedNode removeHead() {
+        DLinkedNode res = head.next;
+        removeNode(res);
+        return res;
     }
 
     public DLinkedNode removeTail() {
