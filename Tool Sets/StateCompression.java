2327ceed49ee115dc81addc9f0de99e3ff78cc12
yihaoye <yihaoyemail@gmail.com>
2022-07-29 16:27:42 +1200

    state compression

--- Tool Sets/StateCompression.java
+++ Tool Sets/StateCompression.java
@@ -5,7 +5,7 @@ public int arrToInt(int[] arr) {
     return (arr[0] << 5) | arr[1];
 }
 
-public int[] intToArr(int i) {
+public int[] intToArr(int i) { // this case return array with 2 elements
     return new int[]{i >> 5, i & 31};
 }
 /************/ 
