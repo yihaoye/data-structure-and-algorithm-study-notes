79d3c2e6ae316179587aac2ca3ab90b7311a040f
yihaoye <yihaoyemail@gmail.com>
2022-07-29 17:59:42 +1200

    lc q890 round 1

---
+++ Leetcode Practices/algorithms/medium/890 Find and Replace Pattern.java
@@ -0,0 +1,59 @@
+/**
+Given a list of strings words and a string pattern, return a list of words[i] that match pattern. You may return the answer in any order.
+
+A word matches the pattern if there exists a permutation of letters p so that after replacing every letter x in the pattern with p(x), we get the desired word.
+
+Recall that a permutation of letters is a bijection from letters to letters: every letter maps to another letter, and no two letters map to the same letter.
+
+ 
+
+Example 1:
+
+Input: words = ["abc","deq","mee","aqq","dkd","ccc"], pattern = "abb"
+Output: ["mee","aqq"]
+Explanation: "mee" matches the pattern because there is a permutation {a -> m, b -> e, ...}. 
+"ccc" does not match the pattern because {a -> c, b -> c, ...} is not a permutation, since a and b map to the same letter.
+Example 2:
+
+Input: words = ["a","b","c"], pattern = "a"
+Output: ["a","b","c"]
+ 
+
+Constraints:
+
+1 <= pattern.length <= 20
+1 <= words.length <= 50
+words[i].length == pattern.length
+pattern and words[i] are lowercase English letters.
+ */
+
+
+
+// My Solution:
+class Solution {
+    public int mod = (int) 1e9 + 7;
+    
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        // 自定义哈希 - 把所有第一个出现的字母映射为 0，第二个映射为 1，如此类推
+        // Time: O(N*M), Space: O(M)
+        List<String> res = new ArrayList<>();
+        int pHash = hash(pattern);
+        for (String word : words) {
+            if (pHash == hash(word)) res.add(word);
+        }
+        return res;
+    }
+    
+    // s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]
+    public int hash(String str) {
+        int res = 0, len = str.length(), nextMapInt = 0;
+        Map<Character, Integer> cMap = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            if (!cMap.containsKey(c)) cMap.put(c, nextMapInt++);
+            
+            res += Math.pow(cMap.get(c) * 31, len--) % mod;
+            res %= mod;
+        }
+        return res;
+    }
+}
