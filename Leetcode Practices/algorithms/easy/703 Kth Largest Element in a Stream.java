b6cc3ecb7032386f9a46b936cf234331baa058be
yihaoye <yihaoyemail@gmail.com>
2022-07-30 16:48:39 +1200

    lc q703 round 1

---
+++ Leetcode Practices/algorithms/easy/703 Kth Largest Element in a Stream.java
@@ -0,0 +1,73 @@
+/**
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Implement KthLargest class:
+
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+ 
+
+Example 1:
+
+Input
+["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+Output
+[null, 4, 5, 5, 8, 8]
+
+Explanation
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3);   // return 4
+kthLargest.add(5);   // return 5
+kthLargest.add(10);  // return 5
+kthLargest.add(9);   // return 8
+kthLargest.add(4);   // return 8
+ 
+
+Constraints:
+
+1 <= k <= 10^4
+0 <= nums.length <= 10^4
+-10^4 <= nums[i] <= 10^4
+-10^4 <= val <= 10^4
+At most 10^4 calls will be made to add.
+It is guaranteed that there will be at least k elements in the array when you search for the kth element.
+ */
+
+
+
+// My Solution:
+class KthLargest {
+    public PriorityQueue<Integer> pq;
+    public int k;
+
+    public KthLargest(int k, int[] nums) {
+        // 优先队列保持 k size，从小到大顺序，若新增比 peek() 大则 poll() 并 add，否则不做任何动作
+        this.pq = new PriorityQueue<>();
+        this.k = k;
+        
+        for (int num : nums) {
+            if (pq.size() < k) {
+                pq.offer(num);
+            } else if (pq.peek() < num) {
+                pq.poll();
+                pq.offer(num);
+            }
+        }
+    }
+    
+    public int add(int val) {
+        if (pq.size() < k) {
+            pq.offer(val);
+        } else if (pq.peek() < val) {
+            pq.poll();
+            pq.offer(val);
+        }
+        return pq.peek();
+    }
+}
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
