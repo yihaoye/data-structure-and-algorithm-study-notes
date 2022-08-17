c5c47d4b7252b57b75ce629a8ddb8974b808c9a6
yihaoye <yihaoyemail@gmail.com>
2022-08-17 16:35:07 +1200

    lc q804 round 1

---
+++ Leetcode Practices/algorithms/easy/804 Unique Morse Code Words.java
@@ -0,0 +1,63 @@
+/**
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+'a' maps to ".-",
+'b' maps to "-...",
+'c' maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+Return the number of different transformations among all words we have.
+
+ 
+
+Example 1:
+
+Input: words = ["gin","zen","gig","msg"]
+Output: 2
+Explanation: The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+There are 2 different transformations: "--...-." and "--...--.".
+Example 2:
+
+Input: words = ["a"]
+Output: 1
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 12
+words[i] consists of lowercase English letters.
+ */
+
+
+
+// My Solution:
+class Solution {
+    public String[] letterMap = new String[]{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+    
+    public int uniqueMorseRepresentations(String[] words) {
+        // HashSet
+        Set<String> set = new HashSet<>();
+        for (String word : words) {
+            set.add(transfer(word));
+        }
+        
+        return set.size();
+    }
+    
+    public String transfer(String word) {
+        StringBuilder strB = new StringBuilder();
+        for (char c : word.toCharArray()) {
+            strB.append(letterMap[c - 'a']);
+        }
+        return strB.toString();
+    }
+}
