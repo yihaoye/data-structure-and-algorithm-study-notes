ab31ed72286296e20a52eb43905bf03a7baebf75
yihaoye <yihaoyemail@gmail.com>
2022-08-19 00:17:13 +1200

    lc q127 round 2

--- Leetcode Practices/algorithms/hard/127 Word Ladder.java
+++ Leetcode Practices/algorithms/hard/127 Word Ladder.java
@@ -33,7 +33,49 @@ All the words in wordList are unique.
 
 
 
-// My Solution: (性能较差)
+// Other's Solution:
+class Solution {    
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        // https://leetcode.com/problems/word-ladder/discuss/1764371/A-very-highly-detailed-EXPLANATION
+        Set<String> set = new HashSet<>(wordList);
+        if (!set.contains(endWord)) return 0;
+        
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+        
+        Set<String> visited = new HashSet<>();
+        queue.add(beginWord);
+        
+        int changes = 1;
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String word = queue.poll();
+                if (word.equals(endWord)) return changes;
+                
+                for (int j = 0; j < word.length(); j++) {
+                    for (int k = 'a'; k <= 'z'; k++) {
+                        char arr[] = word.toCharArray();
+                        arr[j] = (char) k;
+                        
+                        String str = new String(arr);
+                        if (set.contains(str) && !visited.contains(str)) {
+                            queue.add(str);
+                            visited.add(str);
+                        }
+                    }
+                }
+            }
+            ++changes;
+        }
+        return 0;
+    }
+}
+
+
+
+// My Solution: (性能略差)
 class Solution {    
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         // BFS (pruning) + HashMap
@@ -70,7 +112,7 @@ class Solution {
         int diff = 0;
         for (int i=0; i<str1.length(); i++) {
             if (str1.charAt(i) != str2.charAt(i)) diff++;
-            if (diff > 1) return false;
+            if (diff > 1) return false; // 性能优化
         }
         return diff == 1 ? true : false;
     }
