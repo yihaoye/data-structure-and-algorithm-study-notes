0022c76a58ee8dceaa3b08057572e47b538d5384
yihaoye <yihaoyemail@gmail.com>
2022-09-07 18:15:08 +1200

    lc q876 round 2

--- Leetcode Practices/algorithms/easy/876 Middle of the Linked List.java
+++ Leetcode Practices/algorithms/easy/876 Middle of the Linked List.java
@@ -51,3 +51,32 @@ class Solution {
         return slow.next;
     }
 }
+
+
+
+// My Solution 2:
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        // 快慢指针
+        ListNode slow = head, fast = head;
+        while (fast != null) {
+            fast = fast.next;
+            if (fast != null) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+        }
+        
+        return slow;
+    }
+}
