5949b7f0b4d00624803e3bfd0b3f708584dccabd
yihaoye <yihaoyemail@gmail.com>
2022-10-04 18:40:24 +1300

    lc sql q569 round 0

---
+++ Leetcode Practices/database/hard/569 Median Employee Salary.sql
@@ -0,0 +1,112 @@
+/*
+Table: Employee
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| id           | int     |
+| company      | varchar |
+| salary       | int     |
++--------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the company and the salary of one employee.
+ 
+
+Write an SQL query to find the rows that contain the median salary of each company. While calculating the median, when you sort the salaries of the company, break the ties by id.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+---------+--------+
+| id | company | salary |
++----+---------+--------+
+| 1  | A       | 2341   |
+| 2  | A       | 341    |
+| 3  | A       | 15     |
+| 4  | A       | 15314  |
+| 5  | A       | 451    |
+| 6  | A       | 513    |
+| 7  | B       | 15     |
+| 8  | B       | 13     |
+| 9  | B       | 1154   |
+| 10 | B       | 1345   |
+| 11 | B       | 1221   |
+| 12 | B       | 234    |
+| 13 | C       | 2345   |
+| 14 | C       | 2645   |
+| 15 | C       | 2645   |
+| 16 | C       | 2652   |
+| 17 | C       | 65     |
++----+---------+--------+
+Output: 
++----+---------+--------+
+| id | company | salary |
++----+---------+--------+
+| 5  | A       | 451    |
+| 6  | A       | 513    |
+| 12 | B       | 234    |
+| 9  | B       | 1154   |
+| 14 | C       | 2645   |
++----+---------+--------+
+Explanation: 
+For company A, the rows sorted are as follows:
++----+---------+--------+
+| id | company | salary |
++----+---------+--------+
+| 3  | A       | 15     |
+| 2  | A       | 341    |
+| 5  | A       | 451    | <-- median
+| 6  | A       | 513    | <-- median
+| 1  | A       | 2341   |
+| 4  | A       | 15314  |
++----+---------+--------+
+For company B, the rows sorted are as follows:
++----+---------+--------+
+| id | company | salary |
++----+---------+--------+
+| 8  | B       | 13     |
+| 7  | B       | 15     |
+| 12 | B       | 234    | <-- median
+| 11 | B       | 1221   | <-- median
+| 9  | B       | 1154   |
+| 10 | B       | 1345   |
++----+---------+--------+
+For company C, the rows sorted are as follows:
++----+---------+--------+
+| id | company | salary |
++----+---------+--------+
+| 17 | C       | 65     |
+| 13 | C       | 2345   |
+| 14 | C       | 2645   | <-- median
+| 15 | C       | 2645   | 
+| 16 | C       | 2652   |
++----+---------+--------+
+ 
+
+Follow up: Could you solve it without using any built-in or window functions?
+*/
+
+
+
+/* Other's Solution */
+SELECT Id, Company, Salary
+FROM 
+    (SELECT
+        id, 
+        company, 
+        salary, 
+		row_number() over(PARTITION BY company ORDER BY salary) salaryrank, 
+		COUNT(*) over(PARTITION BY company) tte 
+    FROM employee) AS foo
+WHERE salaryrank >= tte/2 AND salaryrank <= tte/2+1;
+/*
+tte: total employee of the company.
+思路：先在各公司做薪水排名，排名在 [员工总数，员工总数+1] 区间内的员工就是领中位数薪水的。
+*/
