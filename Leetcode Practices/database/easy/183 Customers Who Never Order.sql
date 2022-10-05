56ea81bb3ee20b8416bc8dbbae09f1d8f0ec7a6a
yihaoye <yihaoyemail@gmail.com>
2022-10-05 14:21:14 +1300

    lc q183 round 1

--- Leetcode Practices/database/easy/183 Customers Who Never Order.sql
+++ Leetcode Practices/database/easy/183 Customers Who Never Order.sql
@@ -34,10 +34,14 @@ Using the above tables as example, return the following:
 
 
 
-
-
-# Other's Solution:
-# Write your MySQL query statement below
+/* Other's Solution */
 SELECT Name as Customers 
 FROM Customers 
 WHERE Customers.Id NOT IN (SELECT b.CustomerId FROM Orders b)
+
+
+
+/* My Solution */
+SELECT a.name AS Customers FROM
+    (SELECT c.id, c.name, o.id AS order_id FROM Customers c LEFT JOIN Orders o ON c.id = o.customerId) AS a
+WHERE a.order_id IS NULL;
