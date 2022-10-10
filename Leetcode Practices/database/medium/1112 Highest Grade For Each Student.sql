e5b14392b4441ffc03c37ec5aefd5a5b298570c3
yihaoye <yihaoyemail@gmail.com>
2022-10-10 15:08:51 +1300

    lc sql 1112 round 1

---
+++ Leetcode Practices/database/medium/1112 Highest Grade For Each Student.sql
@@ -0,0 +1,55 @@
+/*
+Table: Enrollments
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| student_id    | int     |
+| course_id     | int     |
+| grade         | int     |
++---------------+---------+
+(student_id, course_id) is the primary key of this table.
+ 
+
+Write a SQL query to find the highest grade with its corresponding course for each student. In case of a tie, you should find the course with the smallest course_id.
+
+Return the result table ordered by student_id in ascending order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Enrollments table:
++------------+-------------------+
+| student_id | course_id | grade |
++------------+-----------+-------+
+| 2          | 2         | 95    |
+| 2          | 3         | 95    |
+| 1          | 1         | 90    |
+| 1          | 2         | 99    |
+| 3          | 1         | 80    |
+| 3          | 2         | 75    |
+| 3          | 3         | 82    |
++------------+-----------+-------+
+Output: 
++------------+-------------------+
+| student_id | course_id | grade |
++------------+-----------+-------+
+| 1          | 2         | 99    |
+| 2          | 2         | 95    |
+| 3          | 3         | 82    |
++------------+-----------+-------+
+*/
+
+
+
+/* My Solution */
+SELECT a.student_id, a.course_id, a.grade
+FROM
+    (SELECT student_id, course_id, grade, ROW_NUMBER() OVER (PARTITION BY student_id ORDER BY grade DESC, course_id ASC) as ranking
+    FROM Enrollments) AS a
+WHERE a.ranking <= 1
+ORDER BY a.student_id;
