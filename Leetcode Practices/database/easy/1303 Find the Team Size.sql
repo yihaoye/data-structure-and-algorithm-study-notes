4ec97609a51ab7fcc73503f65bebd2b04414f757
yihaoye <yihaoyemail@gmail.com>
2022-10-12 15:24:03 +1300

    lc sql q1303 round 0

---
+++ Leetcode Practices/database/easy/1303 Find the Team Size.sql
@@ -0,0 +1,58 @@
+/*
+Table: Employee
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| employee_id   | int     |
+| team_id       | int     |
++---------------+---------+
+employee_id is the primary key for this table.
+Each row of this table contains the ID of each employee and their respective team.
+ 
+
+Write an SQL query to find the team size of each of the employees.
+
+Return result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee Table:
++-------------+------------+
+| employee_id | team_id    |
++-------------+------------+
+|     1       |     8      |
+|     2       |     8      |
+|     3       |     8      |
+|     4       |     7      |
+|     5       |     9      |
+|     6       |     9      |
++-------------+------------+
+Output: 
++-------------+------------+
+| employee_id | team_size  |
++-------------+------------+
+|     1       |     3      |
+|     2       |     3      |
+|     3       |     3      |
+|     4       |     1      |
+|     5       |     2      |
+|     6       |     2      |
++-------------+------------+
+Explanation: 
+Employees with Id 1,2,3 are part of a team with team_id = 8.
+Employee with Id 4 is part of a team with team_id = 7.
+Employees with Id 5,6 are part of a team with team_id = 9.
+*/
+
+
+
+/* Other's Solution */
+/* https://leetcode.com/problems/find-the-team-size/discuss/469457/Window-function */
+select employee_id, count(*) over(partition by team_id) as team_size
+from employee;
