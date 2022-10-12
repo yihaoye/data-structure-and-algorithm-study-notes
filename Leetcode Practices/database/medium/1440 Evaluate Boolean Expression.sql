3263767f053def8b0ded7f194d120ed778ced39b
yihaoye <yihaoyemail@gmail.com>
2022-10-12 15:19:46 +1300

    lc sql q1440 round 0

---
+++ Leetcode Practices/database/medium/1440 Evaluate Boolean Expression.sql
@@ -0,0 +1,84 @@
+/*
+Table Variables:
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| name          | varchar |
+| value         | int     |
++---------------+---------+
+name is the primary key for this table.
+This table contains the stored variables and their values.
+ 
+
+Table Expressions:
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| left_operand  | varchar |
+| operator      | enum    |
+| right_operand | varchar |
++---------------+---------+
+(left_operand, operator, right_operand) is the primary key for this table.
+This table contains a boolean expression that should be evaluated.
+operator is an enum that takes one of the values ('<', '>', '=')
+The values of left_operand and right_operand are guaranteed to be in the Variables table.
+ 
+
+Write an SQL query to evaluate the boolean expressions in Expressions table.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Variables table:
++------+-------+
+| name | value |
++------+-------+
+| x    | 66    |
+| y    | 77    |
++------+-------+
+Expressions table:
++--------------+----------+---------------+
+| left_operand | operator | right_operand |
++--------------+----------+---------------+
+| x            | >        | y             |
+| x            | <        | y             |
+| x            | =        | y             |
+| y            | >        | x             |
+| y            | <        | x             |
+| x            | =        | x             |
++--------------+----------+---------------+
+Output: 
++--------------+----------+---------------+-------+
+| left_operand | operator | right_operand | value |
++--------------+----------+---------------+-------+
+| x            | >        | y             | false |
+| x            | <        | y             | true  |
+| x            | =        | y             | false |
+| y            | >        | x             | true  |
+| y            | <        | x             | false |
+| x            | =        | x             | true  |
++--------------+----------+---------------+-------+
+Explanation: 
+As shown, you need to find the value of each boolean expression in the table using the variables table.
+*/
+
+
+
+/* Other's Solution */
+/* https://leetcode.com/problems/evaluate-boolean-expression/discuss/750093/Only-Joins-simplest-and-cleanest-solution */
+SELECT e.left_operand, e.operator, e.right_operand,
+       CASE WHEN operator = '>' THEN IF(v1.value > v2.value, 'true', 'false')
+            WHEN operator = '<' THEN IF(v1.value < v2.value, 'true', 'false')
+            ELSE IF(v1.value = v2.value, 'true', 'false')
+       END AS value
+FROM Expressions e
+JOIN Variables v1 ON v1.name = e.left_operand
+JOIN Variables v2 ON v2.name = e.right_operand;
