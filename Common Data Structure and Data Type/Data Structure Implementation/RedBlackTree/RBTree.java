5f7e4ccf736dd95a5104c9e2be7561d7419a1f8d
yihaoye <yihaoyemail@gmail.com>
2022-11-13 23:15:42 +1300

    rbt

--- Common Data Structure and Data Type/Data Structure Implementation/RedBlackTree/RBTree.java
+++ Common Data Structure and Data Type/Data Structure Implementation/RedBlackTree/RBTree.java
@@ -34,7 +34,7 @@ public class RBTree<T extends Comparable<T>> {
         }
 
         public String toString() {
-            return "" + key + (this.color == RED ? "(R)" : "B");
+            return "" + key + (this.color == RED ? "R" : "B");
         }
     }
 
@@ -42,185 +42,8 @@ public class RBTree<T extends Comparable<T>> {
         mRoot = null;
     }
 
-    private RBTNode<T> parentOf(RBTNode<T> node) {
-        return node != null ? node.parent : null;
-    }
-    private boolean colorOf(RBTNode<T> node) {
-        return node != null ? node.color : BLACK;
-    }
-    private boolean isRed(RBTNode<T> node) {
-        return ((node != null) && (node.color == RED)) ? true : false;
-    }
-    private boolean isBlack(RBTNode<T> node) {
-        return !isRed(node);
-    }
-    private void setBlack(RBTNode<T> node) {
-        if (node != null) node.color = BLACK;
-    }
-    private void setRed(RBTNode<T> node) {
-        if (node != null) node.color = RED;
-    }
-    private void setParent(RBTNode<T> node, RBTNode<T> parent) {
-        if (node != null) node.parent = parent;
-    }
-    private void setColor(RBTNode<T> node, boolean color) {
-        if (node != null) node.color = color;
-    }
-
-    /*
-     * 前序遍历"红黑树"
-     */
-    private void preOrder(RBTNode<T> tree) {
-        if (tree != null) {
-            System.out.print(tree.key+" ");
-            preOrder(tree.left);
-            preOrder(tree.right);
-        }
-    }
-
-    public void preOrder() {
-        preOrder(mRoot);
-    }
-
-    /*
-     * 中序遍历"红黑树"
-     */
-    private void inOrder(RBTNode<T> tree) {
-        if (tree != null) {
-            inOrder(tree.left);
-            System.out.print(tree.key+" ");
-            inOrder(tree.right);
-        }
-    }
-
-    public void inOrder() {
-        inOrder(mRoot);
-    }
-
-
-    /*
-     * 后序遍历"红黑树"
-     */
-    private void postOrder(RBTNode<T> tree) {
-        if (tree != null) {
-            postOrder(tree.left);
-            postOrder(tree.right);
-            System.out.print(tree.key+" ");
-        }
-    }
-
-    public void postOrder() {
-        postOrder(mRoot);
-    }
-
-
-    /*
-     * (递归实现)查找"红黑树x"中键值为key的节点
-     */
-    private RBTNode<T> search(RBTNode<T> x, T key) {
-        if (x == null) return x;
-
-        int cmp = key.compareTo(x.key);
-        if (cmp < 0) return search(x.left, key);
-        else if (cmp > 0) return search(x.right, key);
-        else return x;
-    }
-
-    public RBTNode<T> search(T key) {
-        return search(mRoot, key);
-    }
-
-    /*
-     * (非递归实现)查找"红黑树x"中键值为key的节点
-     */
-    private RBTNode<T> iterativeSearch(RBTNode<T> x, T key) {
-        while (x != null) {
-            int cmp = key.compareTo(x.key);
-
-            if (cmp < 0) x = x.left;
-            else if (cmp > 0) x = x.right;
-            else return x;
-        }
-
-        return x;
-    }
-
-    public RBTNode<T> iterativeSearch(T key) {
-        return iterativeSearch(mRoot, key);
-    }
-
-    /*
-     * 查找最小结点：返回tree为根结点的红黑树的最小结点。
-     */
-    private RBTNode<T> minimum(RBTNode<T> tree) {
-        if (tree == null) return null;
-        while (tree.left != null) tree = tree.left;
-
-        return tree;
-    }
-
-    public T minimum() {
-        RBTNode<T> p = minimum(mRoot);
-        if (p != null) return p.key;
-
-        return null;
-    }
-
-    /*
-     * 查找最大结点：返回tree为根结点的红黑树的最大结点。
-     */
-    private RBTNode<T> maximum(RBTNode<T> tree) {
-        if (tree == null) return null;
-        while (tree.right != null) tree = tree.right;
-
-        return tree;
-    }
-
-    public T maximum() {
-        RBTNode<T> p = maximum(mRoot);
-        if (p != null) return p.key;
-
-        return null;
-    }
-
-    /*
-     * 找结点(x)的后继结点。即，查找"红黑树中数据值大于该结点"的"最小结点"。
-     */
-    public RBTNode<T> successor(RBTNode<T> x) {
-        // 如果x存在右孩子，则"x的后继结点"为 "以其右孩子为根的子树的最小结点"。
-        if (x.right != null) return minimum(x.right);
-
-        // 如果x没有右孩子。则x有以下两种可能：
-        // (01) x是"一个左孩子"，则"x的后继结点"为 "它的父结点"。
-        // (02) x是"一个右孩子"，则查找"x的最低的父结点，并且该父结点要具有左孩子"，找到的这个"最低的父结点"就是"x的后继结点"。
-        RBTNode<T> y = x.parent;
-        while ((y != null) && (x == y.right)) {
-            x = y;
-            y = y.parent;
-        }
-
-        return y;
-    }
-
-    /*
-     * 找结点(x)的前驱结点。即，查找"红黑树中数据值小于该结点"的"最大结点"。
-     */
-    public RBTNode<T> predecessor(RBTNode<T> x) {
-        // 如果x存在左孩子，则"x的前驱结点"为 "以其左孩子为根的子树的最大结点"。
-        if (x.left != null) return maximum(x.left);
-
-        // 如果x没有左孩子。则x有以下两种可能：
-        // (01) x是"一个右孩子"，则"x的前驱结点"为 "它的父结点"。
-        // (01) x是"一个左孩子"，则查找"x的最低的父结点，并且该父结点要具有右孩子"，找到的这个"最低的父结点"就是"x的前驱结点"。
-        RBTNode<T> y = x.parent;
-        while ((y != null) && (x == y.left)) {
-            x = y;
-            y = y.parent;
-        }
-
-        return y;
-    }
-
+    /* 核心部分 */
+    
     /*
      * 对红黑树的节点(x)进行左旋转
      *
@@ -609,6 +432,188 @@ public class RBTree<T extends Comparable<T>> {
     public void clear() {
         destroy(mRoot);
         mRoot = null;
+    }
+
+
+
+    /* 非核心部分 */
+
+    private RBTNode<T> parentOf(RBTNode<T> node) {
+        return node != null ? node.parent : null;
+    }
+    private boolean colorOf(RBTNode<T> node) {
+        return node != null ? node.color : BLACK;
+    }
+    private boolean isRed(RBTNode<T> node) {
+        return ((node != null) && (node.color == RED)) ? true : false;
+    }
+    private boolean isBlack(RBTNode<T> node) {
+        return !isRed(node);
+    }
+    private void setBlack(RBTNode<T> node) {
+        if (node != null) node.color = BLACK;
+    }
+    private void setRed(RBTNode<T> node) {
+        if (node != null) node.color = RED;
+    }
+    private void setParent(RBTNode<T> node, RBTNode<T> parent) {
+        if (node != null) node.parent = parent;
+    }
+    private void setColor(RBTNode<T> node, boolean color) {
+        if (node != null) node.color = color;
+    }
+
+    /*
+     * 前序遍历"红黑树"
+     */
+    private void preOrder(RBTNode<T> tree) {
+        if (tree != null) {
+            System.out.print(tree.key+" ");
+            preOrder(tree.left);
+            preOrder(tree.right);
+        }
+    }
+
+    public void preOrder() {
+        preOrder(mRoot);
+    }
+
+    /*
+     * 中序遍历"红黑树"
+     */
+    private void inOrder(RBTNode<T> tree) {
+        if (tree != null) {
+            inOrder(tree.left);
+            System.out.print(tree.key+" ");
+            inOrder(tree.right);
+        }
+    }
+
+    public void inOrder() {
+        inOrder(mRoot);
+    }
+
+    /*
+     * 后序遍历"红黑树"
+     */
+    private void postOrder(RBTNode<T> tree) {
+        if (tree != null) {
+            postOrder(tree.left);
+            postOrder(tree.right);
+            System.out.print(tree.key+" ");
+        }
+    }
+
+    public void postOrder() {
+        postOrder(mRoot);
+    }
+
+
+    /*
+     * (递归实现)查找"红黑树x"中键值为key的节点
+     */
+    private RBTNode<T> search(RBTNode<T> x, T key) {
+        if (x == null) return x;
+
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return search(x.left, key);
+        else if (cmp > 0) return search(x.right, key);
+        else return x;
+    }
+
+    public RBTNode<T> search(T key) {
+        return search(mRoot, key);
+    }
+
+    /*
+     * (非递归实现)查找"红黑树x"中键值为key的节点
+     */
+    private RBTNode<T> iterativeSearch(RBTNode<T> x, T key) {
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+
+            if (cmp < 0) x = x.left;
+            else if (cmp > 0) x = x.right;
+            else return x;
+        }
+
+        return x;
+    }
+
+    public RBTNode<T> iterativeSearch(T key) {
+        return iterativeSearch(mRoot, key);
+    }
+
+    /*
+     * 查找最小结点：返回tree为根结点的红黑树的最小结点。
+     */
+    private RBTNode<T> minimum(RBTNode<T> tree) {
+        if (tree == null) return null;
+        while (tree.left != null) tree = tree.left;
+
+        return tree;
+    }
+
+    public T minimum() {
+        RBTNode<T> p = minimum(mRoot);
+        if (p != null) return p.key;
+
+        return null;
+    }
+
+    /*
+     * 查找最大结点：返回tree为根结点的红黑树的最大结点。
+     */
+    private RBTNode<T> maximum(RBTNode<T> tree) {
+        if (tree == null) return null;
+        while (tree.right != null) tree = tree.right;
+
+        return tree;
+    }
+
+    public T maximum() {
+        RBTNode<T> p = maximum(mRoot);
+        if (p != null) return p.key;
+
+        return null;
+    }
+
+    /*
+     * 找结点(x)的后继结点。即，查找"红黑树中数据值大于该结点"的"最小结点"。
+     */
+    public RBTNode<T> successor(RBTNode<T> x) {
+        // 如果x存在右孩子，则"x的后继结点"为 "以其右孩子为根的子树的最小结点"。
+        if (x.right != null) return minimum(x.right);
+
+        // 如果x没有右孩子。则x有以下两种可能：
+        // (01) x是"一个左孩子"，则"x的后继结点"为 "它的父结点"。
+        // (02) x是"一个右孩子"，则查找"x的最低的父结点，并且该父结点要具有左孩子"，找到的这个"最低的父结点"就是"x的后继结点"。
+        RBTNode<T> y = x.parent;
+        while ((y != null) && (x == y.right)) {
+            x = y;
+            y = y.parent;
+        }
+
+        return y;
+    }
+
+    /*
+     * 找结点(x)的前驱结点。即，查找"红黑树中数据值小于该结点"的"最大结点"。
+     */
+    public RBTNode<T> predecessor(RBTNode<T> x) {
+        // 如果x存在左孩子，则"x的前驱结点"为 "以其左孩子为根的子树的最大结点"。
+        if (x.left != null) return maximum(x.left);
+
+        // 如果x没有左孩子。则x有以下两种可能：
+        // (01) x是"一个右孩子"，则"x的前驱结点"为 "它的父结点"。
+        // (01) x是"一个左孩子"，则查找"x的最低的父结点，并且该父结点要具有右孩子"，找到的这个"最低的父结点"就是"x的前驱结点"。
+        RBTNode<T> y = x.parent;
+        while ((y != null) && (x == y.left)) {
+            x = y;
+            y = y.parent;
+        }
+
+        return y;
     }
 
     /*
