1499e7d256af0d4dfd885dd75600d3d75a2ea96c
yihaoye <yihaoyemail@gmail.com>
2022-11-22 20:42:34 +1300

    lc q2477 add comment

--- Leetcode Practices/algorithms/medium/2477 Minimum Fuel Cost to Report to the Capital.java
+++ Leetcode Practices/algorithms/medium/2477 Minimum Fuel Cost to Report to the Capital.java
@@ -68,7 +68,7 @@ class Solution {
         /*
             Intuition - We need to track the number of people that reach each node and divide that by the number of seats per car, this will tell us the number of cars required to take us to the node that is closer to node 0
 
-            DFS
+            DFS + 数学
             Imagine you are at a leaf node, you move towards 0. There will be only 1 person in the car (you)
             Now let's say you're somewhere in the middle of the tree, with a car of size 5. You have 3 children nodes. Let's say each child node brings 1 car of 3 people. So a total of 3 * 3 = 9 people. Including you there are 10 people now. Now you have 3 cars from the child nodes and one car of your own. You actually need just 10 / 5 = 2 cars. You take 2 cars and move towards 0
 
