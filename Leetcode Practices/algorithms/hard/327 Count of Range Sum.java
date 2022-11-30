81ef5a91245951f45194195734577e3cac6ebdc4
yihaoye <yihaoyemail@gmail.com>
2022-12-01 00:55:29 +1300

    lc q327 round 0

---
+++ Leetcode Practices/algorithms/hard/327 Count of Range Sum.java
@@ -0,0 +1,252 @@
+/**
+Given an integer array nums and two integers lower and upper, return the number of range sums that lie in [lower, upper] inclusive.
+
+Range sum S(i, j) is defined as the sum of the elements in nums between indices i and j inclusive, where i <= j.
+
+ 
+
+Example 1:
+
+Input: nums = [-2,5,-1], lower = -2, upper = 2
+Output: 3
+Explanation: The three ranges are: [0,0], [2,2], and [0,2] and their respective sums are: -2, -1, 2.
+Example 2:
+
+Input: nums = [0], lower = 0, upper = 0
+Output: 1
+ 
+
+Constraints:
+
+1 <= nums.length <= 10^5
+-2^31 <= nums[i] <= 2^31 - 1
+-10^5 <= lower <= upper <= 10^5
+The answer is guaranteed to fit in a 32-bit integer.
+ */
+
+
+
+// My Solution: (TLE)
+class Solution {
+    public int countRangeSum(int[] nums, int lower, int upper) {
+        // prefix sum + customized red black tree
+        int n = nums.length, res = 0;
+        
+        long sum = 0L;
+        RBTree<Long> rbt = new RBTree<>();
+        
+        for (int i=0; i<n; i++) {
+            sum += nums[i];
+            if (lower <= sum && sum <= upper) res++;
+            res += rbt.dfs(sum-upper, sum-lower); // dp[i] - dp[j] >= lower, dp[i] - dp[j] <= upper
+            rbt.insert(sum);
+        }
+        
+        return res;
+    }
+}
+
+public class RBTree<T extends Comparable<T>> {
+
+    private RBTNode<T> mRoot;    // 根结点
+
+    private static final boolean RED   = false;
+    private static final boolean BLACK = true;
+
+    public class RBTNode<T extends Comparable<T>> {
+        boolean color;        // 颜色
+        T key;                // 关键字(键值)
+        int cnt;
+        RBTNode<T> left;      // 左孩子
+        RBTNode<T> right;     // 右孩子
+        RBTNode<T> parent;    // 父结点
+
+        public RBTNode(T key, boolean color, RBTNode<T> parent, RBTNode<T> left, RBTNode<T> right) {
+            this.key = key;
+            this.cnt = 1;
+            this.color = color;
+            this.parent = parent;
+            this.left = left;
+            this.right = right;
+        }
+
+        public T getKey() {
+            return key;
+        }
+    }
+
+    public RBTree() {
+        mRoot = null;
+    }
+
+    /* 核心部分 */
+    private void leftRotate(RBTNode<T> x) {
+        RBTNode<T> y = x.right;
+
+        x.right = y.left;
+        if (y.left != null) y.left.parent = x;
+
+        y.parent = x.parent;
+
+        if (x.parent == null) {
+            this.mRoot = y;
+        } else {
+            if (x.parent.left == x) x.parent.left = y;
+            else x.parent.right = y;
+        }
+
+        y.left = x;
+        x.parent = y;
+    }
+
+    private void rightRotate(RBTNode<T> y) {
+        RBTNode<T> x = y.left;
+
+        y.left = x.right;
+        if (x.right != null) x.right.parent = y;
+
+        x.parent = y.parent;
+
+        if (y.parent == null) {
+            this.mRoot = x;
+        } else {
+            if (y == y.parent.right) y.parent.right = x;
+            else y.parent.left = x;
+        }
+
+        x.right = y;
+        y.parent = x;
+    }
+
+    private void insertFixUp(RBTNode<T> node) {
+        RBTNode<T> parent, gparent;
+
+        while (((parent = parentOf(node)) != null) && isRed(parent)) {
+            gparent = parentOf(parent);
+
+            if (parent == gparent.left) {
+                RBTNode<T> uncle = gparent.right;
+                if ((uncle != null) && isRed(uncle)) {
+                    setBlack(uncle);
+                    setBlack(parent);
+                    setRed(gparent);
+                    node = gparent;
+                    continue;
+                }
+
+                if (parent.right == node) {
+                    leftRotate(parent);
+                    RBTNode<T> tmp = parent;
+                    parent = node;
+                    node = tmp;
+                }
+
+                setBlack(parent);
+                setRed(gparent);
+                rightRotate(gparent);
+            } else {
+                RBTNode<T> uncle = gparent.left;
+                if ((uncle != null) && isRed(uncle)) {
+                    setBlack(uncle);
+                    setBlack(parent);
+                    setRed(gparent);
+                    node = gparent;
+                    continue;
+                }
+
+                if (parent.left == node) {
+                    rightRotate(parent);
+                    RBTNode<T> tmp = parent;
+                    parent = node;
+                    node = tmp;
+                }
+                
+                setBlack(parent);
+                setRed(gparent);
+                leftRotate(gparent);
+            }
+        }
+
+        setBlack(this.mRoot);
+    }
+
+    private void insert(RBTNode<T> node) {
+        int cmp;
+        RBTNode<T> y = null;
+        RBTNode<T> x = this.mRoot;
+
+        while (x != null) {
+            y = x;
+            cmp = node.key.compareTo(x.key);
+            if (cmp < 0) x = x.left;
+            else if (cmp > 0) x = x.right;
+            else { x.cnt++; return; }
+        }
+
+        node.parent = y;
+        if (y != null) {
+            cmp = node.key.compareTo(y.key);
+            if (cmp < 0) y.left = node;
+            else y.right = node;
+        } else {
+            this.mRoot = node;
+        }
+
+        node.color = RED;
+        insertFixUp(node);
+    }
+
+    public void insert(T key) {
+        RBTNode<T> node = new RBTNode<T>(key, BLACK, null, null, null);
+        insert(node);
+    }
+
+    
+
+    /* 次核心部分 */
+    private RBTNode<T> parentOf(RBTNode<T> node) {
+        return node != null ? node.parent : null;
+    }
+    private boolean colorOf(RBTNode<T> node) {
+        return node != null ? node.color : BLACK;
+    }
+    private boolean isRed(RBTNode<T> node) {
+        return ((node != null) && (node.color == RED)) ? true : false;
+    }
+    private boolean isBlack(RBTNode<T> node) {
+        return !isRed(node);
+    }
+    private void setBlack(RBTNode<T> node) {
+        if (node != null) node.color = BLACK;
+    }
+    private void setRed(RBTNode<T> node) {
+        if (node != null) node.color = RED;
+    }
+    private void setParent(RBTNode<T> node, RBTNode<T> parent) {
+        if (node != null) node.parent = parent;
+    }
+    private void setColor(RBTNode<T> node, boolean color) {
+        if (node != null) node.color = color;
+    }
+    
+    private int dfs(RBTNode<T> x, T fromKey, T toKey) {
+        if (x == null) return 0;
+
+        int res = 0;
+        int cmpF = x.key.compareTo(fromKey);
+        int cmpT = x.key.compareTo(toKey);
+        if (cmpF >= 0 && cmpT <= 0) {
+            res += x.cnt;
+            res += dfs(x.right, fromKey, toKey);
+            res += dfs(x.left, fromKey, toKey);
+        }
+        if (cmpF < 0) res += dfs(x.right, fromKey, toKey);
+        if (cmpT > 0) res += dfs(x.left, fromKey, toKey);
+        
+        return res;
+    }
+
+    public int dfs(T fromKey, T toKey) {
+        return dfs(mRoot, fromKey, toKey);
+    }
+}
