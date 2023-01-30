56c47f22fbcab41158e91069f6bd919033f757af
yihaoye <yihaoyemail@gmail.com>
2023-01-30 23:00:58 +1300

    lc q1146 round 2

--- Leetcode Practices/algorithms/medium/1146 Snapshot Array.java
+++ Leetcode Practices/algorithms/medium/1146 Snapshot Array.java
@@ -73,3 +73,31 @@ class SnapshotArray {
  * int param_2 = obj.snap();
  * int param_3 = obj.get(index,snap_id);
  */
+
+
+
+// Other's Solution: 
+// https://leetcode.com/problems/snapshot-array/solutions/350562/java-python-binary-search/?orderBy=most_votes
+class SnapshotArray {
+    TreeMap<Integer, Integer>[] A;
+    int snap_id = 0;
+    public SnapshotArray(int length) {
+        A = new TreeMap[length];
+        for (int i = 0; i < length; i++) {
+            A[i] = new TreeMap<Integer, Integer>();
+            A[i].put(0, 0);
+        }
+    }
+
+    public void set(int index, int val) {
+        A[index].put(snap_id, val);
+    }
+
+    public int snap() {
+        return snap_id++;
+    }
+
+    public int get(int index, int snap_id) {
+        return A[index].floorEntry(snap_id).getValue();
+    }
+}
