3c51d0c406f841f7b928c53f2304645e117fdf96
yihaoye <yihaoyemail@gmail.com>
2023-03-09 21:43:49 +1300

    lc q2583 round 1

---
+++ Leetcode Practices/algorithms/medium/2583 Kth Largest Sum in a Binary Tree.java
@@ -0,0 +1,81 @@
+/**
+You are given the root of a binary tree and a positive integer k.
+
+The level sum in the tree is the sum of the values of the nodes that are on the same level.
+
+Return the kth largest level sum in the tree (not necessarily distinct). If there are fewer than k levels in the tree, return -1.
+
+Note that two nodes are on the same level if they have the same distance from the root.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,8,9,2,1,3,7,4,6], k = 2
+Output: 13
+Explanation: The level sums are the following:
+- Level 1: 5.
+- Level 2: 8 + 9 = 17.
+- Level 3: 2 + 1 + 3 + 7 = 13.
+- Level 4: 4 + 6 = 10.
+The 2nd largest level sum is 13.
+Example 2:
+
+
+Input: root = [1,2,null,3], k = 1
+Output: 3
+Explanation: The largest level sum is 3.
+ 
+
+Constraints:
+
+The number of nodes in the tree is n.
+2 <= n <= 10^5
+1 <= Node.val <= 10^6
+1 <= k <= n
+ */
+
+
+
+// My Solution:
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public long kthLargestLevelSum(TreeNode root, int k) {
+        // BFS + PriorityQueue
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        PriorityQueue<Long> pq = new PriorityQueue<>(Collections.reverseOrder());
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            long sum = 0L;
+            while (size-- > 0) {
+                TreeNode cur = queue.poll();
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
+                sum += cur.val;
+            }
+            pq.offer(sum);
+        }
+        
+        if (k > pq.size()) return -1;
+        while (--k > 0) {
+            pq.poll();
+        }
+        return pq.poll();
+    }
+}
