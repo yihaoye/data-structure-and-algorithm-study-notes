e7a9daf42334b586f1e1d99b7294f9c3be3879ec
yihaoye <yihaoyemail@gmail.com>
2023-04-06 12:41:00 +1200

    lc q718 round 2

--- Leetcode Practices/algorithms/medium/718 Maximum Length of Repeated Subarray.java
+++ Leetcode Practices/algorithms/medium/718 Maximum Length of Repeated Subarray.java
@@ -41,3 +41,61 @@ class Solution {
         return res;
     }
 }
+
+
+
+// Other's Solution:
+class Solution {
+    private static final int PRIME = 101;
+    private static final int MOD = 1_000_000_007;
+    
+    public int findLength(int[] A, int[] B) {
+        // https://leetcode.com/problems/maximum-length-of-repeated-subarray/solutions/1181612/java-clean-o-n-logn-rolling-hash-solution-with-comments/?orderBy=most_votes&topicTags=rolling-hash
+        if (A.length < B.length) return findLength(B, A);
+        
+        int min = 1, max = Math.min(A.length, B.length) + 1;
+        while (min < max) {
+            int mid = min + (max - min) / 2;
+            if (rollingHashCheck(mid, A, B)) min = mid + 1;
+            else max = mid;
+        }
+        return min - 1; // min means min invalid length
+    }
+    
+    // check every subarray with length mid
+    private boolean rollingHashCheck(int mid, int[] A, int[] B) {
+        Set<Integer> set = new HashSet<>();
+        long power = 1;
+        for (int i = 0; i < mid-1; i++) {
+            power = (power * PRIME) % MOD;
+        }
+        
+        // 1). Adding hash values of subarrays of B into set
+        long hash = 0;
+        for (int j = 0; j < mid; j++) {
+            hash = (hash * PRIME % MOD + B[j]) % MOD;
+        }
+        set.add((int) hash);
+        
+        for (int i = 0, j = mid; j < B.length; i++, j++) {
+            hash = (hash + MOD - B[i] * power % MOD) % MOD;
+            hash = (hash * PRIME + B[j]) % MOD;
+            set.add((int) hash);
+        }
+        
+        // 2.) Check the hash values of subarrays of A
+        hash = 0;
+        for (int j = 0; j < mid; j++) {
+            hash = (hash * PRIME % MOD + A[j]) % MOD;
+        }
+        if (set.contains((int) hash)) return true;
+        
+        for (int i = 0, j = mid; j < A.length; i++, j++) {
+            hash = (hash + MOD - A[i] * power % MOD) % MOD;
+            hash = (hash * PRIME + A[j]) % MOD;
+            if (set.contains((int) hash)) return true;
+        }
+        
+        return false;
+    }
+}
