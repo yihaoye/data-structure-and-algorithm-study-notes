3615399d9261211dc55af3d23fdfebc00bbe080b
yihaoye <yihaoyemail@gmail.com>
2023-04-27 18:06:08 +1200

    lc q1225 round 0

---
+++ Leetcode Practices/database/hard/1225 Report Contiguous Dates.sql
@@ -0,0 +1,98 @@
+/*
+Table: Failed
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| fail_date    | date    |
++--------------+---------+
+fail_date is the primary key for this table.
+This table contains the days of failed tasks.
+ 
+
+Table: Succeeded
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| success_date | date    |
++--------------+---------+
+success_date is the primary key for this table.
+This table contains the days of succeeded tasks.
+ 
+
+A system is running one task every day. Every task is independent of the previous tasks. The tasks can fail or succeed.
+
+Write an SQL query to generate a report of period_state for each continuous interval of days in the period from 2019-01-01 to 2019-12-31.
+
+period_state is 'failed' if tasks in this interval failed or 'succeeded' if tasks in this interval succeeded. Interval of days are retrieved as start_date and end_date.
+
+Return the result table ordered by start_date.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Failed table:
++-------------------+
+| fail_date         |
++-------------------+
+| 2018-12-28        |
+| 2018-12-29        |
+| 2019-01-04        |
+| 2019-01-05        |
++-------------------+
+Succeeded table:
++-------------------+
+| success_date      |
++-------------------+
+| 2018-12-30        |
+| 2018-12-31        |
+| 2019-01-01        |
+| 2019-01-02        |
+| 2019-01-03        |
+| 2019-01-06        |
++-------------------+
+Output: 
++--------------+--------------+--------------+
+| period_state | start_date   | end_date     |
++--------------+--------------+--------------+
+| succeeded    | 2019-01-01   | 2019-01-03   |
+| failed       | 2019-01-04   | 2019-01-05   |
+| succeeded    | 2019-01-06   | 2019-01-06   |
++--------------+--------------+--------------+
+Explanation: 
+The report ignored the system state in 2018 as we care about the system in the period 2019-01-01 to 2019-12-31.
+From 2019-01-01 to 2019-01-03 all tasks succeeded and the system state was "succeeded".
+From 2019-01-04 to 2019-01-05 all tasks failed and the system state was "failed".
+From 2019-01-06 to 2019-01-06 all tasks succeeded and the system state was "succeeded".
+*/
+
+
+
+/*
+Other's Solution
+https://leetcode.com/problems/report-contiguous-dates/solutions/835526/mysql-window-function-easy-to-understand/?envType=study-plan&id=sql-iii&orderBy=most_votes
+*/
+SELECT stats AS period_state, MIN(day) AS start_date, MAX(day) AS end_date
+FROM (
+    SELECT 
+        day, 
+        RANK() OVER (ORDER BY day) AS overall_ranking, 
+        stats, 
+        rk, 
+        (RANK() OVER (ORDER BY day) - rk) AS inv
+    FROM (
+        SELECT fail_date AS day, 'failed' AS stats, RANK() OVER (ORDER BY fail_date) AS rk
+        FROM Failed
+        WHERE fail_date BETWEEN '2019-01-01' AND '2019-12-31'
+        UNION 
+        SELECT success_date AS day, 'succeeded' AS stats, RANK() OVER (ORDER BY success_date) AS rk
+        FROM Succeeded
+        WHERE success_date BETWEEN '2019-01-01' AND '2019-12-31') t
+    ) c
+GROUP BY inv, stats
+ORDER BY start_date;
