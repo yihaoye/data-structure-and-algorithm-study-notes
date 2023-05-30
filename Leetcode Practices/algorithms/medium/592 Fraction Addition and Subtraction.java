e0e0a70aba3c8868508a7ec923d5f07c1faf10d5
yihaoye <yihaoyemail@gmail.com>
2023-05-30 15:35:12 +1200

    lc q592 round 2

--- Leetcode Practices/algorithms/medium/592 Fraction Addition and Subtraction.java
+++ Leetcode Practices/algorithms/medium/592 Fraction Addition and Subtraction.java
@@ -30,6 +30,30 @@ The numerator and denominator of the final result are guaranteed to be valid and
 
 
 
+// Other's Solution:
+class Solution {
+    public String fractionAddition(String expression) {
+        // https://leetcode.com/problems/fraction-addition-and-subtraction/solutions/103384/small-simple-c-java-python/
+        Scanner sc = new Scanner(expression).useDelimiter("/|(?=[-+])");
+        int A = 0, B = 1;
+        while (sc.hasNext()) {
+            int a = sc.nextInt(), b = sc.nextInt();
+            A = A * b + a * B;
+            B *= b;
+            int g = gcd(A, B);
+            A /= g;
+            B /= g;
+        }
+        return A + "/" + B;
+    }
+
+    private int gcd(int a, int b) {
+        return a != 0 ? gcd(b % a, a) : Math.abs(b);
+    }
+}
+
+
+
 // My Solution:
 class Solution {
     public String fractionAddition(String expression) {
