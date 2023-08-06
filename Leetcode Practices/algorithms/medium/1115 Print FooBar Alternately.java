37ecba61bf963934542766105d6a89b5a81e9b6e
yihaoye <yihaoyemail@gmail.com>
2023-08-07 11:53:12 +1200

    lc q1115 update

--- Leetcode Practices/algorithms/medium/1115 Print FooBar Alternately.java
+++ Leetcode Practices/algorithms/medium/1115 Print FooBar Alternately.java
@@ -84,6 +84,8 @@ public class FooBarSynchronized {
     }
 }
 
+
+
 // Lock(volatile write happens-before volatile read)
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
@@ -91,10 +93,10 @@ import java.util.concurrent.locks.ReentrantLock;
 class FooBar {
     private int n;
     // flag 0->foo to be print  1->foo has been printed
-    private int flag=0;
-    ReentrantLock reentrantLock= new ReentrantLock();
-    Condition fooPrintedCondition=reentrantLock.newCondition();
-    Condition barPrintedCondition=reentrantLock.newCondition();
+    private int flag = 0;
+    ReentrantLock reentrantLock = new ReentrantLock();
+    Condition fooPrintedCondition = reentrantLock.newCondition();
+    Condition barPrintedCondition = reentrantLock.newCondition();
 
     public FooBar(int n) {
         this.n = n;
@@ -102,37 +104,30 @@ class FooBar {
 
     public void foo(Runnable printFoo) throws InterruptedException {
         for (int i = 0; i < n; i++) {
-            try {
-                reentrantLock.lock();
-                while (flag == 1) {
-                    barPrintedCondition.await();
-                }
-                // printFoo.run() outputs "foo". Do not change or remove this line.
-                printFoo.run();
-                flag = 1;
-                fooPrintedCondition.signalAll();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                reentrantLock.unlock();
-            }
+            reentrantLock.lock();
+            while (flag == 1) barPrintedCondition.await();
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            printFoo.run();
+            flag = 1;
+            fooPrintedCondition.signalAll();
+            reentrantLock.unlock();
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
         for (int i = 0; i < n; i++) {
-          reentrantLock.lock();
-          while (flag == 0) {
-              fooPrintedCondition.await();
-          }
-          // printBar.run() outputs "bar". Do not change or remove this line.
-        	printBar.run();
-          flag = 0;
-        	barPrintedCondition.signalAll();
-        	reentrantLock.unlock();
+            reentrantLock.lock();
+            while (flag == 0) fooPrintedCondition.await();
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            printBar.run();
+            flag = 0;
+            barPrintedCondition.signalAll();
+            reentrantLock.unlock();
         }
     }
 }
+
+
 
 // CAS (volatile write happens-before volatile read)
 import java.util.concurrent.atomic.AtomicInteger;
