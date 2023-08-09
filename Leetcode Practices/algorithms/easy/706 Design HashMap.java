0d3ce5f49034963b6042d999ecd161849c96c590
yihaoye <yihaoyemail@gmail.com>
2023-08-10 11:27:22 +1200

    lc q706 update

--- Leetcode Practices/algorithms/easy/706 Design HashMap.java
+++ Leetcode Practices/algorithms/easy/706 Design HashMap.java
@@ -29,65 +29,6 @@ Please do not use the built-in HashMap library.
 
 
 
-// My Solution after inspired:
-class MyHashMap {
-    int[] map;
-    
-    /** Initialize your data structure here. */
-    public MyHashMap() {
-        map = new int[1000000];
-        Arrays.fill(map, -1);
-    }
-    
-    /** value will always be non-negative. */
-    public void put(int key, int value) {
-        map[key] = value;
-    }
-    
-    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
-    public int get(int key) {
-        return map[key];
-    }
-    
-    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
-    public void remove(int key) {
-        map[key] = -1;
-    }
-}
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap obj = new MyHashMap();
- * obj.put(key,value);
- * int param_2 = obj.get(key);
- * obj.remove(key);
- */
-// Above can be optimized in following:
-class MyHashMap {
-    int[] map;
-    
-    /** Initialize your data structure here. */
-    public MyHashMap() {
-        map = new int[1000000];
-    }
-    
-    /** value will always be non-negative. */
-    public void put(int key, int value) {
-        map[key] = value + 1;
-    }
-    
-    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
-    public int get(int key) {
-        return map[key] - 1;
-    }
-    
-    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
-    public void remove(int key) {
-        map[key] = 0;
-    }
-}
-
-
-
 // Other's Solution:
 /*
 Traditional Hash-Table Implementation - Using Array of LinkedList - Accepted in 75 ms
@@ -148,6 +89,65 @@ class MyHashMap {
 			this.val = val;
 		}
 	}
+}
+
+
+
+// My Solution after inspired:
+class MyHashMap {
+    int[] map;
+    
+    /** Initialize your data structure here. */
+    public MyHashMap() {
+        map = new int[1000000];
+        Arrays.fill(map, -1);
+    }
+    
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        map[key] = value;
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+        return map[key];
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+        map[key] = -1;
+    }
+}
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
+// Above can be optimized in following:
+class MyHashMap {
+    int[] map;
+    
+    /** Initialize your data structure here. */
+    public MyHashMap() {
+        map = new int[1000000];
+    }
+    
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        map[key] = value + 1;
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+        return map[key] - 1;
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+        map[key] = 0;
+    }
 }
 
 
