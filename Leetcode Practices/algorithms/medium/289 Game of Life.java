79b753d5b49e76afe8292cbceb83b3c3da4e126f
yihaoye <yihaoyemail@gmail.com>
2023-08-10 21:44:10 +1200

    lc q289 update

--- Leetcode Practices/algorithms/medium/289 Game of Life.java
+++ Leetcode Practices/algorithms/medium/289 Game of Life.java
@@ -40,6 +40,56 @@ In this question, we represent the board using a 2D array. In principle, the boa
 
 
 
+// Other's Solution: Time: O(mn), Space: O(1)
+class Solution {
+    public void gameOfLife(int[][] board) {
+        // https://leetcode.cn/problems/game-of-life/solution/sheng-ming-you-xi-by-leetcode-solution/
+        int[] neighbors = {0, 1, -1};
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        // 遍历面板每一个格子里的细胞
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                // 对于每一个细胞统计其八个相邻位置里的活细胞数量
+                int liveNeighbors = 0;
+
+                // 查看相邻 8 个的细胞是否是活细胞
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if ((neighbors[i] == 0 && neighbors[j] == 0)) continue;
+                        // 相邻位置的坐标
+                        int r = (row + neighbors[i]);
+                        int c = (col + neighbors[j]);
+                        if (r >= rows || r < 0 || c >= cols || c < 0) continue;
+                        if (Math.abs(board[r][c]) == 1) liveNeighbors++;
+                    }
+                }
+
+                // 规则 1 或规则 3 
+                if ((board[row][col] == 1) && (liveNeighbors < 2 || liveNeighbors > 3)) {
+                    board[row][col] = -1; // -1 代表这个细胞过去是活的现在死了
+                }
+                // 规则 4
+                if (board[row][col] == 0 && liveNeighbors == 3) {
+                    board[row][col] = 2; // 2 代表这个细胞过去是死的现在活了
+                }
+            }
+        }
+
+        // 遍历 board 得到一次更新后的状态
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (board[row][col] > 0) board[row][col] = 1;
+                else board[row][col] = 0;
+            }
+        }
+    }
+}
+
+
+
 // My Solution:
 class Solution {
     public void gameOfLife(int[][] board) {
