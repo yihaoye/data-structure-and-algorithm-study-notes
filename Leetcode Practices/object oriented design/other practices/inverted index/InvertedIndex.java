87d5dc143bac93292817606cb10d5f6b86ef0828
yihaoye <yihaoyemail@gmail.com>
2023-08-19 13:07:33 +1200

    Inverted Index

--- Leetcode Practices/object oriented design/other practices/inverted index/InvertedIndex.java
+++ Leetcode Practices/object oriented design/other practices/inverted index/InvertedIndex.java
@@ -4,13 +4,14 @@ import java.util.concurrent.*;
 
 public class InvertedIndex {
     // 本例用哈希表实现。
-    // 如果进阶搜索功能要考虑关键词的顺序甚至关心重复次数的话，可以改为使用 Map<String, Map<String, List<Integer>>> keyToTexts，
-    // 其中 Map<String, List<Integer>> 的 String 是 text，List<Integer> 为关键词 key 在 text 中的位置/索引，因为会在解析关键词添加进 List 时保证有序所以可以二分搜索，
-    // 具体过程为已记录上一个关键词（lastKey）在匹配的该 text（可能有多个，每个都需要后面同样的操作）中的位置 idx，
-    // 然后在 keyToTexts.get(nextKey).get(text) 即 List<Integer> 中二分搜索大于 idx 的最小值，如果找到则设为新的 idx 且继续，否则该 text 不匹配直接 break。
+    // 如果进阶搜索功能要考虑关键词的顺序甚至关心重复次数的话，可以改为使用 Map<String, Map<String, List<Integer>>> textToKeys，
+    // 其中 Map<String, List<Integer>> 的 String 是关键词，List<Integer> 为关键词 key 在 text 中的位置/索引，因为会在解析关键词添加进 List 时保证有序所以可以二分搜索，
+    // 具体过程为已记录上一个关键词（lastKey）在匹配的该 text 中的位置 idx，
+    // 然后在 textToKeys.get(text).get(nextKey) 即 List<Integer> 中二分搜索大于 idx 的最小值，如果找到则设为新的 idx 且继续，否则该 text 不匹配直接 break。
+    // 具体实现可以参考 search(String[] keywords, Operation operation) 方法中的注释，另外其实还可以进一步优化逻辑提升性能，但是会增加代码复杂度，这里就不展开了。
     // 关心顺序与重复的话也可以用字典树（Trie）实现，但是时空复杂度都会更高，而且最差可能要遍历整个字典树，所以不推荐。
     private Map<String, Set<String>> keyToTexts;
-    private Map<String, Set<String>> textToKeys;
+    private Map<String, Set<String>> textToKeys; // 用于提高删除效率和进阶场景如关心顺序和重复次数
     private Set<String> stopWords; // 停用词表，反向索引通常会忽略一些常用词，例如 a, the, is, are, will, can, with, etc
     
     /* search 性能提升，非必需 */
@@ -81,6 +82,19 @@ public class InvertedIndex {
             if (operation.equals(Operation.OR)) result.addAll(search(keyword));
             if (operation.equals(Operation.AND)) result.retainAll(search(keyword));
         }
+
+        /* 如果关心顺序和重复次数 */
+        // for (String text : result) {
+        //     Integer idx = -1;
+        //     for (String keyword : keywords) {
+        //         Integer nextIdx = textToKeys.get(text).get(keyword).ceilingKey(idx);
+        //         if (nextIdx == null || nextIdx <= idx) {
+        //             result.remove(text);
+        //             break;
+        //         }
+        //         idx = nextIdx;
+        //     }
+        // }
 
         /* 使用缓存提高性能，非必需，但是注意要引入对 operation 和 delete 的考虑 */
         // searchCache.get(operation).put(hashCode, result);
