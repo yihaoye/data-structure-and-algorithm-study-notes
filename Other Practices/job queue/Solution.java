92e5867a92d78939d71e2825891c67ad02378dfd
yihaoye <yihaoyemail@gmail.com>
2023-08-20 01:22:56 +1200

    job queue

--- Other Practices/job queue/Solution.java
+++ Other Practices/job queue/Solution.java
@@ -46,7 +46,7 @@ class JobQueue {
     private Queue<Job> queue = new LinkedList<>();
 
     public void enqueue(Job job) {
-        for (Job subJob : job.subJobs) enqueue(subJob); // 除了递归，也可以考虑采用 BFS
+        for (Job subJob : job.subJobs) enqueue(subJob); // 除了递归，也可以考虑采用 BFS，因为题目要求子任务也放进队列且实现异步执行，所以在这里就不使用拓扑排序了
         queue.offer(job);
     }
 
@@ -70,9 +70,12 @@ class JobQueue {
     }
 
     public void runJobsAsync() {
+        ExecutorService executor = Executors.newFixedThreadPool(CPU_CORES_NUM);
         while (!queue.isEmpty()) {
             Job job = queue.poll();
-            CompletableFuture.runAsync(() -> runJob(job, 5));
+            executor.execute(() -> runJob(job, 5)); // 比 CompletableFuture.runAsync(() -> runJob(job, 5)); 更好，因为 CompletableFuture 在当调用/任务数比 CPU 核数远多的情况下会因线程切换、竞争导致性能下降，而线程池会阻塞任务队列，保证线程数不会超过 CPU 核数
+            // 注意：Java 没有像 JavaScript 中的事件循环（Event Loop）机制，Java 的异步任务通常都是通过线程实现的
         }
+        executor.shutdown();
     }
 }
