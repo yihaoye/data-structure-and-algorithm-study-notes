4937d87fa7630b295b64c1df5c99e43ac44239ae
yihaoye <yihaoyemail@gmail.com>
2023-08-23 22:50:32 +1200

    lc q415 extends

--- Leetcode Practices/algorithms/easy/415 Add Strings.java
+++ Leetcode Practices/algorithms/easy/415 Add Strings.java
@@ -27,3 +27,37 @@ class Solution {
         return strB.reverse().toString();
     }
 }
+
+
+
+// My Solution 2:
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int carry = 0;
+        StringBuilder strB = new StringBuilder();
+
+        // 处理如果带浮点数 e.g. "345.9999", "22.1", "10.0"
+        int point1 = num1.indexOf("."); int point2 = num2.indexOf(".");
+        if (point1 != -1 && point2 != -1) {
+            int precision1 = num1.length() - point1; int precision2 = num2.length() - point2;
+            if (precision1 > precision2) 
+                for (int i=0; i<precision1-precision2; i++) num2 = num2 + "0";
+            if (precision2 > precision1)
+                for (int i=0; i<precision2-precision1; i++) num1 = num1 + "0";
+        }
+
+        for (int i=num1.length()-1, j=num2.length()-1; i>=0 || j>=0; i--, j--) {
+            if (i == point1 && j == point2) {
+                strB.append(".");
+                continue;
+            }
+            int n1 = (i >= 0) ? num1.charAt(i)-'0' : 0;
+            int n2 = (j >= 0) ? num2.charAt(j)-'0' : 0;
+            int sum = n1 + n2 + carry;
+            carry = sum/10;
+            strB.append(sum%10);
+        }
+        if (carry > 0) strB.append(carry);
+        return strB.reverse().toString();
+    }
+}
