372c74b23778d6ee3806d350e556743b6a163b88
yihaoye <yihaoyemail@gmail.com>
2023-08-24 13:40:06 +1200

    RandomHashMap

--- Other Practices/random map/Solution.java
+++ Other Practices/random map/Solution.java
@@ -4,57 +4,22 @@ class RandomHashMap { // by Copilot (not tested)
     private ListNode[] arr; // List<ListNode> list
     private int count;
 
-    public MyHashMap() {
+    public RandomHashMap() {
         this.arr = new ListNode[10000]; // fix size, for extendable size, use List<ListNode> list
         this.count = 0;
     }
 
-    public void insert(int k, int v) {
-        int index = k % arr.length;
-        ListNode head = arr[index];
-        if (head == null) {
-            arr[index] = new ListNode(k, v);
-            count++;
-        } else {
-            ListNode curr = head;
-            while (curr != null) {
-                if (curr.key == k) {
-                    curr.val = v;
-                    return;
-                }
-                curr = curr.next;
-            }
-            ListNode newNode = new ListNode(k, v);
-            newNode.next = head;
-            head.prev = newNode;
-            arr[index] = newNode;
-            count++;
-        }
+    public void insert(int k, int v) { // O(1)
+        // 参考 LC Q706
+        this.count++; // 如果 insert 了重复的 key，count 不变
     }
 
-    public boolean remove(int k) {
-        int index = k % arr.length;
-        ListNode head = arr[index];
-        if (head == null) return false;
-        ListNode curr = head;
-        while (curr != null) {
-            if (curr.key == k) {
-                if (curr.prev == null) {
-                    arr[index] = curr.next;
-                    if (curr.next != null) curr.next.prev = null;
-                } else {
-                    curr.prev.next = curr.next;
-                    if (curr.next != null) curr.next.prev = curr.prev;
-                }
-                count--;
-                return true;
-            }
-            curr = curr.next;
-        }
-        return false;
+    public boolean remove(int k) { // O(1)
+        // 参考 LC Q706
+        this.count--; // 如果 remove 了不存在的 key，count 不变
     }
 
-    public int getRandom() {
+    public int getRandom() { // O(N) N 为不同的 key 的个数 - 一种进一步提升该函数性能的方法是使用红黑树代替 ListNode[] arr，并且每个 Node 要多增加两个值，一个是该 Node 的左右子树的节点个数，一个是 lazyUpdate，如此 RandomHashMap 的三个函数时间复杂度均为 O(logN)
         if (count == 0) throw new RuntimeException("Empty map");
         int random = (int) (Math.random() * arr.length);
         int index = 0;
