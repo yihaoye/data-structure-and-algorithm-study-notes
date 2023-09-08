b78f21bfe5215256fee450ed913befd4748ce630
yihaoye <yihaoyemail@gmail.com>
2023-09-09 00:49:14 +1200

    lc q1504 update

--- Leetcode Practices/algorithms/medium/1504 Count Submatrices With All Ones.java
+++ Leetcode Practices/algorithms/medium/1504 Count Submatrices With All Ones.java
@@ -46,7 +46,6 @@ class Solution {
         /*
             前缀和 + 单调栈 - 
             https://leetcode-cn.com/problems/count-submatrices-with-all-ones/solution/tong-ji-quan-1-zi-ju-xing-by-leetcode-solution/
-            https://leetcode.cn/problems/count-submatrices-with-all-ones/solutions/316595/shi-yong-yu-yun-suan-fu-javashuang-bai-by-mp1256/
             类似：https://leetcode.com/problems/largest-rectangle-in-histogram/
             时间复杂度 O(N*M)，空间复杂度 O(N*M)
         */
@@ -64,13 +63,13 @@ class Solution {
             Deque<int[]> monoStack = new LinkedList<int[]>(); // 创建一个单调栈，用于维护高度递增的条形矩形
             int sum = 0; // 记录以当前列为底边的所有矩形的总数
             for (int i = 0; i < n; ++i) {
-                int height = 1;
+                int height = 1; // 以 row[i][j] 为横轴宽度的满 1 矩形的高
                 while (!monoStack.isEmpty() && monoStack.peekFirst()[0] > row[i][j]) {
                     sum -= monoStack.peekFirst()[1] * (monoStack.peekFirst()[0] - row[i][j]); // 弹出的元素代表的是矩形的数量，同时从总数中减去多余的部分
-                    height += monoStack.peekFirst()[1]; // 累加当前高度的数量
+                    height += monoStack.peekFirst()[1]; // 更新以 row[i][j] 为横轴宽度的满 1 矩形的高（只能截止匹配到比自己更宽的连续上方的记录，因为如果比自己窄就不是满 1 矩形）
                     monoStack.pollFirst(); 
                 } 
-                sum += row[i][j]; // 类似前缀和
+                sum += row[i][j]; // 类似前缀和 -- https://leetcode.cn/problems/count-submatrices-with-all-ones/solutions/316595/shi-yong-yu-yun-suan-fu-javashuang-bai-by-mp1256/
                 ans += sum; 
                 monoStack.offerFirst(new int[]{row[i][j], height}); 
             } 
