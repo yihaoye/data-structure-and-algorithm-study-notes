a7d626df9e1126487b74c3062599e73e8bb487c4
yihaoye <yihaoyemail@gmail.com>
2023-09-11 01:40:59 +1200

    lc sql q586 round 0

---
+++ Leetcode Practices/database/easy/586 Customer Placing the Largest Number of Orders.sql
@@ -0,0 +1,61 @@
+/*
+Table: Orders
+
++-----------------+----------+
+| Column Name     | Type     |
++-----------------+----------+
+| order_number    | int      |
+| customer_number | int      |
++-----------------+----------+
+order_number is the primary key (column with unique values) for this table.
+This table contains information about the order ID and the customer ID.
+ 
+
+Write a solution to find the customer_number for the customer who has placed the largest number of orders.
+
+The test cases are generated so that exactly one customer will have placed more orders than any other customer.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Orders table:
++--------------+-----------------+
+| order_number | customer_number |
++--------------+-----------------+
+| 1            | 1               |
+| 2            | 2               |
+| 3            | 3               |
+| 4            | 3               |
++--------------+-----------------+
+Output: 
++-----------------+
+| customer_number |
++-----------------+
+| 3               |
++-----------------+
+Explanation: 
+The customer with number 3 has two orders, which is greater than either customer 1 or 2 because each of them only has one order. 
+So the result is customer_number 3.
+ 
+
+Follow up: What if more than one customer has the largest number of orders, can you find all the customer_number in this case?
+*/
+
+
+
+-- GPT's Solution
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+HAVING COUNT(order_number) = (
+        SELECT MAX(cnt)
+        FROM (
+          SELECT COUNT(order_number) AS cnt
+          FROM Orders
+          GROUP BY customer_number
+        ) AS a
+)
