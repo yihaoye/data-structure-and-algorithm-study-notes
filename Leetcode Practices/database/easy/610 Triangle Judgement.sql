02ec504fbdbe1c93aec1733bf397fb23d1ddfad5
yihaoye <yihaoyemail@gmail.com>
2023-09-11 12:06:18 +1200

    lc sql q610 round 1

---
+++ Leetcode Practices/database/easy/610 Triangle Judgement.sql
@@ -0,0 +1,52 @@
+/*
+Table: Triangle
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
+| y           | int  |
+| z           | int  |
++-------------+------+
+In SQL, (x, y, z) is the primary key column for this table.
+Each row of this table contains the lengths of three line segments.
+ 
+
+Report for every three line segments whether they can form a triangle.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Triangle table:
++----+----+----+
+| x  | y  | z  |
++----+----+----+
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
++----+----+----+
+Output: 
++----+----+----+----------+
+| x  | y  | z  | triangle |
++----+----+----+----------+
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |
++----+----+----+----------+
+*/
+
+
+
+-- My Solution:
+SELECT x, y, z, 
+    (CASE 
+        WHEN x + y <= z THEN "No"
+        WHEN x + z <= y THEN "No"
+        WHEN y + z <= x THEN "No"
+        ELSE "Yes"
+    END) AS triangle
+FROM Triangle
