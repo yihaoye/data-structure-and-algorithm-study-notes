c955d353728b3a979b29e6f25936953fffb5a597
yihaoye <yihaoyemail@gmail.com>
2024-01-13 02:28:16 +1100

    lc q460 round 2

--- Leetcode Practices/algorithms/hard/460 LFU Cache.java
+++ Leetcode Practices/algorithms/hard/460 LFU Cache.java
@@ -55,6 +55,99 @@ At most 2 * 105 calls will be made to get and put.
 
 
 
+// Other's Solution 2:
+class LFUCache {
+    Map<Integer, Node> cache;  // 存储缓存的内容
+    Map<Integer, LinkedHashSet<Node>> freqMap; // 存储每个频次对应的双向链表
+    int size;
+    int capacity;
+    int min; // 存储当前最小频次
+
+    public LFUCache(int capacity) {
+        // https://leetcode.cn/problems/lfu-cache/solutions/48636/java-13ms-shuang-100-shuang-xiang-lian-biao-duo-ji/
+        // Time: O(1), Space: O(capacity)
+        cache = new HashMap<> (capacity);
+        freqMap = new HashMap<>();
+        this.capacity = capacity;
+    }
+    
+    public int get(int key) {
+        Node node = cache.get(key);
+        if (node == null) return -1;
+        freqInc(node);
+        return node.value;
+    }
+    
+    public void put(int key, int value) {
+        if (capacity == 0) return;
+        
+        Node node = cache.get(key);
+        if (node != null) {
+            node.value = value;
+            freqInc(node);
+        } else {
+            if (size == capacity) {
+                Node deadNode = removeNode();
+                cache.remove(deadNode.key);
+                size--;
+            }
+            Node newNode = new Node(key, value);
+            cache.put(key, newNode);
+            addNode(newNode);
+            size++;     
+        }
+    }
+
+    void freqInc(Node node) {
+        // 从原freq对应的链表里移除, 并更新min
+        int freq = node.freq;
+        LinkedHashSet<Node> set = freqMap.get(freq);
+        set.remove(node);
+        if (freq == min && set.size() == 0) min = freq + 1;
+
+        // 加入新freq对应的链表
+        node.freq++;
+        LinkedHashSet<Node> newSet = freqMap.get(freq + 1);
+        if (newSet == null) {
+            newSet = new LinkedHashSet<>();
+            freqMap.put(freq + 1, newSet);
+        }
+        newSet.add(node);
+    }
+
+    void addNode(Node node) {
+        LinkedHashSet<Node> set = freqMap.get(1);
+        if (set == null) {
+            set = new LinkedHashSet<>();
+            freqMap.put(1, set);
+        } 
+        set.add(node); 
+        min = 1;
+    }
+
+    Node removeNode() {
+        LinkedHashSet<Node> set = freqMap.get(min);
+        Node deadNode = set.iterator().next();
+        set.remove(deadNode);
+        return deadNode;
+    }
+}
+
+class Node {
+    int key;
+    int value;
+    int freq = 1;
+
+    public Node() {}
+    
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+
+
 // Other's Solution:
 class LFUCache {
     int minfreq, capacity;
