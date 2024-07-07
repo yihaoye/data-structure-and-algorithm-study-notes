0ef0dd22bd2afc1a10a06916e35b3631cdd8b587
yihaoye <yihaoyemail@gmail.com>
2024-07-07 23:35:21 +1000

    quick select

--- Leetcode Practices/algorithms/medium/215 Kth Largest Element in an Array.java
+++ Leetcode Practices/algorithms/medium/215 Kth Largest Element in an Array.java
@@ -3,6 +3,7 @@ Given an integer array nums and an integer k, return the kth largest element in 
 
 Note that it is the kth largest element in the sorted order, not the kth distinct element.
 
+Can you solve it without sorting?
  
 
 Example 1:
