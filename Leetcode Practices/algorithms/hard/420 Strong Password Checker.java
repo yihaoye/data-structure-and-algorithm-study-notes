2bbb6d8deb3f004f44a06063c1f0b0c7e78c965c
yihaoye <yihaoyemail@gmail.com>
2024-10-26 16:36:27 +1100

    lc q420 round 2

--- Leetcode Practices/algorithms/hard/420 Strong Password Checker.java
+++ Leetcode Practices/algorithms/hard/420 Strong Password Checker.java
@@ -115,3 +115,54 @@ class Solution {
         return res;
     }
 }
+
+
+
+// Other's Solution:
+class Solution {
+    public int strongPasswordChecker(String s) {
+        // O(N) - https://leetcode.com/problems/strong-password-checker/solutions/151333/easy-o-n-solution
+        int requiredChar = getRequiredChar(s);
+        if (s.length() < 6) return Math.max(requiredChar, 6 - s.length());
+
+        // only need replacement and deletion now when s.Length >= 6
+        int replace = 0; // total replacements for repeated chars. e.g. "aaa" needs 1 replacement to fix
+        int oned = 0; // total deletions for 3n repeated chars. e.g. "aaa" needs 1 deletion to fix
+        int twod = 0; // total deletions for 3n+1 repeated chars. e.g. "aaaa" needs 2 deletions to fix.
+
+        for (int i = 0; i < s.length();) {
+            int len = 1; // repeated len
+            while (i + len < s.length() && s.charAt(i + len) == s.charAt(i + len - 1)) {
+                len++;
+            }
+            i += len;
+            if (len < 3) continue;
+            replace += len / 3;
+            if (len % 3 == 0) oned += 1;
+            if (len % 3 == 1) twod += 2;
+        }
+
+        // no need deletion when s.Length <= 20
+        if (s.length() <= 20) return Math.max(requiredChar, replace);
+
+        int deleteCount = s.length() - 20;
+        // deleting 1 char in (3n) repeated chars will save one replacement
+        replace -= Math.min(deleteCount, oned);
+        // deleting 2 chars in (3n+1) repeated chars will save one replacement
+        replace -= Math.min(Math.max(deleteCount - oned, 0), twod) / 2;
+        // deleting 3 chars in (3n+2) repeated chars will save one replacement
+        replace -= Math.max(deleteCount - oned - twod, 0) / 3;
+
+        return deleteCount + Math.max(requiredChar, replace);
+    }
+
+    public int getRequiredChar(String s) {
+        int lowercase = 1, uppercase = 1, digit = 1;
+        for (var c : s.toCharArray()) {
+            if (Character.isLowerCase(c)) lowercase = 0;
+            else if (Character.isUpperCase(c)) uppercase = 0;
+            else if (Character.isDigit(c)) digit = 0;
+        }
+        return lowercase + uppercase + digit;
+    }
+}
