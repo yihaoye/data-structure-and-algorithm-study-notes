d9fbaec3387a80cf9c18c92dd9a6e9d8e550e114
yihaoye <yihaoyemail@gmail.com>
2025-05-16 22:46:56 +1000

    Solution template with assert enable

--- Other Practices/Solution.java
+++ Other Practices/Solution.java
@@ -1,37 +1,13 @@
 /*
-Given an n x n 2D matrix , write a function to rotate the image by 90 degrees (clockwise). 
+Description:
+
 
 Input: 
 
-const matrix = [ 
-    [1, 2, 3], 
-    [4, 5, 6], 
-    [7, 8, 9] 
-]; 
  
 Output: 
-[ 
-  [7, 4, 1], 
-  [8, 5, 2], 
-  [9, 6, 3] 
-] 
 
 
-0,0 -> 0,n-1
-0,1 -> 1,n-1
-0,2 -> 2,n-1
-
-1,0 -> 0,n-2 (n-1 - row)
-1,1 -> 1,n-2
-1,2 -> 2,n-2
-
-element (r, c) -> (c, n-1-r)
-
-[ 
-  [0, 0, 0], 
-  [0, 0, 0], 
-  [0, 0, 0] 
-] 
 */
 
 import java.util.*;
@@ -44,24 +20,17 @@ class Solution {
             {4, 5, 6}, 
             {7, 8, 9}
         };
-        s.process(matrix);
-        assert 
+        int res = s.process(matrix);
+        assert res == 0;
     }
 
-    public int[][] process(int[][] matrix) { // Time: O(n^2), Space: (n^2)
-        int n = matrix.length;
-        int[][] res = new int[n][n];
-        for (int r=0; r<n; r++) {
-            for (int c=0; c<n; c++) {
-                res[c][n-1-r] = matrix[r][c];
-            }
-        }
-        for (int[] row : res) {
-            for (int e : row) {
-                System.out.print(e + ", ");
+    public int process(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + ", ");
             }
             System.out.println("");
         }
-        return res;
+        return 0;
     }
 }
--- Other Practices/run.sh
+++ Other Practices/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 javac Solution.java
-java Solution
+java -ea Solution
